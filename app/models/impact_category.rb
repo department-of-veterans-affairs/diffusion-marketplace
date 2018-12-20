5aacf07b03d87a6ac6653b5276413b95ba98b04a
lightning <aurora.hay@agile6.com>
2018-12-20 12:00:23 -0800

    start seed data

--- app/models/impact_category.rb
+++ app/models/impact_category.rb
@@ -1,6 +1,5 @@
 class ImpactCategory < ApplicationRecord
-  acts_as_list
-  has_many :impacts
   has_many :sub_impact_categories, class_name: 'ImpactCategory', foreign_key: 'parent_category_id', dependent: :destroy
-  belongs_to :parent_category, class_name: 'ImpactCategory'
+  belongs_to :parent_category, class_name: 'ImpactCategory', optional: true
+  acts_as_list
 end
