c49fc308d50ed09758fbacd2597865ecb8b04099
lightning <aurora.hay@agile6.com>
2018-12-24 14:38:35 -0800

    add badge images and update schema

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,7 +1,7 @@
 class Badge < ApplicationRecord
   acts_as_list
-  has_attached_file :badge_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :badge_image, content_type: /\Aimage\/.*\z/
+  has_attached_file :badge_image
+  do_not_validate_attachment_file_type :badge_image
 
   belongs_to :strategic_sponsor, optional: true
   has_many :badge_practices
