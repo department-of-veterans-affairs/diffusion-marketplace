2c2039726f339d128d5da64456c5bd3329d2085a
lightning <aurora.hay@agile6.com>
2019-01-14 00:39:38 -0800

    add offset translucent circles

--- app/assets/javascripts/_background-circles.js
+++ app/assets/javascripts/_background-circles.js
@@ -1,6 +1,11 @@
 (($) => {
     const $document = $(document);
 
+    function loadCircleBackgrounds() {
+        backgroundCircles();
+        backgroundTranslucentCircles();
+    }
+
     function backgroundCircles() {
         const $section = $('.diffusion-background-circles');
 
@@ -10,5 +15,14 @@
         }
     }
 
-    $document.on('turbolinks:load', backgroundCircles);
+    function backgroundTranslucentCircles() {
+        const $section = $('.diffusion-background-circles-offset-translucent');
+
+        for (let i = 1; i < 22; i++) {
+            const id = `background-circle-offset-translucent-${i}`;
+            $section.append(`<div class="${id}" id="${id}"></div>`);
+        }
+    }
+
+    $document.on('turbolinks:load', loadCircleBackgrounds);
 })(window.jQuery);
