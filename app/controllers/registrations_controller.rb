ddc66a96c71f731d5b0202ffa5cfd7516af0a73b
Jake Holzhauer <Jake.holzhauer@yahoo.com>
2019-02-08 12:26:23 -0800

    Remove "authenticated" roles

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,6 @@ class RegistrationsController < Devise::RegistrationsController
     resource.save
     yield resource if block_given?
     if resource.persisted?
-      resource.add_role(User::USER_ROLES[0])
       if resource.active_for_authentication?
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
