e24a118b6d1ef8caa714c6239928d3434f53306b
lightning <aurora.hay@agile6.com>
2019-03-13 11:35:46 -0700

    fix layering of background circles

--- app/assets/javascripts/_background-circles.js
+++ app/assets/javascripts/_background-circles.js
@@ -10,37 +10,42 @@
 
     function backgroundCircles() {
         const $section = $('.diffusion-background-circles');
-
-        for (let i = 1; i < 22; i++) {
-            const id = `background-circle-${i}`;
-            $section.append(`<div class="${id}" id="${id}"></div>`);
+        if ($section.children().length === 0) {
+            for (let i = 1; i < 22; i++) {
+                const id = `background-circle-${i}`;
+                $section.append(`<div class="${id}" id="${id}"></div>`);
+            }
         }
     }
 
     function backgroundTranslucentCircles() {
         const $section = $('.diffusion-background-circles-offset-translucent');
-
-        for (let i = 1; i < 13; i++) {
-            const id = `background-circle-offset-translucent-${i}`;
-            $section.append(`<div class="${id}" id="${id}"></div>`);
+        if ($section.children().length === 0) {
+            for (let i = 1; i < 13; i++) {
+                const id = `background-circle-offset-translucent-${i}`;
+                $section.append(`<div class="${id}" id="${id}"></div>`);
+            }
         }
     }
 
     function backgroundPromoTranslucentCircles() {
         const $section = $('.diffusion-background-circles-promo-translucent');
-
-        for (let i = 1; i < 13; i++) {
-            const id = `background-circle-promo-translucent-${i}`;
-            $section.append(`<div class="${id}" id="${id}"></div>`);
+        if ($section.children().length === 0) {
+            for (let i = 1; i < 13; i++) {
+                const id = `background-circle-promo-translucent-${i}`;
+                $section.append(`<div class="${id}" id="${id}"></div>`);
+            }
         }
     }
 
     function backgroundTranslucentBlueCircles() {
         const $section = $('.diffusion-background-circles-translucent-blue');
 
-        for (let i = 1; i < 13; i++) {
-            const id = `background-circle-translucent-blue-${i}`;
-            $section.append(`<div class="${id}" id="${id}"></div>`);
+        if ($section.children().length === 0) {
+            for (let i = 1; i < 13; i++) {
+                const id = `background-circle-translucent-blue-${i}`;
+                $section.append(`<div class="${id}" id="${id}"></div>`);
+            }
         }
     }
 
