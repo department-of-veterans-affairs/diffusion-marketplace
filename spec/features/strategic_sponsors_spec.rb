882a9e879d7cec9ac8d8d37fbb7eb576750b84e5
lightning <aurora.hay@agile6.com>
2019-03-29 11:49:25 -0700

    practice partners: fix tests

--- spec/features/strategic_sponsors_spec.rb
+++ spec/features/strategic_sponsors_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 describe 'Strategic sponsors pages', type: :feature do
-  it 'should navigate to strategic sponsors list page' do
+  it 'should navigate to strategic sponsors list page', js: true do
     visit '/'
-    click_link 'Practice Partners'
+    find(:css, 'a', text: 'Practice Partners').click
     expect(current_path).to eq('/strategic_sponsors')
+  end
+
+  it 'should be 508 compliant' do
+    visit '/strategic_sponsors'
   end
 end
