6764f053afd36d338d852032cb5988181254c598
lightning <aurora.hay@agile6.com>
2019-05-01 17:52:16 -0700

    DM-406: DM-409 - add publications, publication files, and additional documents
    + fix date_initiated in practice importer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
   def date_format(date)
     date.in_time_zone.strftime "%B %Y"
   end
+
+  def content_type(attachment)
+    return 'DOCX' if is_word_doc?(attachment)
+    'PDF' if is_pdf?(attachment)
+  end
+
+  def is_word_doc?(attachment)
+    attachment.instance.attachment_content_type =~ %r(word)
+  end
+
+  def is_pdf?(attachment)
+    attachment.instance.attachment_content_type =~ %r(pdf)
+  end
+
 end
