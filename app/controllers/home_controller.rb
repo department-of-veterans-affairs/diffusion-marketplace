b374a4df64e2b19c23c5ac6497b20e23b9520bb7
ahay-agile6 <aurora.hampton@agile6.com>
2019-05-14 23:27:09 -0700

    DM-404: [App] Implement New Landing Page (#44)
    * Started work on landing page elements - WIP
    
    * DM-404: [DM-415, DM-416] implement new landing page
    
    * Delete _landing_practice.scss

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,19 @@
 class HomeController < ApplicationController
 
   def index
-    @top_three_gold_practices = Practice.joins(:practice_partners).where(approved: true, published: true, practice_partners: {name: 'Diffusion of Excellence'}).limit(3).order(position: :asc)
+    # TODO: make this configurable
+    @top_practice = Practice.find_by(name: 'Project HAPPEN')
+
+
+    # display the "Terrible Trio" && make configurable
+    # TODO: import Rapid Naloxone
+    naloxone = Practice.find_by(name: 'Rapid Naloxone')
+    my_life_my_story = Practice.find_by(name: 'My Life, My Story')
+    stride = Practice.find_by(name: 'Getting Hospitalized Veterans Back on Their Feet: The STRIDE Program')
+
+    @featured_practices = [naloxone, my_life_my_story, stride]
+
+    @facilities_data = facilities_json['features']
   end
 
 end
