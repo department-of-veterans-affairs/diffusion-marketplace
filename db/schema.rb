705f9d212ac2c1f3d1182f76955ac35fbe2239ef
ahay-agile6 <aurora.hampton@agile6.com>
2019-05-22 01:00:07 -0700

    DM-550: track user sign ins (#53)
    * DM-550: track user sign ins
    
    * DM-550: update schema

--- db/schema.rb
+++ db/schema.rb
@@ -616,10 +616,18 @@ ActiveRecord::Schema.define(version: 2019_05_17_052133) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "job_title"
@@ -630,9 +638,11 @@ ActiveRecord::Schema.define(version: 2019_05_17_052133) do
     t.datetime "password_changed_at"
     t.boolean "skip_va_validation", default: false, null: false
     t.boolean "disabled", default: false, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["password_changed_at"], name: "index_users_on_password_changed_at"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
