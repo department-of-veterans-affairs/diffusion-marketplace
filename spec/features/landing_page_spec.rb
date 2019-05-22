ec9d4bc84ffae1c8ac03361fa20bfcdcb1219f63
lightning <aurora.hay@agile6.com>
2019-05-21 23:42:37 -0700

    DM-610: update specs

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -5,5 +5,6 @@ describe 'The landing page', type: :feature do
     visit '/'
     expect(page).to be_accessible.according_to :wcag2a, :section508
     expect(page).to have_content('Diffusion Marketplace')
+    expect(page).to have_content('Saving lives by sharing best practices')
   end
 end
