231d4124ee877357f35cd85666d2963b44bd50b0
Nick Bristow <richard.nick.bristow@gmail.com>
2019-06-05 17:29:54 -0400

    Feedback modal (#64)
    * Add keyboard control to close button.
    Close modal when tabbing out of feedback modal
    
    * Add aria label to close button

--- app/assets/javascripts/_feedback_survey.js
+++ app/assets/javascripts/_feedback_survey.js
@@ -10,16 +10,25 @@
 
         // Get the <span> element that closes the modal
         const $span = $(".close");
+        const $lastEl = $(".last_el");
+        const $firstEl = $(".first_el");
 
         // When the user clicks the button, open the modal
         $feedbackModalTriggers.click(function(e) {
             e.preventDefault();
             modal.style.display = "block";
+            $span.focus();
         });
 
         // When the user clicks on <span> (x), close the modal
         $span.click(function() {
             modal.style.display = "none";
+        });
+
+        $span.on('keypress',function(e) {
+          if(e.which == 13) {
+            modal.style.display = "none";
+          }
         });
 
         // When the user clicks anywhere outside of the modal, close it
@@ -29,6 +38,16 @@
             }
         }
 
+        // When the user shift tabs to the first element in the modal, close it
+
+        $firstEl.on('focus', function(e){
+          modal.style.display = "none";
+        })
+
+        // When the user focuses on the last element in the modal, close it
+        $lastEl.on('focus', function(e){
+          modal.style.display = "none";
+        });
     }
 
     $document.on('turbolinks:load', feedbackSurveyModal);
