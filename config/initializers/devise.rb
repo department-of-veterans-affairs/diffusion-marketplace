a11306ffeb54753cc9e6dfca033ef62d5b79f55d
lightning <aurora.hampton@agile6.com>
2019-06-12 08:50:41 -0700

    DM-627: [DM-778]  monkeypatch for "Remember me"

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -334,3 +334,22 @@ Devise.setup do |config|
   # Time period for account expiry from last_activity_at
   # config.expire_after = 90.days
 end
+
+module Devise
+  module Models
+    module Timeoutable
+      # Checks whether the user session has expired based on configured time.
+      def timedout?(last_access)
+        return false if remember_exists_and_not_expired?
+        last_access && last_access <= self.class.timeout_in.ago
+      end
+
+      private
+
+      def remember_exists_and_not_expired?
+        return false unless respond_to?(:remember_expired?)
+        remember_created_at && !remember_expired?
+      end
+    end
+  end
+end
