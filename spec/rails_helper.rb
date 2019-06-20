d93558a4899ab7b0c689d3a7539340cdf4f64695
Jake Holzhauer <Jake.holzhauer@yahoo.com>
2019-06-20 11:24:00 -0700

    remove pause

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -94,8 +94,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.include PauseHelpers, type: :feature
 end
 
 Shoulda::Matchers.configure do |config|
