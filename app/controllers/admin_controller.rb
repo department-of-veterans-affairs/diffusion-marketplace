742c1453951c205ae23bb4159e4ba941d1f70b67
lightning <aurora.hampton@agile6.com>
2019-06-26 21:55:37 -0700

    DM-000: deliver password email now

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,7 +7,7 @@ class AdminController < ApplicationController
     else
       password = generate_password
       user = User.create! user_params.merge(password: password, password_confirmation: password)
-      AdminMailer.send_set_password(user_id: user.id, password: password).deliver_later
+      AdminMailer.send_set_password(user_id: user.id, password: password).deliver_now
       flash[:success] = "Created user \"#{user.email}\""
       redirect_to controller: :users, action: :index
     end
