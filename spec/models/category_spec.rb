3955fde208c3c1ea9284308f47358829a9041dfe
lightning <aurora.hampton@agile6.com>
2019-08-16 00:35:26 -0700

    fix shoulda matchers tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'associations' do
-    it { should belong_to(:parent_category) }
+    it { should belong_to(:parent_category).optional  }
     it { should have_many(:sub_impact_categories) }
   end
 end
