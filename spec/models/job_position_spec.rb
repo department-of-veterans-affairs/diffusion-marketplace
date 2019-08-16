e31df25aedf3bae4325477706256af595304ffda
lightning <aurora.hampton@agile6.com>
2019-08-16 00:35:26 -0700

    fix shoulda matchers tests

--- spec/models/job_position_spec.rb
+++ spec/models/job_position_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe JobPosition, type: :model do
   describe 'associations' do
-    it { should belong_to(:job_position_category) }
+    it { should belong_to(:job_position_category).optional  }
     it { should have_many(:job_position_practices) }
     it { should have_many(:practices) }
   end
