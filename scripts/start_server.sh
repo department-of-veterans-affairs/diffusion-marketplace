6e9ea77b3bed29af6a59bec399fdcff8c402f619
lightning <aurora.hampton@agile6.com>
2019-08-20 14:07:54 -0700

    DM-VAEC: update puma configuration to run without nginx

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # sleep 999999   # if you need to debug the container before starting server uncomment this line
 
-if [ "$CONTAINER_ROLE" == "bg_worker" ]
-then
-  bundle exec sidekiq
-elif [ "$CONTAINER_ROLE" == "app_server" ]
-then
-  echo "web start: bundle exec rake db:migrate && bundle exec puma -C /app/config/puma.rb"
+#if [ "$CONTAINER_ROLE" == "bg_worker" ]
+#then
+#  bundle exec sidekiq
+#elif [ "$CONTAINER_ROLE" == "app_server" ]
+#then
+  echo "web start: bundle exec rake db:migrate && bundle exec rails s -b 0.0.0.0"
   cp -R /app/public /var/www/
-  bundle exec rails db:migrate && bundle exec puma -C /app/config/puma.rb
-else
-  echo "Error: unknown CONTAINER_ROLE"
-  exit 125
-fi
+  bundle exec rails db:migrate && bundle exec rails s -b 0.0.0.0
+#else
+#  echo "Error: unknown CONTAINER_ROLE"
+#  exit 125
+#fi
