1265ca170a455b92670b90e8f6e4e73157e35db5
lightning <aurora.hampton@agile6.com>
2019-08-22 21:02:46 -0700

    DM-VAEC: do not need to copy anymore?

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -7,7 +7,7 @@ then
 elif [ "$CONTAINER_ROLE" == "app_server" ]
 then
   echo "web start: bundle exec rails db:migrate && bundle exec puma -C config/puma.rb"
-  cp -R /app/public /var/www/
+#  cp -R /app/public /var/www/
   bundle exec rails db:migrate && bundle exec puma -C config/puma.rb
 else
   echo "Error: unknown CONTAINER_ROLE"
