8f99c624891a2943cef23c5db12db17334d4d282
Joshua Drumm <34111449+JoshingYou1@users.noreply.github.com>
2019-08-26 14:58:23 -0700

    DM-933: [App] Add "System Redesign" to "Partners" Page (#89)
    * Updated the surveymonkey spreadsheet file
    
    * Added a new practice partner to the seed data

--- db/schema.rb
+++ db/schema.rb
@@ -362,32 +362,6 @@ ActiveRecord::Schema.define(version: 2019_08_13_231535) do
     t.index ["practice_id"], name: "index_implementation_timeline_files_on_practice_id"
   end
 
-  create_table "impressions", force: :cascade do |t|
-    t.string "impressionable_type"
-    t.integer "impressionable_id"
-    t.integer "user_id"
-    t.string "controller_name"
-    t.string "action_name"
-    t.string "view_name"
-    t.string "request_hash"
-    t.string "ip_address"
-    t.string "session_hash"
-    t.text "message"
-    t.text "referrer"
-    t.text "params"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["controller_name", "action_name", "ip_address"], name: "controlleraction_ip_index"
-    t.index ["controller_name", "action_name", "request_hash"], name: "controlleraction_request_index"
-    t.index ["controller_name", "action_name", "session_hash"], name: "controlleraction_session_index"
-    t.index ["impressionable_type", "impressionable_id", "ip_address"], name: "poly_ip_index"
-    t.index ["impressionable_type", "impressionable_id", "params"], name: "poly_params_request_index"
-    t.index ["impressionable_type", "impressionable_id", "request_hash"], name: "poly_request_index"
-    t.index ["impressionable_type", "impressionable_id", "session_hash"], name: "poly_session_index"
-    t.index ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index"
-    t.index ["user_id"], name: "index_impressions_on_user_id"
-  end
-
   create_table "job_position_categories", force: :cascade do |t|
     t.string "name"
     t.string "short_name"
