fef3dd5ad773f7a130c8efc80bd63728bbf345bb
nickbristow <richard.nick.bristow@gmail.com>
2019-08-26 23:02:48 -0400

    Fix spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'associations' do
     it { should have_many(:practices) }
-    it { should have_one_attached(:avatar) }
   end
 
   describe 'Roles' do
