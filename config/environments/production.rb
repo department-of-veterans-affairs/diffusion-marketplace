e5ca59661a2891d403de65e13bd47ce714560a75
lightning <aurora.hampton@agile6.com>
2019-09-07 19:58:33 -0700

    DM-VAEC: correct s3 settings for paperclip
    - no aws access key id or secret access key to use the instance profile instead

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,14 +96,13 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
       storage: :s3,
+      s3_region: region,
+      s3_host_name: "s3-#{region}.amazonaws.com",
       s3_credentials: {
-          s3_host_name: "s3-#{region}.amazonaws.com",
           bucket: ENV.fetch('S3_BUCKET_NAME'),
-          # access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-          # secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-          s3_region: region,
       },
-      s3_protocol: 'https'
+      s3_protocol: 'https',
+      s3_permissions: 'private'
   }
 
   config.action_mailer.smtp_settings = {
