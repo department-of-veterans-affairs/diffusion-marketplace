8ffe34388950707e94317f3c0da5b44a1b95b815
lightning <aurora.hampton@agile6.com>
2019-09-09 14:47:34 -0700

    DM-930: first iteration of diffusion map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,23 @@ class HomeController < ApplicationController
     @featured_practices = Practice.where featured: true
 
     @facilities_data = facilities_json['features']
+
+    vamc_facilities = JSON.parse(File.read("#{Rails.root}/lib/assets/vamc.json"))
+    @diffusion_histories = Gmaps4rails.build_markers(DiffusionHistory.all.group_by(&:facility_id)) do |dhg, marker|
+
+      facility = vamc_facilities.find {|f| f['StationNumber'] == dhg[0]}
+      marker.lat facility['Latitude']
+      marker.lng facility['Longitude']
+
+      marker.picture({
+                         url: view_context.image_path('fa-map-marker.png'),
+                         width: 16,
+                         height: 24
+                     })
+
+
+      marker.infowindow render_to_string(partial: 'maps/marker', locals: {diffusion_histories: dhg[1], facility: facility})
+    end
   end
 
 end
