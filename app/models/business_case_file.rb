8326cf5918e166a0a213dcf1ac697855648ad53f
lightning <aurora.hampton@agile6.com>
2019-09-10 17:22:10 -0700

    DM-999: presigned urls for private s3 bucket

--- app/models/business_case_file.rb
+++ app/models/business_case_file.rb
@@ -3,4 +3,8 @@ class BusinessCaseFile < ApplicationRecord
   has_attached_file :attachment
   do_not_validate_attachment_file_type :attachment
   belongs_to :practice
+
+  def attachment_s3_presigned_url(style = nil)
+    object_presigned_url(attachment, style)
+  end
 end
