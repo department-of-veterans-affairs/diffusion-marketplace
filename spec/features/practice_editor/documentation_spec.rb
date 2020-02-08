431caa330f605c09eddeaa7ca4ab18def4920d6d
aurora hampton <aurora.hampton@agile6.com>
2020-02-07 20:01:38 -0800

    DM-894: fix documentation test

--- spec/features/practice_editor/documentation_spec.rb
+++ spec/features/practice_editor/documentation_spec.rb
@@ -63,7 +63,7 @@ describe 'Practice editor', type: :feature, js: true do
             fill_in_doc_fields
             find('.add-additional-document-link').click
 
-            all('.hidden-documentation-field').last.attach_file(@doc_path)
+            all('.hidden-new-documentation-field').last.attach_file(@doc_path)
             all('.additional-document-title-input').last.set('Test title 2')
 
             fill_in_pub_fields
