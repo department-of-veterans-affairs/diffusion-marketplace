14fbfb3819036327d959cecffb3138b3b30e4655
Joshua Drumm <joshua.drumm@agile6.com>
2020-03-17 13:56:13 -0400

    DM-1372: Added model tests for timeline milestone

--- spec/models/milestone_spec.rb
+++ spec/models/milestone_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Milestone, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:timeline) }
+  end
 end
