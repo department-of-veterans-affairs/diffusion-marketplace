b74bafa8637e6aea1f57209f00fa8b8e351711d0
aurora hampton <aurora.hampton@agile6.com>
2020-03-20 12:45:45 -0700

    DM-1470: NTLM users do no confirm their accounts

--- app/admin/practices.rb
+++ app/admin/practices.rb
@@ -71,7 +71,8 @@ ActiveAdmin.register Practice do
       # set the user's attributes based on ldap entry
       user.skip_password_validation = true
       user.skip_va_validation = true
-      user.confirm
+      # TODO: public site: do we want created users to confirm their accounts?
+      user.confirm unless ENV['USE_NTLM'] == 'true'
       user.save
       practice.user = user
     end
