9570302c04fc399fd0f64a69eb91be008ec49c31
Joshua Drumm <joshua.drumm@agile6.com>
2020-03-20 16:47:06 -0400

    DM-1495: Fixed an issue where creating new timelines wasn't working as intended

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,7 +1,7 @@
 class Timeline < ApplicationRecord
   acts_as_list scope: :practice
   belongs_to :practice
-  has_many :milestones, -> {order(position: :asc)}, dependent: :destroy
+  has_many :milestones, dependent: :destroy
 
   accepts_nested_attributes_for :milestones, allow_destroy: true
 end
