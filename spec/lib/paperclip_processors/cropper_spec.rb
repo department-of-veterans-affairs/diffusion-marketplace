8775f947e045507f6c722888ecdb2f31a03ff490
aurora hampton <aurora.hampton@agile6.com>
2020-03-24 14:55:30 -0700

    DM-1378: simplify cropper creation

--- spec/lib/paperclip_processors/cropper_spec.rb
+++ spec/lib/paperclip_processors/cropper_spec.rb
@@ -16,16 +16,6 @@ module Paperclip
         # attachment = File.read("#{Rails.root}/spec/assets/charmander.png")
         image_path = "#{Rails.root}/spec/assets/charmander.png"
         image_file = File.new(image_path)
-        @practice.crop_x = 10
-        @practice.crop_y = 10
-        @practice.crop_w = 10
-        @practice.crop_h = 10
-        @practice.main_display_image = ActionDispatch::Http::UploadedFile.new(
-            filename: File.basename(image_file),
-            tempfile: image_file,
-            # detect the image's mime type with MIME if you can't provide it yourself.
-            type: MIME::Types.type_for(image_path).first.content_type
-        )
 
         cropper = Cropper.new(image_file, {}, @practice.main_display_image)
 
