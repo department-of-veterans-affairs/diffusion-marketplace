87c7dc33778efb3c48d5385f261e4908c182e26f
elisa lee <emyl3@users.noreply.github.com>
2020-03-24 17:04:34 -0500

    Finish tests for cropper

--- spec/lib/paperclip_processors/cropper_spec.rb
+++ spec/lib/paperclip_processors/cropper_spec.rb
@@ -4,22 +4,38 @@ require_relative '../../../lib/paperclip_processors/cropper.rb'
 module Paperclip
   RSpec.describe Cropper do
     before do
-
       @practice = Practice.new
       @practice.name = "Crop this image"
       @practice.tagline = "Cropping is so much fun."
-
+      image_path = "#{Rails.root}/spec/assets/charmander.png"
+      @image_file = File.new(image_path)
     end
 
-    describe '#transformation_command' do
-      it 'returns the crop cmd when cropping?' do
-        # attachment = File.read("#{Rails.root}/spec/assets/charmander.png")
-        image_path = "#{Rails.root}/spec/assets/charmander.png"
-        image_file = File.new(image_path)
+    describe 'when cropping?' do
+      it 'returns the crop cmd' do
+        @practice.crop_x = 10
+        @practice.crop_y = 10
+        @practice.crop_w = 10
+        @practice.crop_h = 10
 
-        cropper = Cropper.new(image_file, {}, @practice.main_display_image)
+        cropper = Cropper.new(@image_file, {}, @practice.main_display_image)
 
-        # expect
+        expect(cropper.cropping?).to eq true
+        expect(cropper.transformation_command).to eq ["-crop", "10.0x10.0+10.0+10.0", "+repage", "-auto-orient"]
+      end
+    end
+
+    describe 'when not cropping?' do
+      it 'does not return the crop cmd' do
+        @practice.crop_x = 10
+        @practice.crop_y = 10
+        @practice.crop_w = nil
+        @practice.crop_h = nil
+
+        cropper = Cropper.new(@image_file, {}, @practice.main_display_image)
+
+        expect(cropper.cropping?).to eq false
+        expect(cropper.transformation_command).to eq ["-auto-orient"]
       end
     end
   end
