29764a621a14ba556783e086dbdb118155312442
Joshua Drumm <joshua.drumm@agile6.com>
2020-04-13 16:35:55 -0400

    DM-1175: Fixed iNET practices and Go Fish practices

--- db/migrate/20200409155443_add_inet_partner_to_practice_partners.rb
+++ db/migrate/20200409155443_add_inet_partner_to_practice_partners.rb
@@ -7,32 +7,10 @@ class AddInetPartnerToPracticePartners < ActiveRecord::Migration[5.2]
 
     inet_practice_slugs = [
         'healthier-kidneys-through-your-kitchen',
-        'whoopsafe',
         'geri-vet',
-        'preoperative-frailty-screening-prehabilitation',
-        'gerofit',
-        'video-blood-pressure-visits',
-        'veterans-mental-evaluation-team-vmet',
-        'virtual-claims-clinic',
-        'telerehab-wheeled-mobility-clinic-to-cnh',
-        'jumpstart-new-hire-onboarding-program',
-        'dedicated-environmental-services-training-specialist',
-        'suicide-alert-red-card',
-        'increasing-access-to-primary-care-using-pharmacist-providers',
-        '4-sight',
-        'caring-for-older-adults-and-caregivers-at-home-coach',
-        'copd-care',
-        'rn-stay-interviews-a-nurse-retention-strategy',
-        'project-happen',
-        'utilizing-machine-learning-uml',
-        'advanced-comprehensive-diabetes-care-acdc',
-        'environmental-management-service-ems',
-        'vha-rapid-naloxone',
-        'national-telewound-care-practice',
-        'the-stride-program',
-        'my-life-my-story',
-        'vione',
-        'flow3'
+        'cards-for-connection',
+        'pride-in-all-who-served-reducing-healthcare-disparities-for-lgbt-veterans',
+        'transcending-self-therapy-integrative-cognitive-behavioral-treatment'
     ]
     inet_practice_slugs.each do |ips|
       inet_practice = Practice.find_by(slug: ips)
---
+++ db/migrate/20200413182933_add_go_fish_badge_to_badges_and_create_go_fish_badge_practices.rb
@@ -0,0 +1,44 @@
+class AddGoFishBadgeToBadgesAndCreateGoFishBadgePractices < ActiveRecord::Migration[5.2]
+  def change
+    Badge.create({name: 'Go Fish'})
+
+    go_fish_badge_id = Badge.find_by(name: 'Go Fish').id
+    go_fish_practice_slugs = [
+        'healthier-kidneys-through-your-kitchen',
+        'whoopsafe',
+        'geri-vet',
+        'preoperative-frailty-screening-prehabilitation',
+        'gerofit',
+        'video-blood-pressure-visits',
+        'veterans-mental-evaluation-team-vmet',
+        'virtual-claims-clinic',
+        'telerehab-wheeled-mobility-clinic-to-cnh',
+        'jumpstart-new-hire-onboarding-program',
+        'dedicated-environmental-services-training-specialist',
+        'suicide-alert-red-card',
+        'increasing-access-to-primary-care-using-pharmacist-providers',
+        '4-sight',
+        'caring-for-older-adults-and-caregivers-at-home-coach',
+        'copd-care',
+        'rn-stay-interviews-a-nurse-retention-strategy',
+        'project-happen',
+        'utilizing-machine-learning-uml',
+        'advanced-comprehensive-diabetes-care-acdc',
+        'environmental-management-service-ems',
+        'vha-rapid-naloxone',
+        'national-telewound-care-practice',
+        'the-stride-program',
+        'my-life-my-story',
+        'vione',
+        'flow3'
+    ]
+    go_fish_practice_slugs.each do |gfps|
+      go_fish_practice = Practice.find_by(slug: gfps)
+      if !go_fish_practice.nil?
+        BadgePractice.create({practice_id: go_fish_practice.id, badge_id: go_fish_badge_id})
+      else
+        puts "The slug #{gfps} does not exist"
+      end
+    end
+  end
+end
