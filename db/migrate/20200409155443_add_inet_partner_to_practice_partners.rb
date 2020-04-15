2f48f29df0fdce55abc34225203a49b5333616cb
Joshua Drumm <joshua.drumm@agile6.com>
2020-04-15 13:11:38 -0400

    DM-1175: Fixed an issue in the migration that creates the iNET practice partner

--- db/migrate/20200409155443_add_inet_partner_to_practice_partners.rb
+++ db/migrate/20200409155443_add_inet_partner_to_practice_partners.rb
@@ -2,7 +2,7 @@ class AddInetPartnerToPracticePartners < ActiveRecord::Migration[5.2]
   def change
     iNet = PracticePartner.find_by(slug: 'vha-innovators-network')
     unless iNet
-      PracticePartner.create!({name: 'VHA Innovators Network', short_name: 'iNET', description: 'The VHA Innovators Network (iNET) is a network of 33 VA medical centers changing the way employees think and solve problems through training and accelerated operationalizing innovation.', slug: 'vha-innovators-network'})
+      iNet = PracticePartner.create!({name: 'VHA Innovators Network', short_name: 'iNET', description: 'The VHA Innovators Network (iNET) is a network of 33 VA medical centers changing the way employees think and solve problems through training and accelerated operationalizing innovation.', slug: 'vha-innovators-network'})
     end
 
     inet_practice_slugs = [
