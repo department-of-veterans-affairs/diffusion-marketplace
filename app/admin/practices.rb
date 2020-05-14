dd491e47ea91e2ab0e3e8cce558f58e28f9bdce4
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2020-05-14 12:39:48 -0700

    dm-1687
    Updated csv column names

--- app/admin/practices.rb
+++ app/admin/practices.rb
@@ -9,13 +9,13 @@ ActiveAdmin.register Practice do
 
   csv do
     if params[:scope] == "get_practice_owner_emails"
-      column :name
-      column(:user) {|practice| practice.user&.email}
+      column(:practice_name) {|practice| practice.name}
+      column(:owner_email) {|practice| practice.user&.email}
     else
       column :id
-      column :name
+      column(:practice_name) {|practice| practice.name}
       column :support_network_email
-      column(:user) {|practice| practice.user&.email}
+      column(:owner_email) {|practice| practice.user&.email}
       column :created_at
     end
   end
@@ -26,7 +26,7 @@ ActiveAdmin.register Practice do
     id_column unless params[:scope] == "get_practice_owner_emails"
     column(:practice_name) {|practice| practice.name}
     column :support_network_email unless params[:scope] == "get_practice_owner_emails"
-    column(:user_email) {|practice| practice.user&.email}
+    column(:owner_email) {|practice| practice.user&.email}
     column :created_at unless params[:scope] == "get_practice_owner_emails"
     actions
   end
