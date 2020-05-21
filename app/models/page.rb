e9b64fb76e3165bff0197e42c062cd352595e485
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2020-05-21 10:56:48 -0700

    dm-1704
    Added regex to ensure slug/URL field in page builder is URL safe.

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,8 +6,9 @@ class Page < ApplicationRecord
   validates :title, presence:true
   validates :description, presence:true
   validates_uniqueness_of :slug
-  validates :slug, format: { without: /\s/, message: "URL can not contain spaces" }
-  validate :string_presence
+  SLUG_FORMAT = /^[a-zA-Z0-9_-]*$*/
+  validates :slug, uniqueness: {case_sensitive: false},
+            format: {with: Regexp.new('\A' + SLUG_FORMAT.source + '\z'), message: "invalid characters in URL"}
   validate :downcase_fields
   before_save :downcase_fields
 
@@ -15,11 +16,5 @@ class Page < ApplicationRecord
 
   def downcase_fields
     self.slug = self.slug.downcase
-  end
-
-  def string_presence
-    if slug.include?("/")
-      errors.add(:slug, "URL cannot contain '/'")
-    end
   end
 end
