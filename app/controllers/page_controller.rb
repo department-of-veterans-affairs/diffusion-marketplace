767ad848705de06ea2ab30a5731be4af3b67886f
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2020-05-28 14:08:16 -0700

    dm-1790
    custom page permissions - ie., if unpublished - only logged in admin can view.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,10 +1,12 @@
 class PageController < ApplicationController
+  #before_action :is_admin, :only => [:new, :edit, :create, :destroy]
+
   def show
     page_slug = params[:page_slug] ? params[:page_slug] : 'home'
     @page = Page.includes(:page_group).find_by(slug: page_slug, page_groups: {slug: params[:page_group_friendly_id]})
     @path_parts = request.path.split('/')
     @facilities_data = facilities_json
-
+    is_admin @page
     if page_slug.include?('home')
       @breadcrumbs = [
           { text: 'Home', path: root_path },
@@ -23,5 +25,24 @@ class PageController < ApplicationController
           { text: "#{@page.title}" }
       ]
     end
+  end
+  def is_admin(page)
+    is_a_admin = false
+    if current_user
+      if not @page.published
+        current_user.roles.each do |role|
+          if role.name == "admin"
+            is_a_admin = true
+            break
+          end
+        end
+        if not is_a_admin
+          redirect_to(root_path)
+        end
+      end
+    else
+      redirect_to(root_path)
+    end
+      #(current_user.nil?) ? redirect_to(root_path) : (redirect_to(root_path) unless current_user.admin?)
   end
 end
