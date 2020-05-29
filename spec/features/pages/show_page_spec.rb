bd01ee50effef0c58065285d508290634516423f
elisa lee <emyl3@users.noreply.github.com>
2020-05-29 13:50:38 -0500

    DM-1704: Fix failing tests

--- spec/features/pages/show_page_spec.rb
+++ spec/features/pages/show_page_spec.rb
@@ -7,13 +7,18 @@ describe 'Page Builder - Show', type: :feature do
       Practice.create!(name: 'A cool practice', approved: true, published: true, tagline: 'Test tagline')
     ]
     page_group = PageGroup.create(name: 'programming', slug: 'programming', description: 'Pages about programming go in this group.')
-    page = Page.create(page_group: page_group, title: 'ruby', description: 'what a gem', slug: 'ruby-rocks', created_at: Time.now)
-    Page.create(page_group: page_group, title: 'javascript', description: 'cool stuff', slug: 'javascript', created_at: Time.now)
+    page = Page.create(page_group: page_group, title: 'ruby', description: 'what a gem', slug: 'ruby-rocks', created_at: Time.now, published: Time.now)
+    Page.create(page_group: page_group, title: 'javascript', description: 'cool stuff', slug: 'javascript', created_at: Time.now, published: Time.now)
     practice_list_component = PagePracticeListComponent.create(practices: [@practices[0][:id].to_s, @practices[1][:id].to_s,])
     subpage_hyperlink_component = PageSubpageHyperlinkComponent.create(url: '/programming/javascript', title: 'Check out JavaScript', description: 'It is pretty cool too')
-    PageComponent.create(page: page, component: practice_list_component, created_at: Time.now )
-    PageComponent.create(page: page, component: subpage_hyperlink_component, created_at: Time.now )
+    PageComponent.create(page: page, component: practice_list_component, created_at: Time.now)
+    PageComponent.create(page: page, component: subpage_hyperlink_component, created_at: Time.now)
 
+    user = User.create!(email: 'sandy.cheeks@bikinibottom.net', password: 'Password123',
+      password_confirmation: 'Password123', skip_va_validation: true, confirmed_at: Time.now, accepted_terms: true)
+    user.add_role(:admin)
+    # must be logged in to view pages
+    login_as(user, scope: :user, run_callbacks: false)
     visit '/programming/ruby-rocks'
   end
 
