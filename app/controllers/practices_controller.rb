11e1b1bb90421d1cb4de855e62cf2e63c401c33b
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2020-06-18 13:55:16 -0700

    dm-1825

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -257,6 +257,8 @@ class PracticesController < ApplicationController
   # /practices/slug/introduction
   def introduction
     set_practice
+    # @data = JSON.parse(File.read("#{Rails.root}/lib/assets/practice_origin_lookup.json"))
+    # debugger
   end
 
   # GET /practices/1/origin
@@ -403,7 +405,8 @@ class PracticesController < ApplicationController
   def practice_params
     params.require(:practice).permit(:need_training, :short_name, :tagline, :process, :it_required, :need_new_license, :description, :name, :initiating_facility, :summary, :origin_title, :origin_story, :cost_to_implement_aggregate, :sustainability_aggregate, :veteran_satisfaction_aggregate, :difficulty_aggregate, :date_initiated,
                                      :number_adopted, :number_departments, :number_failed, :implementation_time_estimate, :implementation_time_estimate_description, :implentation_summary, :implentation_fte,
-                                     :training_provider, :training_length, :training_test, :training_provider_role, :required_training_summary, :support_network_email, :initiating_facility_other,
+                                     :training_provider, :training_length, :training_test, :training_provider_role, :required_training_summary, :support_network_email,
+                                     :initiating_facility_other, :initiating_facility_type, :initiating_department__office_id, :initiating_visn_id,
                                      :main_display_image, :crop_x, :crop_y, :crop_h, :crop_w,
                                      :delete_main_display_image,
                                      :origin_picture, :origin_picture_original_w, :origin_picture_original_h, :origin_picture_crop_x, :origin_picture_crop_y, :origin_picture_crop_w, :origin_picture_crop_h,
