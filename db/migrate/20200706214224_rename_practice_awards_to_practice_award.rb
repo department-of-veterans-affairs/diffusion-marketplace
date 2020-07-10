2da5c22c77c669748dd20026febb8179a76b6f9a
lightning hampton <aurora.hampton@agile6.com>
2020-07-10 09:31:04 -0700

    DM-1825: remove unnecessary migrations

--- db/migrate/20200706214224_rename_practice_awards_to_practice_award.rb
+++
@@ -1,4 +0,0 @@
-class RenamePracticeAwardsToPracticeAward < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20200706214303_rename_practice_origin_facilities_to_practice_origin_facility.rb
+++
@@ -1,4 +0,0 @@
-class RenamePracticeOriginFacilitiesToPracticeOriginFacility < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
