32d337ec37fdfefb253310a106491210675a7912
elisa lee <emyl3@users.noreply.github.com>
2020-07-20 14:31:48 -0500

    DM-1825: [DM-1946] Fix last updated time

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -1,5 +1,5 @@
 module TimeHelper
   def timeago(time)
-    content_tag(:time, time.iso8601, class: 'timeago', datetime: time.iso8601)
+    content_tag(:time, time.iso8601, class: 'timeago', datetime: time.iso8601, title: "#{time.in_time_zone('America/New_York').strftime("%B %e, %Y at %I:%M %p")}")
   end
 end
