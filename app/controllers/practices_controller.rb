61d3e6321303e20b34ad0aaa3d2104f8b7c32ba3
Aurora Hampton <aurora.hampton@agile6.com>
2020-08-17 22:23:19 -0700

    DM-1990: update tests

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -4,7 +4,7 @@ class PracticesController < ApplicationController
                                       :commit, :committed, :highlight, :un_highlight, :feature,
                                       :un_feature, :favorite, :instructions, :overview, :origin,
                                       :collaborators, :impact, :resources, :documentation,
-                                      :complexity, :timeline, :risk_and_mitigation, :contact,
+                                      :departments, :timeline, :risk_and_mitigation, :contact,
                                       :checklist, :publication_validation, :adoptions,
                                       :create_or_update_diffusion_history]
   before_action :set_facility_data, only: [:show, :planning_checklist]
