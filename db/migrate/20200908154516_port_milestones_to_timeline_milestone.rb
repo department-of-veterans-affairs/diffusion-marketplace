bfc45c9fe55b66144b89e86f865aa706c6d7c151
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2020-09-08 15:54:25 -0700

    dm-2060
    Migration task to port milestones to Timeline.milestone field with line breaks.

---
+++ db/migrate/20200908154516_port_milestones_to_timeline_milestone.rb
@@ -0,0 +1,18 @@
+class PortMilestonesToTimelineMilestone < ActiveRecord::Migration[5.2]
+  def change
+    tl_ids = Array.new
+    Milestone.all.each do |ms|
+      if(!tl_ids.include?(ms.timeline_id))
+        tl_ids.push(ms.timeline_id)
+        tl_milestone = "";
+        cur_tl_id = ms.timeline_id
+        Milestone.all.where(timeline_id: cur_tl_id.to_s).each do |d|
+          tl_milestone += d.description + "\r\n"
+        end
+        tl = Timeline.find_by(id: ms.timeline_id.to_s)
+        tl.milestone = tl_milestone
+        tl.save
+      end
+    end
+  end
+end
