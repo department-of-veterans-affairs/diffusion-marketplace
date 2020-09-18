8c0c75fa373e59783213bd94820848baf67aae33
Aurora Hampton <aurora.hampton@agile6.com>
2020-09-18 10:22:23 -0700

    DM-2015: rearrange test

--- spec/features/practice_editor/about_spec.rb
+++ spec/features/practice_editor/about_spec.rb
@@ -1,14 +1,11 @@
 require 'rails_helper'
 
-describe 'Practice editor', type: :feature, js: true do
-    before do
-        @admin = User.create!(email: 'toshiro.hitsugaya@soulsociety.com', password: 'Password123', password_confirmation: 'Password123', skip_va_validation: true, confirmed_at: Time.now, accepted_terms: true)
-        @practice = Practice.create!(name: 'An awesome practice', slug: 'an-awesome-practice', approved: true, published: true, tagline: 'Test tagline', date_initiated: Date.new(2011, 12, 31))
-        @admin.add_role(User::USER_ROLES[0].to_sym)
-    end
-
+describe 'Practice editor', type: :feature do
     describe 'About page' do
         before do
+            @admin = User.create!(email: 'toshiro.hitsugaya@soulsociety.com', password: 'Password123', password_confirmation: 'Password123', skip_va_validation: true, confirmed_at: Time.now, accepted_terms: true)
+            @practice = Practice.create!(name: 'An awesome practice', slug: 'an-awesome-practice', approved: true, published: true, tagline: 'Test tagline', date_initiated: Date.new(2011, 12, 31))
+            @admin.add_role(User::USER_ROLES[0].to_sym)
             login_as(@admin, :scope => :user, :run_callbacks => false)
             @origin_story = 'This practice was founded on the basis of being awesome.'
             @creator_name = 'Grimmjow Jaegerjaquez'
