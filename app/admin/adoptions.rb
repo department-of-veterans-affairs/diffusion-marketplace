c1ab3dec14b91f2fdb65b7ef7e263437c37aacca
Joshua Drumm <joshua.drumm@agile6.com>
2020-10-14 13:44:18 -0400

    DM-2067: Added 'All adoptions' panel in Adoptions tab and wrote a unit test

--- app/admin/adoptions.rb
+++ app/admin/adoptions.rb
@@ -7,19 +7,21 @@ ActiveAdmin.register_page "Adoptions" do
     helper_method :adoption_rurality
     helper_method :adoption_xlsx_styles
     helper_method :get_adoption_values
-    helper_method :get_adoption_counts
+    helper_method :adoption_counts_by_practice
+    helper_method :all_adoption_counts
     before_action :set_adoption_values
 
     def set_adoption_values
       @practices = Practice.order(Arel.sql("lower(name) ASC"))
       @complete_map = {}
-      @adoption_counts = {}
+      @practice_adoption_counts = {}
       @practice_ids = {}
       @practices.each do |p|
         get_adoption_values(p, @complete_map)
-        @adoption_counts[p.name] = get_adoption_counts(p)
+        @practice_adoption_counts[p.name] = adoption_counts_by_practice(p)
         @practice_ids.merge!({p.name => p.id})
       end
+      @all_adoption_counts = all_adoption_counts
     end
   end
 
@@ -30,7 +32,7 @@ ActiveAdmin.register_page "Adoptions" do
 
         # building out xlsx file
         p.workbook.add_worksheet(:name => "Practice Adoptions - #{Date.today}") do |sheet|
-          sheet.add_row ["Adoptions by Practice - #{Date.today}"], style: @xlsx_main_header
+          sheet.add_row ['Practice Adoptions'], style: @xlsx_main_header
           sheet.add_row [''], style: @xlsx_divider
           sheet.add_row ['Please Note'], style: @xlsx_legend_no_bottom_border
           sheet.add_row ['Adoption date is based on the adoption status.'], style: @xlsx_legend_no_y_border
@@ -40,6 +42,14 @@ ActiveAdmin.register_page "Adoptions" do
           sheet.merge_cells 'A1:C1'
           sheet.add_row [''], style: @xlsx_divider
 
+          sheet.add_row ['All adoptions'], style: @xlsx_sub_header_2
+          sheet.add_row ["#{@date_headers[:current]}", @all_adoption_counts[:adoptions_this_month]], style: @xlsx_entry
+          sheet.add_row ["#{@date_headers[:one_month_ago]}", @all_adoption_counts[:adoptions_one_month_ago]], style: @xlsx_entry
+          sheet.add_row ["#{@date_headers[:two_month_ago]}", @all_adoption_counts[:adoptions_two_months_ago]], style: @xlsx_entry
+          sheet.add_row ["#{@date_headers[:total]}", @all_adoption_counts[:total_adoptions]], style: @xlsx_entry
+
+          sheet.add_row [''], style: @xlsx_divider
+
           @complete_map.each do |name, value|
             if value.present?
               practice_id = @practice_ids[name]
@@ -47,7 +57,7 @@ ActiveAdmin.register_page "Adoptions" do
               sheet.add_row ["Practice Id", practice_id], style: @xlsx_entry
               # adoption counts
               sheet.add_row ['Adoption Counts'], style: @xlsx_sub_header
-              @adoption_counts.each do |key, counts|
+              @practice_adoption_counts.each do |key, counts|
                 if key == name
                   sheet.add_row ["#{@date_headers[:current]}", counts[:adopted_this_month]], style: @xlsx_entry
                   sheet.add_row ["#{@date_headers[:one_month_ago]}", counts[:adopted_one_month_ago]], style: @xlsx_entry
@@ -94,30 +104,40 @@ ActiveAdmin.register_page "Adoptions" do
   end
 
   content do
-    if DiffusionHistory.any?
-      h1 'Adoptions by practice'
-      div(class: 'form-and-legend-container') do
-        # export .xlsx button
-        form action: admin_adoptions_export_all_adoptions_path, method: :get, style: 'text-align: left' do |f|
-          f.input :submit, type: :submit, value: 'Download All', style: 'margin-bottom: 1rem'
-        end
+    div(class: 'form-and-legend-container') do
+      # export .xlsx button
+      form action: admin_adoptions_export_all_adoptions_path, method: :get, style: 'text-align: left' do |f|
+        f.input :submit, type: :submit, value: 'Download All', style: 'margin-bottom: 1rem'
+      end
 
-        div(class: 'adoptions-legend') do
-          h3 do
-            'Please Note'
+      div(class: 'adoptions-legend') do
+        h3 do
+          'Please Note'
+        end
+        h4 do
+          span 'Adoption date is based on the adoption status.'
+        end
+        ul do
+          li do
+            span 'Completed/Unsuccessful: '
+            span 'End Date'
           end
-          h4 do
-            span 'Adoption date is based on the adoption status.'
+          li do
+            span 'In Progress: '
+            span 'Start Date'
           end
-          ul do
-            li do
-              span 'Completed/Unsuccessful: '
-              span 'End Date'
-            end
-            li do
-              span 'In Progress: '
-              span 'Start Date'
-            end
+        end
+      end
+    end
+
+    if DiffusionHistory.any?
+      panel 'All adoptions' do
+        columns class: 'all-adoptions-columns' do
+          table_for all_adoption_counts do
+            column("#{date_headers[:current]}", class: "all-adoptions-current-month") {|ac| ac[:adoptions_this_month]}
+            column("#{date_headers[:one_month_ago]}", class: "all-adoptions-last-month") {|ac| ac[:adoptions_one_month_ago]}
+            column("#{date_headers[:two_month_ago]}", class: "all-adoptions-two-months-ago") {|ac| ac[:adoptions_two_months_ago]}
+            column("#{date_headers[:total]}", class: "all-adoptions-total") {|ac| ac[:total_adoptions]}
           end
         end
       end
@@ -130,7 +150,7 @@ ActiveAdmin.register_page "Adoptions" do
               h3 do
                 "Adoption Counts"
               end
-              adoption_counts.each do |key, counts|
+              practice_adoption_counts.each do |key, counts|
                 if key == name
                   table_for counts do
                     column("#{date_headers[:current]}") {|c| c[:adopted_this_month]}
