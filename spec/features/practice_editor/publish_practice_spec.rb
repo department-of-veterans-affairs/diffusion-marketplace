1620f47021f129107cb199fc4859c8e76f19b527
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2020-10-21 10:34:37 -0700

    DM-2145
    retry, again publishing modal test.

--- spec/features/practice_editor/publish_practice_spec.rb
+++ spec/features/practice_editor/publish_practice_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Practice editor', type: :feature, js: true do
   before do
     @admin = User.create!(email: 'toshiro.hitsugaya@soulsociety.com', password: 'Password123', password_confirmation: 'Password123', skip_va_validation: true, confirmed_at: Time.now, accepted_terms: true)
-    @practice = Practice.create!(name: 'A public practice', tagline: 'a public tagline', slug: 'a-public-practice', approved: true, published: false)
+    @practice = Practice.create!(name: 'A public practice', slug: 'a-public-practice', approved: true, published: false)
     @practice_partner = PracticePartner.create!(name: 'Diffusion of Excellence', short_name: '', description: 'The Diffusion of Excellence Initiative', icon: 'fas fa-heart', color: '#E4A002')
     @admin.add_role(User::USER_ROLES[0].to_sym)
     Category.create!(name: 'Pulmonary Care')
@@ -52,11 +52,15 @@ describe 'Practice editor', type: :feature, js: true do
       page.has_css?('.publication-modal-body')
       expect(page).to have_content('Cannot publish yet')
       expect(page).to have_content('This is what you need to do before publishing your practice to the Diffusion Marketplace')
+      expect(page).to have_content('You must include a tagline for your practice')
       expect(page).to have_content('You must include the initiation date for your practice')
       expect(page).to have_content('You must include the initiating facility for your practice')
       expect(page).to have_content('You must include a practice summary')
       expect(page).to have_content('You must include at least one adoption')
       expect(page).to have_content('You must include a support network email')
+      expect(page).to have_content('You must include a problem statement for your practice')
+      expect(page).to have_content('You must include a solution statement for your practice')
+      expect(page).to have_content('You must include a results statement for your practice')
       find('.back-to-editor-button').click
       set_pr_required_fields
       set_initiating_fac
