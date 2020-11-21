d179f614c6d4c6c510813f061a40a0c3bc04d17d
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2020-11-20 16:26:25 -0800

    dm-2202
    Includes dm-2177 as well.  Also found a bug in the email covid categories... update_categories method being called from different endpoints i.e., (overview, Adoptions, Implementation ..etc) will trigger the update with no category_params.. and we have logic says that if no category params exist... but local ones do exist assume those are being removed.. so the email will go out but the category items aren't actually removed.. just the email says they will be...
     :)  fix is to only run the covid category mailer if the current endpoint is 'introdcuction' - then the category_params will be populated.

--- app/services/save_practice_service.rb
+++ app/services/save_practice_service.rb
@@ -145,8 +145,9 @@ class SavePracticeService
     category_params = @practice_params[:category]
     practice_category_practices = @practice.category_practices
     practice_categories = @practice.categories
-
-    covid_category_notifications(category_params, practice_categories)
+    if  @current_endpoint.downcase == "introduction"
+      covid_category_notifications(category_params, practice_categories)
+    end
     if category_params.present?
       category_attribute_params = @practice_params[:categories_attributes]
       cat_keys = category_params.keys
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   post '/accept_terms', action: 'accept_terms', controller: 'users', as: 'accept_terms'
 
   resources :practices, except: :index do
-    get '/planning-checklist', action: 'planning_checklist', as: 'planning_checklist'
     get '/committed', action: 'committed', as: 'committed'
     get '/edit/instructions', action: 'instructions', as: 'instructions'
     get '/edit/introduction', action: 'introduction', as: 'introduction'
