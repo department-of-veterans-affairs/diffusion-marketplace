cdb2f2a42342b01c7dbf7a713de89dfcea5135f3
Joshua Drumm <joshua.drumm@agile6.com>
2020-11-24 15:19:30 -0500

    DM-2208: Fixed some breadcrumb issues

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -8,6 +8,10 @@ module NavigationHelper
       session[:breadcrumbs] = []
     end
 
+    if controller == 'devise/sessions'
+      empty_breadcrumbs
+    end
+
     ### HOMEPAGE BREADCRUMBS
     if controller == 'home'
       # reset if home page
@@ -19,13 +23,13 @@ module NavigationHelper
       # PII/PHI
       if action == 'pii_phi_information'
         empty_breadcrumbs
-        session[:breadcrumbs] << { 'display': 'PII/PHI Information', 'path': '' }
+        session[:breadcrumbs] << { 'display': 'PII/PHI Information', 'path': pii_phi_information_path }
       end
 
       # Diffusion map
       if action == 'diffusion_map'
         empty_breadcrumbs
-        session[:breadcrumbs] << { 'display': 'Map of diffusion', 'path': '' }
+        session[:breadcrumbs] << { 'display': 'Map of diffusion', 'path': diffusion_map_path }
       end
     end
 
@@ -43,7 +47,7 @@ module NavigationHelper
     end
 
     def add_practice_breadcrumb(practice)
-      session[:breadcrumbs] << { 'display': practice.name, 'path': practice_path(practice) }.stringify_keys
+      session[:breadcrumbs] << { 'practice': 'true', 'display': practice.name, 'path': practice_path(practice) }.stringify_keys
     end
 
     def add_checklist_breadcrumb(practice)
@@ -62,13 +66,13 @@ module NavigationHelper
         session[:breadcrumbs] << {'display': 'Practices', 'path': '/practices'}
       end
 
-      search_breadcrumb = session[:breadcrumbs].find { |bc| bc['display'] == 'Search' || bc[:display] == 'Search' }
+      search_breadcrumb = session[:breadcrumbs].find { |bc| bc['display'] == 'Search' || bc[:display] == 'Search' } || nil
       url = URI::parse(request.referer || '')
 
       # search page
       if action == 'search'
-        remove_breadcrumb(search_breadcrumb) if search_breadcrumb.present?
-        session[:breadcrumbs] << {'display': 'Search', 'path': ''}
+        empty_breadcrumbs
+        session[:breadcrumbs] << {'display': 'Search', 'path': search_path}
       end
 
       # add the search breadcrumb if there is a search query going to the practice page
@@ -81,7 +85,14 @@ module NavigationHelper
 
       # practice path
       if action == 'show'
+        # Failsafe in the event a user is on the search page but travels to a practice page via the url bar
+        if search_breadcrumb.present? && search_breadcrumb['path'] == ''
+          search_breadcrumb['path'] = search_path
+        end
+
         if practice_breadcrumb(practice_by_id).blank?
+          # remove first practice if there is more than one in the breadcrumbs (to prevent too many crumbs)
+          session[:breadcrumbs].delete_if { |bc| bc['practice'].present? }
           add_practice_breadcrumb(practice_by_id)
           # If there are any duplicate breadcrumbs, delete them
         elsif practice_breadcrumb(practice_by_id).present? && practice_breadcrumb(practice_by_id).count > 1
@@ -163,32 +174,34 @@ module NavigationHelper
     end
 
     ### PRACTICE PARTNER BREADCRUMBS
+    def add_partners_breadcrumb
+      session[:breadcrumbs] << { 'display': 'Partners', 'path': practice_partners_path }
+    end
+
     if controller == 'practice_partners'
       # practice partners path
       if action == 'index'
-        # empty the bread crumbs and start a new 'path'
+        # empty the breadcrumbs and start a new 'path'
         empty_breadcrumbs
-        session[:breadcrumbs] << { 'display': 'Partners', 'path': practice_partners_path }
+        add_partners_breadcrumb
       end
 
       # practice partner show path
       if action == 'show'
+        empty_breadcrumbs
         # add the practice partner to the crumbs if it's not there already
         @practice_partner = PracticePartner.friendly.find(params[:id])
-        practice = Practice.find_by(slug: session[:user_return_to].split('/').pop) if session[:user_return_to].present?
         partner_breadcrumb = session[:breadcrumbs].find { |b| b['display'] == @practice_partner.name }
-        add_partner_breadcrumb = session[:breadcrumbs] << { 'display': @practice_partner.name, 'path': practice_partner_path(@practice_partner) }
 
-        if practice.present? && practice_breadcrumb(practice).blank?
-          add_practice_breadcrumb(practice)
-        end
 
         if partner_breadcrumb.blank?
-          add_partner_breadcrumb
+          add_partners_breadcrumb
+          session[:breadcrumbs] << { 'display': @practice_partner.name, 'path': practice_partner_path(@practice_partner) }
           # If there are any duplicate practice partner name breadcrumbs, delete them
         elsif partner_breadcrumb.present? && partner_breadcrumb.count > 1
           remove_breadcrumb(partner_breadcrumb)
-          add_partner_breadcrumb
+          add_partners_breadcrumb
+          session[:breadcrumbs] << { 'display': @practice_partner.name, 'path': practice_partner_path(@practice_partner) }
         end
       end
     end
@@ -197,23 +210,24 @@ module NavigationHelper
     if controller == 'page'
       if action == 'show'
         @page_slug = params[:page_slug] ? params[:page_slug] : 'home'
-        @page = Page.includes(:page_group).find_by(slug: @page_slug.downcase, page_groups: {slug: params[:page_group_friendly_id].downcase})
-        @builder_landing_page = Page.where(slug: 'home', page_group_id: @page.page_group_id)
+        @page = Page.includes(:page_group).find_by(slug: @page_slug.downcase, page_groups: {slug: params[:page_group_friendly_id].downcase}) || nil
+        @builder_landing_page = Page.where(slug: 'home', page_group_id: @page.page_group_id) || nil
+        @builder_page_path = "/#{params[:page_group_friendly_id]}/#{@page_slug}"
 
         def add_landing_page_breadcrumb(path)
           session[:breadcrumbs] << { 'display': "#{@page.page_group.name}", 'path': path }
         end
 
         def add_sub_page_breadcrumb
-          session[:breadcrumbs] << { 'display': "#{@page.title}", 'path': '' }
+          session[:breadcrumbs] << { 'display': "#{@page.title}", 'path': @builder_page_path }
         end
 
         if @page_slug == 'home'
           empty_breadcrumbs
-          add_landing_page_breadcrumb('')
+          add_landing_page_breadcrumb(@builder_page_path)
         elsif @builder_landing_page.exists?
           empty_breadcrumbs
-          add_landing_page_breadcrumb("/#{@page.page_group.slug}")
+          add_landing_page_breadcrumb("/#{params[:page_group_friendly_id]}")
           add_sub_page_breadcrumb
         else
           empty_breadcrumbs
@@ -239,25 +253,25 @@ module NavigationHelper
     if controller == 'users'
       if action == 'show'
         empty_breadcrumbs
-        session[:breadcrumbs] << { 'display': 'Profile', 'path': '' }
+        session[:breadcrumbs] << { 'display': 'Profile', 'path': user_path(current_user) } if current_user.present?
       end
 
       if action == 'edit_profile' && current_user.present?
         empty_breadcrumbs
         add_profile_breadcrumb
-        session[:breadcrumbs] << { 'display': 'Edit', 'path': '' }
+        session[:breadcrumbs] << { 'display': 'Edit', 'path': edit_profile_path } if current_user.present?
       end
 
       if action == 'relevant_to_you'
         empty_breadcrumbs
-        session[:breadcrumbs] << { 'display': 'Relevant to you', 'path': '' }
+        session[:breadcrumbs] << { 'display': 'Relevant to you', 'path': relevant_to_you_path } if current_user.present?
       end
     end
 
     ### NOMINATE PRACTICE BREADCRUMBS
     if action == 'index' && controller == 'nominate_practices'
       empty_breadcrumbs
-      session[:breadcrumbs] << { 'display': 'Nominate a practice', 'path': '' }
+      session[:breadcrumbs] << { 'display': 'Nominate a practice', 'path': nominate_a_practice_path }
     end
   end
 end
