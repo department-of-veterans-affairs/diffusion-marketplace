864d407df8ac42743a3299b7d7407bd7db8f8ab6
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2020-12-02 12:50:10 -0800

    dm-2222
    we want to use last_updated for the view not 'practice_page_updated' bc Practices that have not been updated prior to the implementation will display as null until they update they're pages - and that will cause confusion.  Were still doing the right thing by updating the 'practice_pages_updated' field to trigger the updated_at - so will work fine.

--- app/admin/practices.rb
+++ app/admin/practices.rb
@@ -27,7 +27,7 @@ ActiveAdmin.register Practice do
     column(:owner_email) {|practice| practice.user&.email}
     column :enabled unless params[:scope] == "get_practice_owner_emails"
     column :created_at unless params[:scope] == "get_practice_owner_emails"
-    column 'Last Updated', :practice_pages_updated unless params[:scope] == "get_practice_owner_emails"
+    column 'Last Updated', :updated_at unless params[:scope] == "get_practice_owner_emails"
     actions do |practice|
       practice_enabled_action_str = practice.enabled ? "Disable" : "Enable"
       item practice_enabled_action_str, enable_practice_admin_practice_path(practice), method: :post
