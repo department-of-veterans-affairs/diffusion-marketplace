257228efafb9f9f99eb4fb34cf2424e2c9cbca51
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2021-02-03 15:21:29 -0800

    dm-2290
    session monitoring.

--- app/models/practice_editor_session.rb
+++ app/models/practice_editor_session.rb
@@ -65,10 +65,13 @@ class PracticeEditorSession < ApplicationRecord
   end
   def self.monitor_session(practice_id, user_id, thread)
     ctr = 0
-    rec_id = PracticeEditorSession.where(practice_id: practice_id, user_id: user_id, session_end_time: nil).order("session_start_time DESC").first()
+    session_rec = PracticeEditorSession.where(practice_id: practice_id, user_id: user_id, session_end_time: nil).order("session_start_time DESC").first()
+    session_start = session_rec.session_start_time
     loop do
-      sleep 5
-      puts 'rec_id: ' + rec_id.id.to_s
+      sleep 8
+      diff = minutes_in_session(session_start)
+      puts diff.to_s
+      puts 'rec_id: ' + session_rec.id.to_s
       puts 'thread_id: ' + Thread.current.object_id.to_s
       puts practice_id.to_s + ", " + user_id.to_s
       ctr += 1
@@ -78,5 +81,9 @@ class PracticeEditorSession < ApplicationRecord
         Thread.exit
       end
     end
+  end
+  def self.minutes_in_session(session_start_time)
+    minutes = ((DateTime.now - session_start_time.to_datetime) * 24 * 60).to_i
+    return minutes
   end
 end
