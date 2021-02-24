4071d6e1fed7fc0492436ffc421483d92ae29ac4
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2021-02-23 16:00:29 -0800

    dm-2290
    update user email addresses to va.gov

--- spec/features/practice_viewer/metrics_spec.rb
+++ spec/features/practice_viewer/metrics_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'Metrics section', type: :feature, js: true do
   before do
     @admin = User.create!(email: 'toshiro.hitsugaya@soulsociety.com', password: 'Password123', password_confirmation: 'Password123', skip_va_validation: true, confirmed_at: Time.now, accepted_terms: true)
-    @user1 = User.create!(first_name: 'Fred', last_name: 'Smalls', email: 'hisagi.shuhei@soulsociety.com', password: 'Password123', password_confirmation: 'Password123', skip_va_validation: true, confirmed_at: Time.now, accepted_terms: true)
+    @user1 = User.create!(first_name: 'Fred', last_name: 'Smalls', email: 'hisagi.shuhei@va.gov', password: 'Password123', password_confirmation: 'Password123', skip_va_validation: true, confirmed_at: Time.now, accepted_terms: true)
     @user1.add_role(User::USER_ROLES[0].to_sym)
-    @user2 = User.create!(email: 'momo.hinamori@soulsociety.com', first_name: 'Momo', last_name: 'H', password: 'Password123', password_confirmation: 'Password123', skip_va_validation: true, confirmed_at: Time.now, accepted_terms: true)
-    @practice = Practice.create!(name: 'A public practice', approved: true, published: true, tagline: 'Test tagline', support_network_email: 'test@va.gov', user: @admin)
+    @user2 = User.create!(email: 'momo.hinamori@va.gov', first_name: 'Momo', last_name: 'H', password: 'Password123', password_confirmation: 'Password123', skip_va_validation: true, confirmed_at: Time.now, accepted_terms: true)
+    @practice = Practice.create!(name: 'A public practice', approved: true, published: true, tagline: 'Test tagline', support_network_email: 'test@test.com', user: @admin)
     @practice_partner = PracticePartner.create!(name: 'Diffusion of Excellence', short_name: '', description: 'The Diffusion of Excellence Initiative', icon: 'fas fa-heart', color: '#E4A002')
     @practice_email = PracticeEmail.create!(practice: @practice, address: 'test2@test.com')
   end
