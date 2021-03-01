96df5140ec756d01d8ae587cb7c6a3ee9b881eb7
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2021-03-01 12:10:46 -0800

    md-2290
    unit test - 'should not allow a user to edit a practice that is locked'.

--- spec/features/practice_editor/session_spec.rb
+++ spec/features/practice_editor/session_spec.rb
@@ -9,12 +9,32 @@ describe 'Practice editor sessions', type: :feature, js: true do
     @user_2.add_role(User::USER_ROLES[0].to_sym)
   end
 
-  it 'should not allow a user to edit practice if practice is locked for editing' do
+  it 'should not allow a user to edit a practice that is locked' do
+    #intorduction page.
     login_as(@user, :scope => :user, :run_callbacks => false)
     visit practice_introduction_path(@practice)
     logout(@user)
     login_as(@user_2, :scope => :user, :run_callbacks => false)
+    locked_msg = 'You cannot edit this practice since it is currently being edited by satoru.gojo@va.gov'
     visit practice_introduction_path(@practice)
-    expect(page).to have_content('You cannot edit this practice since it is currently being edited by satoru.gojo@va.gov')
+    expect(page).to have_content(locked_msg)
+
+    visit practice_adoptions_path(@practice)
+    expect(page).to have_content(locked_msg)
+
+    visit practice_editors_path(@practice)
+    expect(page).to have_content(locked_msg)
+
+    visit practice_implementation_path(@practice)
+    expect(page).to have_content(locked_msg)
+
+    visit practice_overview_path(@practice)
+    expect(page).to have_content(locked_msg)
+
+    visit practice_contact_path(@practice)
+    expect(page).to have_content(locked_msg)
+
+    visit practice_about_path(@practice)
+    expect(page).to have_content(locked_msg)
   end
 end
