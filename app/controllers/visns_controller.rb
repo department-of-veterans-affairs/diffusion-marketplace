e3295f13ab9f8fa8c4b4d8ea4bf0c092e3d2b7ad
Joshua Drumm <joshua.drumm@agile6.com>
2021-04-12 16:39:02 -0400

    DM-2361: Adding js to VISN search

--- app/controllers/visns_controller.rb
+++ app/controllers/visns_controller.rb
@@ -31,7 +31,6 @@ class VisnsController < ApplicationController
 
   def show
     @primary_visn_liaison = VisnLiaison.find_by(visn: @visn, primary: true)
-
     @visn_va_facilities = @visn.va_facilities
 
     @visn_va_facility_markers = Gmaps4rails.build_markers(@visn_va_facilities) do |facility, marker|
@@ -54,6 +53,38 @@ class VisnsController < ApplicationController
 
       marker.infowindow render_to_string(partial: 'visns/maps/show_infowindow', locals: { va_facility: va_facility })
     end
+
+    searchable_practices = Practice.searchable_practices
+
+    practices_created_by_visn = []
+    searchable_practices.each do |p|
+      origin_facilities = p.practice_origin_facilities
+      initiating_facility = p.initiating_facility
+      # add practices that have practice_origin_facilities
+      if p.facility? && origin_facilities.any?
+        @visn_va_facilities.each do |vaf|
+          origin_facilities.each do |of|
+            practices_created_by_visn << p if of.facility_id === vaf.station_number.to_s
+          end
+        end
+      # add practices that have an initiating_facility
+      elsif p.visn? && initiating_facility.present?
+        practices_created_by_visn << p if initiating_facility === @visn.id.to_s
+      end
+    end
+
+    @practices_created_json = practices_json(practices_created_by_visn)
+
+    practices_adopted_by_visn = []
+    searchable_practices.each do |p|
+      @visn_va_facilities.each do |vaf|
+        p.diffusion_histories.each do |dh|
+          practices_adopted_by_visn << p if dh.facility_id === vaf.station_number.to_s
+        end
+      end
+    end
+
+    @practices_adopted_json = practices_json(practices_adopted_by_visn)
   end
 
   private
