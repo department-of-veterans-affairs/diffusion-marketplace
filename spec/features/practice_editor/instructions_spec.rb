49c61dbcba435226a60d5c04dd48a39aea80c152
Joshua Drumm <joshua.drumm@agile6.com>
2021-04-15 14:01:25 -0400

    DM-2483: Updated PII/PHI form links

--- spec/features/practice_editor/instructions_spec.rb
+++ spec/features/practice_editor/instructions_spec.rb
@@ -23,9 +23,8 @@ describe 'Practice editor', type: :feature, js: true do
             expect(page).to have_no_content('Save')
             expect(page).to have_no_content('Publish practice')
             expect(page).to have_link(href: pii_phi_information_path)
-            expect(page).to have_link(href: "https://vaww.rtp.portal.va.gov/DEAN/IE/DOE/10-3203.pdf")
-            expect(page).to have_link(href: "https://vaww.rtp.portal.va.gov/DEAN/IE/DOE/10-3203.pdf")
-            expect(page).to have_link(href: "https://vaww.rtp.portal.va.gov/DEAN/IE/DOE/10-5345.pdf")
+            expect(page).to have_link(href: "https://vaww.va.gov/vaforms/medical/pdf/VA%20Form%2010-3203%20Fill.pdf")
+            expect(page).to have_link(href: "https://vaww.va.gov/vaforms/medical/pdf/VHA%20Form%2010-5345%20Fill-revision.pdf")
             expect(page).to have_link(href: practice_path('project-happen'))
         end
     end
