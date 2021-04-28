03c1a5e8104520b404371ea4b6728d69c6154ea4
Joshua Drumm <joshua.drumm@agile6.com>
2021-04-28 19:56:39 -0400

    DM-2361: Fixed breadcrumb for visn show page when a query is present

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -54,6 +54,10 @@ module NavigationHelper
       session[:breadcrumbs].slice!(session[:breadcrumbs].index(crumb))
     end
 
+    def add_visn_index_breadcrumb
+      session[:breadcrumbs] << { 'display': 'VISNs', 'path': visns_path }
+    end
+
     ### PRACTICE BREADCRUMBS
     if controller == 'practices'
       if action == 'index'
@@ -80,6 +84,17 @@ module NavigationHelper
         empty_breadcrumbs
         search_breadcrumb['path'] = "#{url.path}?#{url.query}" if search_breadcrumb.present?
         session[:breadcrumbs] << {'display': 'Search', 'path': "#{url.path}?#{url.query}"}
+        add_practice_breadcrumb(practice_by_id)
+      end
+
+      # add the VISN search query to breadcrumb if there is a VISN search query going to the practice page
+      if action == 'show' && url.path.include?('visns') && (url.query.present? && url.query.include?('query='))
+        visn_number = session[:breadcrumbs].last['path'].split('/').pop
+        visn_show_breadcrumb = visn_number.to_i.between?(1,23) || nil
+
+        empty_breadcrumbs
+        add_visn_index_breadcrumb
+        session[:breadcrumbs] << {'display': visn_number, 'path': "#{url.path}?#{url.query}"} if visn_show_breadcrumb
         add_practice_breadcrumb(practice_by_id)
       end
 
@@ -291,10 +306,6 @@ module NavigationHelper
     end
 
     ### VISN BREADCRUMBS
-    def add_visn_index_breadcrumb
-      session[:breadcrumbs] << { 'display': 'VISNs', 'path': visns_path }
-    end
-
     def visn_by_number
       Visn.find_by!(number: params[:number])
     end
