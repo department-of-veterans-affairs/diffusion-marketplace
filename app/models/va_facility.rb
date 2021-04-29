e92cff08cee1045d3e99df95daed7f48fced1f36
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2021-04-29 14:42:00 -0700

    dm-2357
    refactor sql for maturity level

--- app/models/va_facility.rb
+++ app/models/va_facility.rb
@@ -37,6 +37,8 @@ class VaFacility < ApplicationRecord
   def self.get_adoptions_by_facility_and_keyword(station_number, key_word)
     search_term = key_word
     key_word = "%" + key_word.downcase + "%"
+    maturity_level = get_maturity_level(search_term)
+
     sql = "SELECT distinct p.id, p.name, p.summary, p.slug, dh.facility_id, dhs.status, dhs.start_time,
           (select count(*) from diffusion_histories where p.id = diffusion_histories.practice_id) adoptions
           FROM practices p
@@ -48,22 +50,19 @@ class VaFacility < ApplicationRecord
           WHERE p.published = true AND p.enabled = true AND p.approved = true AND dh.facility_id = $1
           AND (p.name ilike ($2) OR p.description ilike ($2) OR p.short_name ilike ($2) OR p.summary ilike ($2) OR p.tagline ilike ($2)
           OR p.overview_problem ilike ($2) OR p.overview_solution ilike ($2) OR p.overview_results ilike ($2) "
-    if search_term.downcase == "emerging"
-      sql += "OR p.maturity_level = 0 "
-    elsif search_term.downcase == "replicate"
-      sql += "OR p.maturity_level = 1 "
-    elsif search_term.downcase == "scale"
-      sql += "OR p.maturity_level = 2 "
+    if !maturity_level.nil?
+      sql += "OR p.maturity_level = ($3) "
     end
     sql += "OR '" + search_term + "' ilike any(c.related_terms) "
     sql += "OR c.name ilike ($2) OR vaf.official_station_name ilike ($2) "
     sql += "OR vaf.common_name ilike ($2)) order by adoptions desc"
-    ActiveRecord::Base.connection.exec_query(sql, "SQL", [[nil, "#{station_number}"], [nil, "#{key_word}"]]).to_a
+    ActiveRecord::Base.connection.exec_query(sql, "SQL", [[nil, "#{station_number}"], [nil, "#{key_word}"], [nil, maturity_level]]).to_a
   end
 
   def self.get_adoptions_by_facility_category_and_keyword(station_number, category_id, key_word)
     search_term = key_word
     key_word = "%" + key_word.downcase + "%"
+    maturity_level = get_maturity_level(search_term)
     sql = "SELECT distinct p.id, p.name, p.summary, p.slug, dh.facility_id, dhs.status, dhs.start_time,
           (select count(*) from diffusion_histories where p.id = diffusion_histories.practice_id) adoptions
           FROM practices p
@@ -75,17 +74,26 @@ class VaFacility < ApplicationRecord
           WHERE p.published = true AND p.enabled = true AND p.approved = true AND dh.facility_id = $1 AND c.id = $2
           AND (p.name ilike ($3) OR p.description ilike ($3) OR p.short_name ilike ($3) OR p.summary ilike ($3) OR p.tagline ilike ($3)
           OR p.overview_problem ilike ($3) OR p.overview_solution ilike ($3) OR p.overview_results ilike ($3) "
-        if search_term.downcase == "emerging"
-          sql += "OR p.maturity_level = 0 "
-        elsif search_term.downcase == "replicate"
-          sql += "OR p.maturity_level = 1 "
-        elsif search_term == "scale"
-          sql += "OR p.maturity_level = 2 "
+        if !maturity_level.nil?
+          sql += "OR p.maturity_level = ($4) "
         end
         sql += "OR '" + search_term + "' ilike any(c.related_terms) "
         sql += "OR c.name ilike ($3) OR vaf.official_station_name ilike ($3) "
         sql += "OR vaf.common_name ilike ($3)) order by adoptions desc"
-    ActiveRecord::Base.connection.exec_query(sql, "SQL", [[nil, "#{station_number}"], [nil, "#{category_id}"], [nil, "#{key_word}"],]).to_a
+    ActiveRecord::Base.connection.exec_query(sql, "SQL", [[nil, "#{station_number}"], [nil, "#{category_id}"], [nil, "#{key_word}"], [nil, maturity_level]]).to_a
+  end
+
+  def self.get_maturity_level(search_term)
+    maturity_level = nil
+    search_term = search_term.downcase
+    if search_term.include?("emerging")
+      maturity_level = 0
+    elsif search_term.include?("replicate")
+      maturity_level = 1
+    elsif search_term.include?("scale")
+      maturity_level = 2
+    end
+    maturity_level
   end
 
   def self.rewrite_practices_adopted_at_this_facility_filtered_by_category(adoptions_at_facility, total_adoptions_for_practice)
@@ -96,6 +104,25 @@ class VaFacility < ApplicationRecord
         ret_val += '<tr>'
         ret_val += '<th scope="row" role="rowheader">'
         ret_val += '<a class="dm-internal-link" href="/practices/' + ad["slug"] + '"> ' + ad["name"] + '</a>'
+
+
+
+        #<a class="dm-internal-link" href="<%= practice_path(ad["slug"]) %>"><%=ad["name"]%></a>
+
+        # if @current_user
+
+
+        # ret_val += '<a href="<%= practice_favorite_path(' + ad["id"].to_s + ', format: :js) %>" data-method="post" data-remote="true" rel="nofollow" aria-label="Bookmark <%= @practice.name %>" tabindex="-1" aria-hidden="true" class="dm-favorite-practice-link dm-icon-link text-no-underline font-sans-xs text-primary desktop:grid-col-3 grid-col-6 desktop:margin-bottom-0 margin-bottom-2">'
+        # ret_val += '<i class="<%= current_user.favorite_practice_ids.include?(' + ad["id"].to_s + ' ? "fas" : "far" %> fa-bookmark dm-favorite-icon-<%= @practice.id %>  margin-right-05"></i>'
+        # ret_val += '</a>'
+
+        # <a href="<%= practice_favorite_path(@practice, format: :js) %>" data-method="post" data-remote="true" rel="nofollow" aria-label="Bookmark <%= @practice.name %>" tabindex="-1" aria-hidden="true" class="dm-favorite-practice-link dm-icon-link text-no-underline font-sans-xs text-primary desktop:grid-col-3 grid-col-6 desktop:margin-bottom-0 margin-bottom-2">
+        # <i class="<%= current_user.favorite_practice_ids.include?(@practice.id) ? 'fas' : 'far '%> fa-bookmark dm-favorite-icon-<%= @practice.id %>  margin-right-05"></i>
+        #   <span class="dm-favorite-practice-link-text">
+        #     <%= current_user.favorite_practice_ids.include?(@practice.id) ? 'Bookmarked' : 'Bookmark' %>
+        #   </span>
+        # </a>
+
         ret_val += '<br />' + ad["summary"] + '</th>'
         ret_val += '<td data-sort-value='  + ad["status"] + '>' + ad["status"] + '</td>'
         ret_val += '<td data-sort-value='  + start_date + '>' + start_date + '</td>'
