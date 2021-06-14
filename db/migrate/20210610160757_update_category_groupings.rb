001afd9ad3e8e8c6f59be54e9fe646aa3dcab031
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2021-06-14 15:18:10 -0700

    dm-2504

--- db/migrate/20210610160757_update_category_groupings.rb
+++ db/migrate/20210610160757_update_category_groupings.rb
@@ -6,9 +6,9 @@ class UpdateCategoryGroupings < ActiveRecord::Migration[5.2]
 
     strategy_id = strategic_rec.id
     clinical_rec = Category.find_by(name: 'Clinical')
-    clinical_id = clinical_rec.id
+    clinical_id = clinical_rec.id unless clinical_rec.blank?
     operational_rec = Category.find_by(name: 'Operational')
-    operational_id = operational_rec.id
+    operational_id = operational_rec.id unless operational_rec.blank?
 
     cur_others = Category.where(name: 'Other', parent_category_id: nil)
     if cur_others.blank?
@@ -28,11 +28,11 @@ class UpdateCategoryGroupings < ActiveRecord::Migration[5.2]
                    created_at: Time.now, updated_at: Time.now, is_other: false)
 
     Category.find_each do |cat|
-      if cat.name = "Access to Care" || "Age-Friendly" || "COVID-19" || "Employee Experience" || "Health Equity" || "High Reliability" || "Moving Forward" || "Suicide Prevention"
+      if cat.name == "Access to Care" || cat.name == "Age-Friendly" || cat.name == "COVID-19" || cat.name == "Employee Experience" || cat.name == "Health Equity" || cat.name == "High Reliability" || cat.name == "Moving Forward" || cat.name == "Suicide Prevention"
         cat.update(parent_category_id: strategy_id)
-      elsif cat.name = "Building Management" || "Contracting" || "Healthcare Administration" || "Information Technology" || "Logistics" || "Veterans Benefits" || "Workforce Development"
+      elsif cat.name == "Building Management" || cat.name == "Contracting" || cat.name == "Healthcare Administration" || cat.name == "Information Technology" || cat.name == "Logistics" || cat.name == "Veterans Benefits" || cat.name == "Workforce Development"
         cat.update(parent_category_id: operational_id)
-      elsif cat.name = "Anesthesiology" ||"Chaplain Care" || "Continuity of Care" || "Coordinated Care" || "COVID-19 Testing and Screening" || "Curbside Care" || "Dentistry" || "Dermatology" || "Emergency Care" || "Environmental Services" || "Ethics in Health Care" || "Geriatric Health" || "Home Health" || "Home Health" ||"Hospital Medicine" || "Inpatient Care" || "LGBT Health" || "Medication Management" || "Mental Health" || "Nephrology" || "Neurology" || "Neurology" || "Nursing" || "Nutrition & Food" || "Oncology" || "Ophthalmology" || "Outpatient Care" || "Pathology" || "Pharmacy" || "Primary Care" || "Prosthetic and Sensory Aids" || "Psychiatry" || "Pulmonary Care" || "Radiology" || "Rural Health" || "Specialty Care" || "Surgery" || "Telehealth" || "Urology" || "Veteran Experience" || "Whole Health" || "Women's Health"
+      elsif cat.name == "Anesthesiology" || cat.name == "Chaplain Care" || cat.name == "Continuity of Care" || cat.name == "Coordinated Care" || cat.name == "COVID-19 Testing and Screening" || cat.name == "Curbside Care" || cat.name == "Dentistry" || cat.name == "Dermatology" || cat.name == "Emergency Care" || cat.name == "Environmental Services" || cat.name == "Ethics in Health Care" || cat.name == "Geriatric Health" || cat.name == "Home Health" || cat.name == "Hospital Medicine" || cat.name == "Inpatient Care" || cat.name == "LGBT Health" || cat.name == "Medication Management" || cat.name == "Mental Health" || cat.name == "Nephrology" || cat.name == "Neurology" || cat.name == "Nursing" || cat.name == "Nutrition & Food" || cat.name == "Oncology" || cat.name == "Ophthalmology" || cat.name == "Outpatient Care" || cat.name == "Pathology" || cat.name == "Pharmacy" || cat.name == "Primary Care" || cat.name == "Prosthetic and Sensory Aids" || cat.name == "Psychiatry" || cat.name == "Pulmonary Care" || cat.name == "Radiology" || cat.name == "Rural Health" || cat.name == "Specialty Care" || cat.name == "Surgery" || cat.name == "Telehealth" || cat.name == "Urology" || cat.name == "Veteran Experience" || cat.name == "Whole Health" || cat.name == "Women's Health"
         cat.update(parent_category_id: clinical_id)
       end
     end
