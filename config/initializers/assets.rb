432c8c929bff847ba00c363c115364afa2a79a95
Joshua Drumm <34111449+JoshingYou1@users.noreply.github.com>
2021-06-25 11:15:43 -0400

    Dm 2639 (#360)
    * DM-2369: Added Veterans Crisis Line banner and modal to header
    
    * DM-2639: Finished mobile styling and added a unit test
    
    * DM-2639: Made modal link open in a new window
    
    * DM-2639: Removed some code that didn't belong on this branch
    
    Co-authored-by: Joshua Drumm <joshua.drumm@agile6.com>

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -43,6 +43,7 @@ Rails.application.config.assets.precompile += %w(
                                                   va_facilities/facility_created_practice_search.js
                                                   va_facilities/facility_adopted_practice_search.js
                                                   shared/_textarea_counter.js
+                                                  _header_utilities.js
                                               )
 
 # Precompile additional assets.
