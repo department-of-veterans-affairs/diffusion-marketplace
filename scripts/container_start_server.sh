32712d38e6246cd2a7ea3e2d308d760e0fda39ff
Aurora Hampton <aurora.hampton@agile6.com>
2021-08-06 09:40:13 -0700

    DM-DEVOPS: run containers as non-root user (#376)
    * run nginx as nginx user in docker container
    
    * nginx.conf permissions
    
    * copy public files app to nginx home
    
    * nginx user in application container
    
    * nginx is the owner of the app now
    
    * start app server with nginx as owner
    
    * uncomment va cert copy
    
    * recursive ownership for nginx
    
    * make nginx owner of proxy root
    
    * copy assets in start server script
    
    * script to start the app container with correct permissions
    
    * executable start script
    
    * redirect puma to existing log directory

--- scripts/container_start_server.sh
+++ scripts/container_start_server.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# This script starts the app within the container
+
 # sleep 999999   # if you need to debug the container before starting server uncomment this line
 
 if [ "$CONTAINER_ROLE" == "bg_worker" ]
@@ -7,7 +9,7 @@ then
 elif [ "$CONTAINER_ROLE" == "app_server" ]
 then
   echo "web start: bundle exec rails db:migrate && bundle exec puma -C config/puma.rb"
-  cp -R /app/public /var/www/
+  cp -R /home/nginx/app/public /home/nginx/www
   bundle exec rails db:migrate && bundle exec puma -C config/puma.rb
 else
   echo "Error: unknown CONTAINER_ROLE"
