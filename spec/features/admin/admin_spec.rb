bb7a1fec5c4c6be9f71b96545a9b9a9e96263f1a
elisa lee <emyl3@users.noreply.github.com>
2021-08-18 14:44:56 -0500

    DM-2664: Add sleep to admin_spec

--- spec/features/admin/admin_spec.rb
+++ spec/features/admin/admin_spec.rb
@@ -590,7 +590,7 @@ describe 'The admin dashboard', type: :feature do
     logout(@user2)
     login_as(@admin, :scope => :user, :run_callbacks => false)
     visit '/admin/practices/the-best-practice-ever/edit'
-    expect(page).to have_selector('#practice_user_input', visible: true)
+    sleep 0.2
     fill_in('practice_user_id', with: @user.email)
     click_button('Update Practice')
 
