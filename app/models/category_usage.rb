11f1e8e4737ab728d01f18c90425cda05d0cff1a
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2021-08-24 10:43:26 -0700

    dm-2800
    match query text to categories...

--- app/models/category_usage.rb
+++ app/models/category_usage.rb
@@ -3,6 +3,7 @@ class CategoryUsage < ApplicationRecord
 
 
   def self.store_chosen_categories(s_query, chosen_categories)
+    match_search_query_to_categories s_query unless s_query.blank?
     cat_ids = get_chosen_category_ids(chosen_categories)
     cat_ids.each do |cat_id|
       CategoryUsage.create(category_id: cat_id)
@@ -17,5 +18,26 @@ class CategoryUsage < ApplicationRecord
       cat_ids << cat_rec.id unless cat_rec.blank?
     end
     cat_ids
+  end
+
+  def self.match_search_query_to_categories(s_query)
+    debugger
+    s_query = s_query.downcase
+    cats = []
+    categories = Category.all
+    categories.each do |cat_name|
+      cats << cat_name.name.downcase!
+    end
+    debugger
+    if !cats.blank?
+      cats.each do |internal_cat|
+        if internal_cat != nil
+          if s_query.include? internal_cat
+            debugger
+          end
+        end
+      end
+    end
+    debugger
   end
 end
