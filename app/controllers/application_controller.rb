c7bcfa6d5412f640ec614729d9d2d5ffb5f6fe89
Joshua Drumm <joshua.drumm@agile6.com>
2021-08-24 16:08:36 -0400

    DM-2600: Still working on 'authenticate_ldap' fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -59,7 +59,7 @@ class ApplicationController < ActionController::Base
   def set_user_param
     if current_user.blank?
       # check to see if the user is using NTLM
-      logger.debug "REMOTE_USER: #{request.env["REMOTE_USER"]}"
+      logger.debug "request.env: #{request.env}"
       user = User.authenticate_ldap(request.env["REMOTE_USER"])
       # if so, log them in and set the user_type to 'ntlm' in the session
       if user.present?
