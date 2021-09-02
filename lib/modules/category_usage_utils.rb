0268f67193b4de9653a002377077408b0be0c1f8
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2021-09-01 17:11:43 -0700

    dm-2800

--- lib/modules/category_usage_utils.rb
+++ lib/modules/category_usage_utils.rb
@@ -1,7 +1,6 @@
 module CategoryUsageUtils
   def get_top_six_categories
     sql = "select cu.category_id, c.name, count(*) from category_usages cu join categories c on cu.category_id=c.id where cu.created_at >= (CURRENT_DATE - 90) group by cu.category_id, c.name order by count desc limit(6)"
-    records_array = ActiveRecord::Base.connection.execute(sql)
-    records_array
+    ActiveRecord::Base.connection.execute(sql)
   end
 end
