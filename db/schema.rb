57dba58bcc9f368de524d7662e07afa9824fa096
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2021-09-08 11:03:01 -0700

    dm-2858
    schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_165620) do
+ActiveRecord::Schema.define(version: 2021_09_03_160740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -964,7 +964,6 @@ ActiveRecord::Schema.define(version: 2021_08_17_165620) do
     t.integer "maturity_level"
     t.datetime "date_published"
     t.datetime "practice_pages_updated"
-    t.string "highlight_title"
     t.string "highlight_body"
     t.boolean "retired", default: false, null: false
     t.string "retired_reason"
@@ -1225,6 +1224,7 @@ ActiveRecord::Schema.define(version: 2021_08_17_165620) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "hidden", default: false, null: false
     t.index ["station_number"], name: "index_va_facilities_on_station_number", unique: true
     t.index ["visn_id"], name: "index_va_facilities_on_visn_id"
   end
