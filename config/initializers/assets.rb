84283d8343b87371d21533dbb9a076f52dfc1a0b
elisa lee <emyl3@users.noreply.github.com>
2021-09-21 15:53:47 -0500

    DM-2853: Implement session timeout and redirect (#391)
    * DM-2853: Implement session timeout and redirect
    
    * DM-2853: Check user session before updating practice editor session
    
    * DM-2853: Update session spec
    
    * DM-2853: Fix home map spec

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w(
+                                                  session_timeout_poller.js
                                                   _visnSelect.js
                                                   _officeSelect.js
                                                   ie.js
