79b4827b3b22c05e721d1d8e3726c87622ac7231
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2021-09-21 17:15:14 -0700

    dm-2820
    revert back to version that looks like it worked.

--- spec/features/admin/admin_spec.rb
+++ spec/features/admin/admin_spec.rb
@@ -599,9 +599,9 @@ describe 'The admin dashboard', type: :feature do
     logout(@user2)
     login_as(@admin, :scope => :user, :run_callbacks => false)
     visit '/admin/practices/the-best-practice-ever/edit'
-    fill_in('#practice_user_id', with: @user.email)
+    sleep 0.5
+    fill_in('practice_user_id', with: @user.email)
     click_button('Update Practice')
-
     expect(Practice.first.commontator_thread.subscribers).to include(@user, @user2)
   end
 end
