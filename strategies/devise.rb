795b867e6aaaf9520fdb6ec3a89ecf9d1176aebc
Joshua Drumm <joshua.drumm@agile6.com>
2021-09-30 13:56:09 -0400

    DM-2600: Swapped 'public' with 'guest' for some conditionals

--- strategies/devise.rb
+++ strategies/devise.rb
@@ -1,10 +1,7 @@
 Devise::Strategies::Authenticatable.class_eval do
-  include UserUtils
-
   private
-
   def valid_password?
-    return true if env["REMOTE_USER"].present?
+    return true if ENV['REMOTE_USER'].present?
     Devise::Encryptor.compare(self.class, encrypted_password, password)
   end
 end
