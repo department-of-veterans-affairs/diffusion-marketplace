79352f561272316768f7501f641570b8f537ae02
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2021-10-05 09:06:52 -0700

    dm-2858

--- spec/features/practice_viewer/adoption_accordions_spec.rb
+++ spec/features/practice_viewer/adoption_accordions_spec.rb
@@ -54,9 +54,9 @@ describe 'Adoption accordions', type: :feature, js: true do
     mobile_unsuccessful_text = 'Unsuccessful: Facilities that started but stopped working towards adoption.'
 
     def desktop_tooltip_expectation_flow(text, index)
-      expect(page).to have_selector('.usa-tooltip__body', text: text, visible: true)
+      expect(page).to have_selector('.usa-tooltip__trigger', text: text, visible: true)
       all('.usa-tooltip')[index].hover
-      expect(page).to have_selector('.usa-tooltip__body', text: text, visible: true)
+      expect(page).to have_selector('.usa-tooltip__trigger', text: text, visible: true)
     end
 
     def mobile_tooltip_expectation_flow(text, element_1, element_2)
