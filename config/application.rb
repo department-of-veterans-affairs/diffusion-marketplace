9dbe4903420156fb4139f207926fb46cb6e82ac6
elisa lee <emyl3@users.noreply.github.com>
2021-10-07 09:24:12 -0500

    DM-2927: Strip potentially forgeable headers in middleware

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,7 @@ module DiffusionMarketplace
 
     config.active_job.queue_adapter = :sidekiq
 
+    config.middleware.use HeadersFilter
     config.middleware.use NTLMAuthentication if ENV['USE_NTLM'] == 'true'
   end
 end
---
+++ lib/middleware/headers-filter.rb
@@ -0,0 +1,25 @@
+
+require 'rack'
+# modified from https://github.com/pusher/rack-headers_filter
+
+class HeadersFilter
+  HEADERS_TO_REMOVE = %w[
+    HTTP_X_FORWARDED_HOST
+    HTTP_X_FORWARDED_PORT
+    HTTP_X_FORWARDED_SCHEME
+    HTTP_X_FORWARDED_SSL
+  ]
+
+  attr_reader :remove_headers
+
+  def initialize(app)
+    @remove_headers = HEADERS_TO_REMOVE
+    @app = app
+  end
+
+  def call(env)
+    @remove_headers.each { |header| env.delete(header) }
+    env["HTTP_HOST"] = ENV['RAILS_ENV'] == 'production' ? ENV.fetch('HOSTNAME').split('//')[1] : 'localhost:3200'
+    @app.call(env)
+  end
+end
