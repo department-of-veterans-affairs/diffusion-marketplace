c7fc1989af3d540543ac85c82fe311abc34ddb13
elisa lee <emyl3@users.noreply.github.com>
2021-10-08 10:21:46 -0500

    DM-2924: Sanitize session timeout URLs (#402)
    * DM-2924: Sanitize session timeout URLs
    
    * DM-2924: Add admin dashboard route as valid internal path

--- app/controllers/session_timeout_controller.rb
+++ app/controllers/session_timeout_controller.rb
@@ -1,5 +1,6 @@
 # implementation source: https://medium.com/codex/user-session-inactivity-timeout-with-rails-and-devise-7269ac3a8213
 class SessionTimeoutController < Devise::SessionsController
+  include InternalUrlUtils
   prepend_before_action :skip_timeout, only: [:check_session_timeout, :render_timeout]
 
   def check_session_timeout
@@ -11,11 +12,21 @@ class SessionTimeoutController < Devise::SessionsController
     redirect_pathname = params[:path]
     reset_session
     sign_out(current_user)
-    url_text = redirect_pathname === '/' ? nil : 'Return to where you left off.'
-    redirect_to root_path, :flash => { alert: 'Your session has expired.', url: redirect_pathname, url_text: url_text }
+    hide_redirect_path = redirect_pathname === '/' || !is_valid_path(redirect_pathname)
+    url_text = hide_redirect_path ? nil : 'Return to where you left off.'
+    url = hide_redirect_path ? nil : redirect_pathname
+    redirect_to root_path, :flash => { alert: 'Your session has expired.', url: url, url_text: url_text }
   end
 
   private
+  def is_valid_path(path)
+    begin
+      return true if validate_url(path)
+    rescue
+      return false
+    end
+  end
+
   def ttl_to_timeout
     return 0 if user_session.blank?
     Devise.timeout_in - (Time.now.utc - last_request_time).to_i
--- app/models/concerns/internal_url_validator.rb
+++ app/models/concerns/internal_url_validator.rb
@@ -1,23 +1,11 @@
 class InternalUrlValidator < ActiveModel::Validator
+  include InternalUrlUtils
+
   def validate(record)
     url = record.url
     begin
       if url.present?
-        # checks if url begins with a "/"
-        if url.chars.first != '/'
-          raise StandardError.new 'must begin with a "/"'
-        end
-
-        route = Rails.application.routes.recognize_path(record.url)
-
-        # checks if the record exists with the specified id
-        if route.present? && route[:id].present?
-          model = route[:controller].classify
-          db_record = model.constantize.find_by(slug: route[:id])
-          if db_record.nil?
-            raise StandardError.new 'not a valid URL'
-          end
-        end
+        validate_url(url)
       end
     rescue => e
       record.errors.add(:url, e.message)
