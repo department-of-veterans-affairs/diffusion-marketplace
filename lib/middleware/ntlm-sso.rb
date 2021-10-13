88039abc8444ed53d0f08d6508e85bc1d352fb74
elisa lee <emyl3@users.noreply.github.com>
2021-10-13 09:57:53 -0500

    DM-2921: Add additional public IPs (#406)

--- lib/middleware/ntlm-sso.rb
+++ lib/middleware/ntlm-sso.rb
@@ -9,7 +9,21 @@ class NTLMAuthentication
 
   def call(env)
     request = Rack::Request.new(env)
-    public_ips = ['10.236.28.71', '10.237.28.71', '10.238.28.71', '10.239.28.71']
+    public_ips = [
+      '10.236.28.70',
+      '10.237.28.70',
+      '10.238.28.70',
+      '10.239.28.70',
+      '10.236.28.71',
+      '10.237.28.71',
+      '10.238.28.71',
+      '10.239.28.71',
+      '10.236.28.72',
+      '10.237.28.72',
+      '10.238.28.72',
+      '10.239.28.72'
+    ]
+
     unless public_ips.include?(request.env["HTTP_X_FORWARDED_FOR"].split(',').first)
       auth = Rack::Auth::NTLMSSO.new(@app)
       return auth.call(env)
