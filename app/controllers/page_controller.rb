9dc2b5f03a6f82248dc644adbee39e7fb1cc8692
Joshua Drumm <joshua.drumm@agile6.com>
2021-10-15 12:43:36 -0400

    DM-2941: Refactored some code in page_controller

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -4,6 +4,8 @@ class PageController < ApplicationController
     @page = Page.includes(:page_group).find_by(slug: page_slug.downcase, page_groups: {slug: params[:page_group_friendly_id].downcase})
     @path_parts = request.path.split('/')
     @facilities_data = VaFacility.cached_va_facilities
-    redirect_to(root_path) unless @page.published
+    unless @page.published
+      redirect_to(root_path) unless current_user.has_role?(:admin)
+    end
   end
 end
