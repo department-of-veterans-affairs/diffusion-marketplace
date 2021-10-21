ed867985bfa5ce5e531e9c8e4ee5736d81cc1c5a
elisa lee <emyl3@users.noreply.github.com>
2021-10-21 10:32:14 -0500

    Fix search for public users (#416)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 
 
   def index
-    @practices = helpers.is_user_a_guest? ? Practice.searchable_practices('a_to_z', true) : Practice.searchable_practices('a_to_z', false)
+    @practices = helpers.is_user_a_guest? ? Practice.searchable_public_practices('a_to_z') : Practice.searchable_practices('a_to_z')
     @favorite_practices = current_user&.favorite_practices || []
     @highlighted_pr = Practice.where(highlight: true, published: true, enabled: true, approved: true).first
     @popular_categories = get_most_popular_categories
