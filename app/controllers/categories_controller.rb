c37b0cad39e0c41dd42de945e536c96ddb767cb9
Joshua Drumm <joshua.drumm@agile6.com>
2021-12-28 18:10:26 -0500

    DM-3092: Removed debugger

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,6 @@ class CategoriesController < ApplicationController
   include CategoriesHelper
 
   def update_category_usage
-    debugger
     query_val = params["query"]
     chosen_categories = params["chosenCategories"]
     cat_id = params["catId"]
