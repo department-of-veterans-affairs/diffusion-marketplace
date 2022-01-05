20bd4a5bca894491d2847a2e1909b0b8e0abba5c
elisa lee <emyl3@users.noreply.github.com>
2022-01-05 11:54:36 -0600

    DM-3090: Paginate partners page innovation cards

--- app/controllers/practice_partners_controller.rb
+++ app/controllers/practice_partners_controller.rb
@@ -11,6 +11,16 @@ class PracticePartnersController < ApplicationController
   # GET /practice_partners/1.json
   def show
     @facilities_data = VaFacility.cached_va_facilities.order_by_station_name
+    @partner_practices = helpers.is_user_a_guest? ? @practice_partner.practices.published_enabled_approved.public_facing.sort_by_retired.sort_a_to_z : @practice_partner.practices.published_enabled_approved.sort_by_retired.sort_a_to_z
+    @pagy_partner_practices, @paginated_partner_practices = pagy_array(
+      @partner_practices,
+      items: 12,
+      link_extra: "data-remote='true' class='paginated-partner-practices-page-#{params[:page].present? ? params[:page].to_i + 1 : 2}-link dm-button--outline-secondary margin-top-105 width-15'"
+    )
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
