90d9a7fa05d835fab86e3bd4c1b3a44fcb3fdeaf
Joshua Drumm <joshua.drumm@agile6.com>
2022-01-05 17:35:48 -0500

    DM-2998: Fixed a failing test

--- spec/features/admin/admin_categories_spec.rb
+++ spec/features/admin/admin_categories_spec.rb
@@ -60,7 +60,7 @@ describe 'Admin Dashboard Categories Tab', type: :feature do
       expect(cache_keys).not_to include("categories")
       add_categories_to_cache
       find('.search-filters-accordion-button').click
-      expect(page).to have_content('Completely update...')
+      expect(page).to have_content('Completely updated category')
     end
 
     it 'Should be reset if a new category is created through the admin panel' do
