539c2bb03731907d973acd1a5c3b1ad84e2b96f2
elisa lee <emyl3@users.noreply.github.com>
2022-01-10 11:36:29 -0600

    DM-3090: Paginate partners page innovation cards (#458)
    * DM-3090: Paginate partners page innovation cards
    
    * DM-3090: Fix test
    
    * DM-3090: Fix flakey test
    
    * DM-3090: Fix page builder card css issue
    
    * DM-3090: Use cached practices

--- app/controllers/practice_partners_controller.rb
+++ app/controllers/practice_partners_controller.rb
@@ -11,6 +11,16 @@ class PracticePartnersController < ApplicationController
   # GET /practice_partners/1.json
   def show
     @facilities_data = VaFacility.cached_va_facilities.order_by_station_name
+    @partner_practices = helpers.is_user_a_guest? ? @practice_partner.practices.searchable_public_practices : @practice_partner.practices.searchable_practices
+    @pagy_partner_practices, @paginated_partner_practices = pagy_array(
+      @partner_practices,
+      items: 12,
+      link_extra: "data-remote='true' class='paginated-partner-practices-page-#{params[:page].present? ? params[:page].to_i + 1 : 2}-link dm-button--outline-secondary margin-top-105 width-15'"
+    )
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
--- spec/features/practice_partners_spec.rb
+++ spec/features/practice_partners_spec.rb
@@ -8,31 +8,81 @@ describe 'Practice partners pages', type: :feature do
     @approver = User.create!(email: 'squidward.tentacles@bikinibottom.net', password: 'Password123', password_confirmation: 'Password123', skip_va_validation: true, confirmed_at: Time.now, accepted_terms: true)
     @admin.add_role(User::USER_ROLES[1].to_sym)
     @approver.add_role(User::USER_ROLES[0].to_sym)
-    @user_practice = Practice.create!(name: 'A public practice', approved: true, published: true, initiating_facility_type: 'other', user: @user2)
-    PracticePartnerPractice.create!(practice_partner: @pp, practice: @user_practice)
+    @pr_1 = Practice.create!(name: 'A public practice', approved: true, published: true, enabled: true, is_public: true, initiating_facility_type: 'other', user: @user2)
+    @pr_2 = Practice.create!(name: 'practice two', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    @pr_3 = Practice.create!(name: 'practice three', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    @pr_4 = Practice.create!(name: 'practice four', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    @pr_5 = Practice.create!(name: 'practice five', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    @pr_6 = Practice.create!(name: 'practice six', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    @pr_7 = Practice.create!(name: 'practice seven', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    @pr_8 = Practice.create!(name: 'practice eight', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    @pr_9 = Practice.create!(name: 'practice nine', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    @pr_10 = Practice.create!(name: 'practice ten', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    @pr_11 = Practice.create!(name: 'practice eleven', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    @pr_12 = Practice.create!(name: 'practice twelve', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    @pr_13 = Practice.create!(name: 'practice thirteen', approved: true, published: true, enabled: true, initiating_facility_type: 'other', user: @user2)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_1)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_2)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_3)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_4)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_5)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_6)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_7)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_8)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_9)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_10)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_11)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_12)
+    PracticePartnerPractice.create!(practice_partner: @pp, practice: @pr_13)
   end
 
-  it 'should navigate to strategic sponsors list page' do
-    visit '/partners'
-    # TODO: this is timing out in CI
-    # expect(page).to be_accessible.according_to :wcag2a, :section508
-    expect(current_path).to eq('/partners')
+  context 'for a logged in user' do
+    before do
+      login_as(@user, :scope => :user, :run_callbacks => false)
+      page.set_rack_session(:user_type => 'ntlm')
+    end
+
+    it 'should navigate to strategic sponsors list page' do
+      visit '/partners'
+      # TODO: this is timing out in CI
+      # expect(page).to be_accessible.according_to :wcag2a, :section508
+      expect(current_path).to eq('/partners')
+    end
+
+    it 'should show the initiating facility\'s name' do
+      @pr_1.update(initiating_facility: 'Foobar Facility')
+      visit '/partners/diffusion-of-excellence'
+      expect(page).to be_accessible.according_to :wcag2a, :section508
+      expect(page).to have_content(@pr_1.name)
+      expect(page).to have_content('Foobar Facility')
+    end
+
+    it 'should display the initiating facility\'s initiating facility property if it is not found in the map' do
+      @pr_1.update(initiating_facility: 'Test facility')
+      visit '/partners/diffusion-of-excellence'
+      expect(page).to have_content(@pr_1.name)
+      expect(page).to have_content('Test facility')
+    end
+
+    it 'should paginate the practice cards if there are more than 12 cards' do
+      visit '/partners/diffusion-of-excellence'
+      expect(page).to have_content('Load more')
+      pr_card_count = find_all('.dm-practice-card').size
+      expect(pr_card_count).to eq(12)
+      find('.paginated-partner-practices-page-2-link').click
+      expect(page).to have_content('practice two')
+      updated_pr_card_count = find_all('.dm-practice-card').size
+      expect(updated_pr_card_count).to eq(13)
+      expect(page).to have_no_content('Load more')
+    end
   end
 
-  it 'should show the initiating facility\'s name' do
-    @user_practice.update(initiating_facility: 'Foobar Facility')
-    visit '/partners/diffusion-of-excellence'
-    expect(page).to be_accessible.according_to :wcag2a, :section508
-    expect(page).to have_content(@user_practice.name)
-    expect(page).to have_content('Foobar Facility')
-  end
-
-  it 'should display the initiating facility\'s initiating facility property if it is not found in the map' do
-    @user_practice.update(initiating_facility: 'Test facility')
-    visit '/partners/diffusion-of-excellence'
-    expect(page).to be_accessible.according_to :wcag2a, :section508
-
-    expect(page).to have_content(@user_practice.name)
-    expect(page).to have_content('Test facility')
+  context 'for a public user' do
+    it 'should only display the public practice' do
+      visit '/partners/diffusion-of-excellence'
+      pr_card_count = find_all('.dm-practice-card').size
+      expect(pr_card_count).to eq(1)
+      expect(page).to have_content('A public practice')
+    end
   end
 end
