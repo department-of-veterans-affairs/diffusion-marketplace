1c2aad35e485842d8446ec80c3c8ff4fc31e4497
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-01-18 13:53:49 -0800

    dm-3011
    Should throw internal server error if visn number does not exist.

--- spec/features/crh_spec.rb
+++ spec/features/crh_spec.rb
@@ -2,13 +2,9 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe 'show page' do
-  it 'should be there if the VISN number exists in the DB' do
-    visit '/crh/1'
-    expect(page).to have_content("Department of Veterans Affairs")
-  end
-
   it 'should throw error if VISN number does not exist in the DB' do
     visit '/crh/50'
-    expect(page).to have_content("Couldn't find Visn")
+    expect(page).to have_content('Internal server error')
+    expect(page).to have_content('We\'re sorry, something went wrong. We\'re working to fix it as soon as we can. Please check back in 30 minutes.')
   end
 end
