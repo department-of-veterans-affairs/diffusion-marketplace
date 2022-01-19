e9b8d85b766b72d09d199245d4713aa09a23a180
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-01-19 10:17:46 -0800

    dm-3110
    mod unit test.

--- spec/features/crh_spec.rb
+++ spec/features/crh_spec.rb
@@ -4,21 +4,20 @@ require 'spec_helper'
 describe 'CRH pages', type: :feature do
 
   before do
-    def visit_crh_page s_url
-      visit s_url
-    end
+    @visn_1 = Visn.create!(name: 'visn_1', number: 1)
+    @crh = ClinicalResourceHub.create!(name: 'crh1', visn_id: @visn_1.id)
   end
   describe 'show page' do
     it 'should throw error if crh_path not defined' do
-      visit_crh_page '/crh'
+      visit clinical_resource_hub_path
       expect(page).to have_content('Page not found')
       expect(page).to have_content('We\'re sorry, we can\'t find the page you\'re looking for. It might have been removed, changed names, or is otherwise unavailable.')
     end
 
     if 'should throw error if visn_number not found in DB'
-      visit_crh_page '/crh/150'
-      expect(page).to have_content('Page not found')
-      expect(page).to have_content('We\'re sorry, we can\'t find the page you\'re looking for. It might have been removed, changed names, or is otherwise unavailable.')
+      visit clinical_resource_hub_path(@crh)
+      expect(page).to_not have_content('Page not found')
+      expect(page).to_not have_content('We\'re sorry, we can\'t find the page you\'re looking for. It might have been removed, changed names, or is otherwise unavailable.')
     end
   end
 end
