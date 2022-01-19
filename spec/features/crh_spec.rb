ad2373d46143616592cc0465cb6d783d8fbf35e5
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-01-19 11:06:21 -0800

    dm-3110
    update unit test.

--- spec/features/crh_spec.rb
+++ spec/features/crh_spec.rb
@@ -1,23 +1,26 @@
 require 'rails_helper'
-require 'spec_helper'
 
-describe 'CRH pages', type: :feature do
-
+describe 'Clinical_Resource_Hubs', type: :feature do
   before do
     @visn_1 = Visn.create!(name: 'visn_1', number: 1)
     @crh = ClinicalResourceHub.create!(name: 'crh1', visn_id: @visn_1.id)
   end
-  describe 'show page' do
-    it 'should throw error if crh_path not defined' do
-      visit clinical_resource_hub_path
-      expect(page).to have_content('Page not found')
-      expect(page).to have_content('We\'re sorry, we can\'t find the page you\'re looking for. It might have been removed, changed names, or is otherwise unavailable.')
+
+  describe 'CRH show page' do
+    describe 'bad route' do
+      it 'should show 404 error' do
+        visit '/crh/'
+        expect(page).to have_content('Page not found')
+        expect(page).to have_content('We\'re sorry, we can\'t find the page you\'re looking for. It might have been removed, changed names, or is otherwise unavailable.')
+      end
     end
 
-    if 'should throw error if visn_number not found in DB'
-      visit clinical_resource_hub_path(@crh)
-      expect(page).to_not have_content('Page not found')
-      expect(page).to_not have_content('We\'re sorry, we can\'t find the page you\'re looking for. It might have been removed, changed names, or is otherwise unavailable.')
+    describe 'route works' do
+      it 'should not show 404' do
+        visit '/crh/1'
+        expect(page).to_not have_content('Page not found')
+        expect(page).to_not have_content('We\'re sorry, we can\'t find the page you\'re looking for. It might have been removed, changed names, or is otherwise unavailable.')
+      end
     end
   end
 end
