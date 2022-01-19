06f948de7684f675ba56cf03df1d2342c3e9a9d6
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-01-18 16:57:21 -0800

    dm-3110

--- spec/features/crh_spec.rb
+++ spec/features/crh_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 require 'spec_helper'
 
-describe 'show page' do
-  it 'should throw error if crh_path not defined' do
-    visit '/crh'
-    expect(page).to have_content('Page not found')
-    expect(page).to have_content('We\'re sorry, we can\'t find the page you\'re looking for. It might have been removed, changed names, or is otherwise unavailable.')
-  end
+describe 'CRH pages', type: :feature do
+  describe 'show page' do
+    it 'should throw error if crh_path not defined' do
+      visit '/crh'
+      expect(page).to have_content('Page not found')
+      expect(page).to have_content('We\'re sorry, we can\'t find the page you\'re looking for. It might have been removed, changed names, or is otherwise unavailable.')
+    end
 
-  if 'should throw error if visn_number not found in DB'
-    visit '/crh/150'
-    expect(page).to have_content('Page not found')
-    expect(page).to have_content('We\'re sorry, we can\'t find the page you\'re looking for. It might have been removed, changed names, or is otherwise unavailable.')
+    if 'should throw error if visn_number not found in DB'
+      visit '/crh/150'
+      expect(page).to have_content('Page not found')
+      expect(page).to have_content('We\'re sorry, we can\'t find the page you\'re looking for. It might have been removed, changed names, or is otherwise unavailable.')
+    end
   end
 end
