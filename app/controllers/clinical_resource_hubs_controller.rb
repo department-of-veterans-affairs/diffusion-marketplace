e8c1142ac5e61a9d14edfffc52a13b45cad4590e
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-02-28 15:36:31 -0800

    dm-3111
    Category search combo box.

--- app/controllers/clinical_resource_hubs_controller.rb
+++ app/controllers/clinical_resource_hubs_controller.rb
@@ -1,11 +1,16 @@
 class ClinicalResourceHubsController < ApplicationController
   include PracticesHelper
+  include PracticeUtils
   include StatesHelper
   before_action :set_crh, only: [:show]
   def show
     visn_id = @visn.id
     visn_number = @visn.number
     @states_str = get_visn_associated_states_str(visn_id)
+    debugger
+    @practices_created_categories = []
+    practices_created_by_crh_count = PracticeOriginFacility.where(clinical_resource_hub_id: id).count
+    get_categories_by_practices(practices_created_by_crh_count, @practices_created_categories)
   end
 
   private
