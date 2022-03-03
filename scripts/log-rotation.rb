d95a2dd0d068774a1c174cf9b001828695e7e359
elisa lee <emyl3@users.noreply.github.com>
2022-03-03 16:44:56 -0600

    Update log rotation script (#476)

--- scripts/log-rotation.rb
+++ scripts/log-rotation.rb
@@ -3,15 +3,14 @@ require 'aws-sdk-s3'
 
 def log_rotation
   aws_client = Aws::S3::Client.new(region: 'us-gov-west-1')
-  File.open('/home/nginx/app/log/production.log', 'rb') do |file|
+  file = File.read('/home/nginx/app/log/production.log')
+  response = aws_client.put_object(body: file, bucket: ENV["DM_LOG_ROTATION_BUCKET"], key: "#{DateTime.now.strftime('%Y-%m-%d')}-production.log")
+  puts response
+  `> /home/nginx/app/log/production.log`
 
-    response = aws_client.put_object(body: file, bucket: ENV["DM_LOG_ROTATION_BUCKET"], key: "#{DateTime.now.strftime('%Y-%m-%d')}-production.log")
-    puts response
-    `> /home/nginx/app/log/production.log`
   rescue StandardError => e
     puts response
     puts "There was an error. #{e.message}"
-  end
 end
 
 log_rotation
