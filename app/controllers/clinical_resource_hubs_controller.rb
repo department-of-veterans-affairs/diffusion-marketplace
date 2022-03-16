e6cf36d950865f79933ce73a1af61e309f3ea1b7
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-03-16 11:38:21 -0700

    dim-3111

--- app/controllers/clinical_resource_hubs_controller.rb
+++ app/controllers/clinical_resource_hubs_controller.rb
@@ -36,7 +36,7 @@ class ClinicalResourceHubsController < ApplicationController
     sort_option = params[:sort_option] || 'a_to_z'
     search_term = params[:search_term] ? params[:search_term].downcase : nil
     categories = params[:categories] || nil
-    created_practices = helpers.is_user_a_guest? ? @crh.get_crh_created_innovations(@crh.id, search_term, sort_option, categories, true) : Practice.get_crh_created_innovations(@crh.id, search_term, sort_option, categories, false)
+    created_practices = helpers.is_user_a_guest? ? @crh.get_crh_created_innovations(@crh.id, search_term, sort_option, categories, true) : @crh.get_crh_created_innovations(@crh.id, search_term, sort_option, categories, false)
     @pagy_created_practices = pagy_array(
         created_practices,
         items: 3,
