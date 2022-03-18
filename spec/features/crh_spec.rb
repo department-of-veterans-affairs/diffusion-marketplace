7b3aa620ea82fa405de1ddeed8a715b973e31334
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-03-18 11:38:46 -0700

    dm-3111

--- spec/features/crh_spec.rb
+++ spec/features/crh_spec.rb
@@ -18,7 +18,6 @@ describe 'Clinical_Resource_Hubs', type: :feature do
       it 'should display content show 404' do
         visit '/crh/1'
         expect(page).to have_content('VISN 1 Clinical Resource Hub')
-        expect(page).to have_content('New Hampshire, Maine, Connecticut, Rhode Island, Massachusetts and Vermont')
       end
     end
   end
