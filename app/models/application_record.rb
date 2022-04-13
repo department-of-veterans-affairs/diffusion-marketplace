244674d959f65f8b8ebb659e86eae602251601f8
Joshua Drumm <joshua.drumm@agile6.com>
2022-04-13 17:40:44 -0400

    DM-3354: Attempting to return obj.url instead of presigned_url

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -10,15 +10,8 @@ class ApplicationRecord < ActiveRecord::Base
     signer.presigned_get_url(object_key: path)
   end
 
-  def object_presigned_url(obj, style = nil)
-    if Rails.env.test?
+  def object_presigned_url(obj)
       obj.url
-    else
-      # this presumes the object path is s3 ready that has a beginning forward slash
-      # need to take out first `/` from the path, `.sub` takes care of that
-      path = obj.path(style).sub('/', '')
-      s3_presigned_url(path)
-    end
   end
 
   def process_avatar_crop(crop_options)
