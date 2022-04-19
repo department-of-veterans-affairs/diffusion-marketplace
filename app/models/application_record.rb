c3bc532d2539be8b8208d522aa18db39bb1cfddf
elisa lee <emyl3@users.noreply.github.com>
2022-04-19 15:56:09 -0500

    DM-3231: Reduce s3_signer cache expiration time

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def signer
-    Rails.cache.fetch('s3_signer', expires_in: 30.minutes) do
+    Rails.cache.fetch('s3_signer', expires_in: 5.minutes) do
       s3_bucket = Aws::S3::Bucket.new(ENV['S3_BUCKET_NAME'])
       WT::S3Signer.for_s3_bucket(s3_bucket, expires_in: 84000)
     end
--- app/models/practice_resource.rb
+++ app/models/practice_resource.rb
@@ -1,12 +1,6 @@
 class PracticeResource < ApplicationRecord
   acts_as_list scope: :practice
   after_create :attachment_crop
-  before_save :clear_searchable_practices_cache_on_save
-  before_destroy :clear_searchable_practices_cache_on_destroy
-   after_save :reset_searchable_practices_cache
-  after_destroy :reset_searchable_practices_cache
-
-  attr_accessor :reset_cached_searchable_practices
 
   has_attached_file :attachment, styles: {thumb: '768x432>'}, :processors => [:cropper]
   before_post_process :skip_for_non_image
@@ -22,27 +16,6 @@ class PracticeResource < ApplicationRecord
 
   def attachment_s3_presigned_url(style = nil)
     object_presigned_url(attachment, style)
-  end
-
-  def clear_searchable_practices_cache
-    cache_keys = ["searchable_practices_json", "searchable_public_practices_json"]
-    cache_keys.each do |cache_key|
-      Rails.cache.delete(cache_key)
-    end
-  end
-
-  def clear_searchable_practices_cache_on_save
-    if self.changed?
-      self.reset_cached_searchable_practices = true
-    end
-  end
-
-  def clear_searchable_practices_cache_on_destroy
-    self.reset_cached_searchable_practices = true
-  end
-
-  def reset_searchable_practices_cache
-    clear_searchable_practices_cache if self.reset_cached_searchable_practices
   end
 
   private
