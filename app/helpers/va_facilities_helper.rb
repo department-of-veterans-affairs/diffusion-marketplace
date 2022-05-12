9d42352713150036a532cfa3a1fe6c3ea6f17d77
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-05-12 10:17:32 -0700

    dm-3373 crh-feature
    another small refactor

--- app/helpers/va_facilities_helper.rb
+++ app/helpers/va_facilities_helper.rb
@@ -6,8 +6,7 @@ module VaFacilitiesHelper
 
   def crh_adopted_practices_count(visn_id)
     crh = ClinicalResourceHub.find_by(visn_id: visn_id)
-    @practices_adopted_by_crh = crh.get_crh_adopted_practices(crh.id,  is_user_guest: is_user_a_guest?)
-    @practices_adopted_by_crh.count
+    crh.get_crh_adopted_practices(crh.id,  is_user_guest: is_user_a_guest?).count
   end
   def created_practices_count(id)
     is_user_a_guest? ? Practice.public_facing.get_by_created_facility(id).size : Practice.published_enabled_approved.get_by_created_facility(id).size
