df82f4282c096301a786544061d9f21ce604ec96
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-05-16 15:16:07 -0700

    dm-3833
    Make Contact us and Nominate available for public users again.

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -4,20 +4,14 @@ class AboutController < ApplicationController
 
   def email
     if params["phone"].to_s.length > 0
-      log_spam_attempt "About"
+      log_spam_attempt "Contact us"
       redirect_to root_path
     else
-      if helpers.is_user_a_guest?
-        respond_to do |format|
-          format.html { redirect_to root_path }
-        end
-      else
-        ContactUsMailer.send_contact_email(options = { email: params[:email], subject: params[:subject], message: params[:message] }, 'About', 'contact_us_mailer/contact_us_email').deliver_now
-        success = 'You successfully sent a message to the Diffusion Marketplace team.'
-        flash[:success] = success
-        respond_to do |format|
-          format.html { redirect_back fallback_location: root_path, notice: flash[:notice] }
-        end
+      ContactUsMailer.send_contact_email(options = { email: params[:email], subject: params[:subject], message: params[:message] }, 'About', 'contact_us_mailer/contact_us_email').deliver_now
+      success = 'You successfully sent a message to the Diffusion Marketplace team.'
+      flash[:success] = success
+      respond_to do |format|
+        format.html { redirect_back fallback_location: root_path, notice: flash[:notice] }
       end
     end
   end
--- app/controllers/nominate_practices_controller.rb
+++ app/controllers/nominate_practices_controller.rb
@@ -10,17 +10,11 @@ class NominatePracticesController < ApplicationController
       log_spam_attempt "Nominate"
       redirect_to root_path
     else
-      if helpers.is_user_a_guest?
-        respond_to do |format|
-          format.html { redirect_to root_path }
-        end
-      else
-        NominateAPracticeMailer.send_contact_email(options = { email: params[:email], subject: params[:subject], message: params[:message] }, 'Nominate', 'nominate_a_practice_mailer/nominate_a_practice_email').deliver_now
-        success = 'Message sent. The Diffusion Marketplace team will review your nomination.'
-        flash[:success] = success
-        respond_to do |format|
-          format.html { redirect_back fallback_location: root_path, notice: flash[:notice] }
-        end
+      NominateAPracticeMailer.send_contact_email(options = { email: params[:email], subject: params[:subject], message: params[:message] }, 'Nominate', 'nominate_a_practice_mailer/nominate_a_practice_email').deliver_now
+      success = 'Message sent. The Diffusion Marketplace team will review your nomination.'
+      flash[:success] = success
+      respond_to do |format|
+        format.html { redirect_back fallback_location: root_path, notice: flash[:notice] }
       end
     end
   end
