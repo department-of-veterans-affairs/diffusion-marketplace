7e3a99814e0d8b71fb8bb06dfaced6c3e94f4691
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-06-09 10:06:42 -0700

    dm-3453
    Fix test.

--- spec/features/practice_viewer/introduction_spec.rb
+++ spec/features/practice_viewer/introduction_spec.rb
@@ -138,12 +138,10 @@ describe 'Practice viewer - introduction', type: :feature, js: true do
       end
 
       it 'should provide a link for each origin VA facility or VISN that directs the user to the show page of that facility/VISN' do
-        new_window = window_opened_by { click_link('Palo Alto VA Medical Center-Menlo Park (Palo Alto-Menlo Park)') }
-        within_window new_window do
-          expect(page).to have_content('Palo Alto VA Medical Center-Menlo Park')
-          expect(page).to have_content('This facility has created')
-          expect(page).to have_content('Main number:')
-        end
+        click_link('Palo Alto VA Medical Center-Menlo Park (Palo Alto-Menlo Park)')
+        expect(page).to have_content('Palo Alto VA Medical Center-Menlo Park')
+        expect(page).to have_content('This facility has created')
+        expect(page).to have_content('Main number:')
         visit practice_introduction_path(@pr_max)
         find('#initiating_facility_type_visn').sibling('label').click
         select('VISN-6', :from => 'editor_visn_select')
