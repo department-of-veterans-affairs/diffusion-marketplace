5b418d84d014bafe95153f8c5d7467bd7ae9891a
Joshua Drumm <joshua.drumm@agile6.com>
2022-07-12 18:23:54 -0400

    Removed opentelemetry code

--- config/initializers/opentelemetry.rb
+++
@@ -1,7 +0,0 @@
-require 'opentelemetry/sdk'
-require 'opentelemetry/exporter/otlp'
-require 'opentelemetry/instrumentation/all'
-OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'ruby-quickstart'
-  c.use_all() # enables all instrumentation!
-end
