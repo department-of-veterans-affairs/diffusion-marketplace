cad52aa1b527967b19fe791f85313f626de5d3e6
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2022-07-25 18:52:42 -0700

    Address rspec deprecation warnings

--- spec/features/pages/show_page_spec.rb
+++ spec/features/pages/show_page_spec.rb
@@ -90,12 +90,12 @@ describe 'Page Builder - Show', type: :feature do
 
   it 'Should display the page image' do
     expect(page).to have_css("img[src*='charmander.png']")
-    page.should have_css('.flex-justify-end')
+    expect(page).to have_css('.flex-justify-end')
   end
 
   it 'should display the page image with a url' do
     expect(page).to have_css("img[src*='SpongeBob.png']")
-    page.should have_css('.flex-justify-center')
+    expect(page).to have_css('.flex-justify-center')
 
     # get the parent element of the image's URL
     external_link = page.find("img[src*='SpongeBob.png']").find(:xpath, '..')
