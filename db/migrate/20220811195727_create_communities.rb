5a091f796fc007c3539e84e765fad968439a87c9
Joshua Drumm <joshua.drumm@agile6.com>
2022-08-15 16:41:35 -0400

    DM-3528: Added distro email field to communities

--- db/migrate/20220811195727_create_communities.rb
+++ db/migrate/20220811195727_create_communities.rb
@@ -2,6 +2,7 @@ class CreateCommunities < ActiveRecord::Migration[6.0]
   def change
     create_table :communities do |t|
       t.string :name
+      t.string :distribution_email
       t.text :home_description
       t.string :intro_header
       t.text :intro_text
