951224a148a6cf0001a6569b52b0d6fb46223e2c
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2022-09-23 16:28:42 -0700

    Allow 'Load more' buttons of different types on the same page

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -33,7 +33,7 @@ class PageController < ApplicationController
     events = []
     page_components.each_with_index do |pc, index|
       if pc.component_type === 'PageEventComponent'
-        component = pc.component_type.constantize.find(pc.component_id)
+        component = pc.component
         events << component
         @event_ids << pc.component_id
       end
@@ -52,11 +52,10 @@ class PageController < ApplicationController
     return pagy_array(
           events,
           items: 3,
-          page_param: page_event_list_index.to_s,
-          link_extra: "data-remote='true' class='dm-paginated-#{page_event_list_index}-link dm-paginated-#{page_event_list_index}-events-#{params_index.nil? ? 2 : params_index.to_i + 1}-link dm-button--outline-secondary margin-top-105 width-auto'"
+          page_param: "events-#{page_event_list_index.to_s}",
+          link_extra: "data-remote='true' class='dm-paginated-events-#{page_event_list_index}-link dm-paginated-#{page_event_list_index}-events-#{params_index.nil? ? 2 : params_index.to_i + 1}-link dm-button--outline-secondary margin-top-105 width-auto'"
         )
   end
-
 
   def set_pagy_news_items_array(page_components)
     page_news_item_list_index = 0 # TODO: support multiple news item lists per page
@@ -64,7 +63,7 @@ class PageController < ApplicationController
     news_items = []
     page_components.each_with_index do |pc, index|
       if pc.component_type === 'PageNewsComponent'
-        component = pc.component_type.constantize.find(pc.component_id)
+        component = pc.component
         news_items << component
         @news_items_ids << pc.component_id
       end
@@ -82,9 +81,9 @@ class PageController < ApplicationController
   def get_pagy_news_items_array (news_items, page_news_item_list_index, params_index)
     return pagy_array(
           news_items,
-          items: 2,
-          page_param: page_news_item_list_index.to_s,
-          link_extra: "data-remote='true' class='dm-paginated-#{page_news_item_list_index}-link dm-paginated-#{page_news_item_list_index}-news-items-#{params_index.nil? ? 2 : params_index.to_i + 1}-link dm-button--outline-secondary margin-top-105 width-auto'"
+          items: 6,
+          page_param: "news-#{page_news_item_list_index.to_s}",
+          link_extra: "data-remote='true' class='dm-paginated-news-items-#{page_news_item_list_index}-link dm-paginated-#{page_news_item_list_index}-news-items-#{params_index.nil? ? 2 : params_index.to_i + 1}-link dm-button--outline-secondary margin-top-105 width-auto'"
         )
   end
 
@@ -94,7 +93,7 @@ class PageController < ApplicationController
 
     page_components.each_with_index do |pc, index|
       if pc.component_type === 'PagePracticeListComponent'
-        component = pc.component_type.constantize.find(pc.component_id)
+        component = pc.component
         practices_ids = component.practices
         practices = helpers.is_user_a_guest? ? Practice.where(id: practices_ids).public_facing.sort_by_retired.sort_a_to_z : Practice.where(id: practices_ids).published_enabled_approved.sort_by_retired.sort_a_to_z
 
@@ -114,8 +113,8 @@ class PageController < ApplicationController
     return pagy_array(
           practices,
           items: 6,
-          page_param: page_practice_list_index.to_s,
-          link_extra: "data-remote='true' class='dm-paginated-#{page_practice_list_index}-link dm-paginated-#{page_practice_list_index}-practices-#{params_index.nil? ? 2 : params_index.to_i + 1}-link dm-button--outline-secondary margin-top-105 width-auto'"
+          page_param: "practices-#{page_practice_list_index.to_s}",
+          link_extra: "data-remote='true' class='dm-paginated-practices-#{page_practice_list_index}-link dm-paginated-#{page_practice_list_index}-practices-#{params_index.nil? ? 2 : params_index.to_i + 1}-link dm-button--outline-secondary margin-top-105 width-auto'"
         )
   end
 end
