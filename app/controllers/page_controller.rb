8a9b2b3d8dccf1858d058c08b59bee4e9a173c59
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-09-28 11:46:42 -0700

    dm-3508
    remove commented code and fix margin in info window.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -80,24 +80,4 @@ class PageController < ApplicationController
           link_extra: "data-remote='true' class='dm-paginated-#{page_practice_list_index}-link dm-paginated-#{page_practice_list_index}-practices-#{params_index.nil? ? 2 : params_index.to_i + 1}-link dm-button--outline-secondary margin-top-105 width-auto'"
         )
   end
-
-  # def get_adopting_facilities_for_these_practices(practices_list, successful_adoptions, in_progress_adoptions, unsuccessful_adoptions)
-  #   va_facilities_list = []
-  #   practices_list.practices.each do |pr|
-  #     diffusion_histories = DiffusionHistory.where(practice_id: pr)
-  #     diffusion_histories.each do |dh|
-  #       dhs = DiffusionHistoryStatus.where(diffusion_history_id: dh[:id]).first.status
-  #       unless dh.va_facility_id.nil?
-  #         if (dhs == 'Completed' || dhs == 'Implemented' || dhs == 'Complete') && successful_adoptions
-  #           va_facilities_list.push dh.va_facility_id
-  #         elsif (dhs == 'In progress' || dhs == 'Planning' || dhs == 'Implementing') && in_progress_adoptions
-  #           va_facilities_list.push dh.va_facility_id
-  #         elsif dhs == "Unsuccessful" && unsuccessful_adoptions
-  #           va_facilities_list.push dh.va_facility_id
-  #         end
-  #       end
-  #     end
-  #   end
-  #   va_facilities_list
-  # end
 end
