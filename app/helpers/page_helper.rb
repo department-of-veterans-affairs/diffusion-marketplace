1c16b7ab581ffab1075fef47fbf8f3198b950e87
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-10-06 15:56:40 -0700

    dm-3508

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -1,8 +1,8 @@
 module PageHelper
-  def practices_to_display_in_info_window(facility_id, practice_list, names, taglines, short_names, slugs)
+  def get_practices_by_diffusion_histories(facility_id, map_component, names, taglines, short_names, slugs)
     dh = DiffusionHistory.where(va_facility_id: facility_id)
     dh.each do |dh|
-      if practice_list.practices.include?(dh.practice_id.to_s)
+      if map_component.practices.include?(dh.practice_id.to_s)
         names.push dh.practice.name
         short_names.push dh.practice.short_name
         taglines.push dh.practice.tagline
