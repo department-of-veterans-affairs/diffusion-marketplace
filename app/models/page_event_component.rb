14eb0f97ab09732bdb819d311589fa087adae611
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2022-10-07 21:46:18 -0700

    Add backend validation for URL fields

--- app/models/page_event_component.rb
+++ app/models/page_event_component.rb
@@ -1,3 +1,10 @@
 class PageEventComponent < ApplicationRecord
   has_one :page_component, as: :component, autosave: true
+
+  validates_with InternalUrlValidator,
+                 on: [:create, :update],
+                 if: Proc.new { |component| component.url.present? && component.url.chars.first === '/' }
+  validates_with ExternalUrlValidator,
+                 on: [:create, :update],
+                 if: Proc.new { |component| component.url.present? && component.url.chars.first != '/' }
 end
