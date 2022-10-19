93185b649582b730a7d6cb6d8191a750a673a001
bradjohnson92008 <60527788+bradjohnson92008@users.noreply.github.com>
2022-10-19 15:10:56 -0700

    dm-3632
    test for home page.

--- spec/features/shared/header_spec.rb
+++ spec/features/shared/header_spec.rb
@@ -11,7 +11,7 @@ describe 'Diffusion Marketplace header', type: :feature, js: true do
     page_group = PageGroup.create(name: 'competitions', slug: 'competitions', description: 'competitions page')
     Page.create(page_group: page_group, title: 'Shark Tank', description: 'Shark Tank page', slug: 'shark-tank', has_chrome_warning_banner: true, created_at: Time.now, published: Time.now)
     page_group_2 = PageGroup.create(name: 'covid-19', slug: 'covid-19', description: 'covid-19 page')
-    page_group_3 = PageGroup.create(name: 'home', slug: 'xr-network', description: 'xr-network pages')
+    page_group_3 = PageGroup.create(name: 'xr-network', slug: 'xr-network', description: 'xr-network pages')
     Page.create(page_group: page_group_3, title: 'XR Network', description: 'XR Network page', slug: 'xr-network', has_chrome_warning_banner: false, created_at: Time.now, published: Time.now)
 
 
