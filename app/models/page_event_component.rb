0a8e2c391788a3ed3fefadc5d5f5b536f233a2b0
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2023-05-16 15:20:30 -0700

    Move date rendering logic to component model

--- app/models/page_event_component.rb
+++ app/models/page_event_component.rb
@@ -7,4 +7,16 @@ class PageEventComponent < ApplicationRecord
   validates_with ExternalUrlValidator,
                  on: [:create, :update],
                  if: Proc.new { |component| component.url.present? && component.url.chars.first != '/' }
+
+  def rendered_date
+    return if self.start_date.blank?
+
+    if self.end_date.present? && self.start_date.year != self.end_date.year
+      return self.start_date.strftime("%B %e, %Y") + " - " + self.end_date.strftime("%B%e, %Y")
+    elsif self.end_date.present?
+      return self.start_date.strftime("%B %e") + " - " + self.end_date.strftime("%e, %Y")
+    else
+      return self.start_date.strftime("%B %e, %Y")
+    end
+  end
 end
