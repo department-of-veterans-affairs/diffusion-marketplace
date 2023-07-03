f87e8d6bc6ba574fdf2468b01a8fdb915fca36e1
PhilipDeFraties <philip.defraties@gmail.com>
2023-07-03 13:31:21 -0600

    Add specs for triple paragraph pb component

---
+++ spec/features/pages/triple_paragraph_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+describe 'Page Builder - Show - Paginated Components', type: :feature do
+  before do
+    page_group = PageGroup.create(
+      name: 'programming', 
+      slug: 'programming', 
+      description: 'Pages about programming go in this group.',
+    )
+    page = Page.create(
+      page_group: page_group, 
+      title: 'ruby', description: 'what a gem', 
+      slug: 'ruby-rocks', 
+      has_chrome_warning_banner: true, 
+      created_at: Time.now, 
+      published: Time.now,
+    )
+    @component = PageTripleParagraphComponent.create(
+      title1: 'This is first title', 
+      text1: "This is first body",
+      title2: "This is second title",
+      text2: "This is second body",
+      title3: "This is third title",
+      text3: "This is third body",
+    )
+    PageComponent.create(page: page, component: @component)
+
+    # must be logged in to view pages
+    login_as(@user, scope: :user, run_callbacks: false)
+  end
+
+  context 'Titles and paragraphs' do
+    it 'displays 3 titles and paragraphs' do
+      visit 'programming/ruby-rocks'
+
+      expect(page).to have_content "This is first title"
+      expect(page).to have_content "This is first body"
+      expect(page).to have_content "This is second title"
+      expect(page).to have_content "This is second body"
+      expect(page).to have_content "This is third title"
+      expect(page).to have_content "This is third body"
+    end
+
+    it 'displays component with optional background color' do
+      @component.update!(has_background_color: true)
+
+      visit 'programming/ruby-rocks'
+
+      within('div.bg-primary-lighter') do
+        expect(page).to have_content "This is first title"
+        expect(page).to have_content "This is first body"
+        expect(page).to have_content "This is second title"
+        expect(page).to have_content "This is second body"
+        expect(page).to have_content "This is third title"
+        expect(page).to have_content "This is third body"
+      end
+    end
+  end
+end
