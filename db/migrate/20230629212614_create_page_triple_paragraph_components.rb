c6a4de14195de0b972876885ca0dd02e6bfdb363
PhilipDeFraties <philip.defraties@gmail.com>
2023-07-05 13:33:04 -0600

    edit data type for titles

--- db/migrate/20230629212614_create_page_triple_paragraph_components.rb
+++ db/migrate/20230629212614_create_page_triple_paragraph_components.rb
@@ -2,11 +2,11 @@ class CreatePageTripleParagraphComponents < ActiveRecord::Migration[6.0]
   def change
     create_table :page_triple_paragraph_components, id: :uuid do |t|
       t.belongs_to :page_component, foreign_key: true
-      t.text :title1
+      t.string :title1
       t.text :text1
-      t.text :title2
+      t.string :title2
       t.text :text2
-      t.text :title3
+      t.string :title3
       t.text :text3
       t.boolean :has_background_color, default: false
       t.timestamps
