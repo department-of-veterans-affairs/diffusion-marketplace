9d902a87e893fc8d76dca84ff802a42806c427e0
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2023-07-31 17:36:49 -0700

    CTA component

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -129,12 +129,8 @@ ActiveAdmin.register Page do
             para "#{component&.practices.length} Practice#{component&.practices.length == 1 ? '' : 's'}" if pc.component_type == 'PagePracticeListComponent'
             # Practice list
             para component&.practices.map {|pid| Practice.find(pid).name }.join("\n") if pc.component_type == 'PagePracticeListComponent'
-            # Button text
-            if (pc.component_type == 'PageCtaComponent' ) && component&.button_text.present?
-              para "Button text: #{component.button_text}"
-            end
 
-            MIGRATED_COMPONENTS = component_type = [ 'PageHeader2Component', 'PageHeader3Component', 'PageEventComponent', 'PageNewsComponent', 'PageSubpageHyperlinkComponent', 'PagePublicationComponent', 'PageAccordionComponent', 'PageBlockQuoteComponent', 'PageSimpleButtonComponent', 'PageTwoToOneImageComponent', 'PageOneToOneImageComponent', 'PageTripleParagraphComponent', 'PageDownloadableFileComponent', 'PageParagraphComponent', 'PageMapComponent', 'PageYouTubePlayerComponent', 'PageImageComponent']
+            MIGRATED_COMPONENTS = component_type = [ 'PageHeader2Component', 'PageHeader3Component', 'PageEventComponent', 'PageNewsComponent', 'PageSubpageHyperlinkComponent', 'PagePublicationComponent', 'PageAccordionComponent', 'PageBlockQuoteComponent', 'PageSimpleButtonComponent', 'PageTwoToOneImageComponent', 'PageOneToOneImageComponent', 'PageTripleParagraphComponent', 'PageDownloadableFileComponent', 'PageParagraphComponent', 'PageMapComponent', 'PageYouTubePlayerComponent', 'PageImageComponent', 'PageCtaComponent']
             if MIGRATED_COMPONENTS.include? pc.component_type
               ul do
                 component.class::FORM_FIELDS.each do | key, value|
--- app/models/page_cta_component.rb
+++ app/models/page_cta_component.rb
@@ -1,4 +1,11 @@
 class PageCtaComponent < ApplicationRecord
   has_one :page_component, as: :component, autosave: true
   validates :cta_text, :button_text, :url, presence: true
+
+  FORM_FIELDS = { # Fields and labels in .arb for
+    cta_text: 'CTA Text',
+    button_text: 'Button Text',
+    url: 'Action Link',
+    has_background_color?: 'Add Background Color'
+  }.freeze
 end
