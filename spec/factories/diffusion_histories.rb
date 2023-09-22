a66314d1ca8d6b272a45f96792774176e5ead2ac
PhilipDeFraties <philip.defraties@gmail.com>
2023-09-22 15:47:40 -0600

    add factories

---
+++ spec/factories/diffusion_histories.rb
@@ -0,0 +1,55 @@
+FactoryBot.define do
+  factory :diffusion_history do
+    association :practice
+
+    trait :with_va_facility do
+      association :va_facility
+    end
+
+    trait :with_clinical_resource_hub do
+      association :clinical_resource_hub
+    end
+
+    trait :with_status_complete do
+      after(:create) do |dh|
+        create(:diffusion_history_status, :complete, diffusion_history: dh)
+      end
+    end
+
+    trait :with_status_completed do
+      after(:create) do |dh|
+        create(:diffusion_history_status, :completed, diffusion_history: dh)
+      end
+    end
+
+    trait :with_status_in_progress do
+      after(:create) do |dh|
+        create(:diffusion_history_status, :in_progress, diffusion_history: dh)
+      end
+    end
+
+    trait :with_status_planning do
+      after(:create) do |dh|
+        create(:diffusion_history_status, :planning, diffusion_history: dh)
+      end
+    end
+
+    trait :with_status_implementing do
+      after(:create) do |dh|
+        create(:diffusion_history_status, :implementing, diffusion_history: dh)
+      end
+    end
+
+    trait :with_status_implemented do
+      after(:create) do |dh|
+        create(:diffusion_history_status, :implemented, diffusion_history: dh)
+      end
+    end
+
+    trait :with_status_unsuccessful do
+      after(:create) do |dh|
+        create(:diffusion_history_status, :unsuccessful, diffusion_history: dh)
+      end
+    end
+  end
+end
