a25566e622bd87a19ec548afd6a6a5ea81a24f4b
PhilipDeFraties <philip.defraties@gmail.com>
2023-09-26 15:02:22 -0600

    remove `#created-crh-practices` endpoint and route

--- app/controllers/clinical_resource_hubs_controller.rb
+++ app/controllers/clinical_resource_hubs_controller.rb
@@ -20,31 +20,6 @@ class ClinicalResourceHubsController < ApplicationController
     get_categories_by_practices(@practices_adopted_by_crh, @crh_practices_adopted_categories)
   end
 
-  # GET /crh/:id/created_crh_practices
-  def created_crh_practices
-    page = 1
-    page = params[:page].to_i if params[:page].present?
-    sort_option = params[:sort_option] || 'a_to_z'
-    search_term = params[:search_term] ? params[:search_term].downcase : nil
-    categories = params[:categories] || nil
-    created_practices = @crh.get_crh_created_innovations(@crh.id, search_term, sort_option, categories, helpers.is_user_a_guest?)
-    @pagy_created_practices = pagy_array(
-        created_practices,
-        items: 3,
-        page: page
-    )
-    @pagy_created_info = @pagy_created_practices[0]
-    practices = @pagy_created_practices[1]
-    practice_cards_html = ''
-    practices.each do |pr|
-      pr_html = render_to_string('shared/_practice_card', layout: false, locals: { practice: pr })
-      practice_cards_html += pr_html
-    end
-    respond_to do |format|
-      format.json { render :json => { practice_cards_html: practice_cards_html, count: created_practices.size, next: @pagy_created_info.next } }
-    end
-  end
-
   private
   def set_crh
     @visn = params[:id].present? ? Visn.find_by!(number: params[:id]) : Visn.find_by!(number: params[:number])
