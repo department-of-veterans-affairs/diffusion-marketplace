662cd1d3cdfb67e61ae1cf60533f0f4a0d135b32
PhilipDeFraties <philip.defraties@gmail.com>
2023-09-26 15:47:27 -0600

    edits `va_facilities` factory

--- spec/factories/va_facilities.rb
+++ spec/factories/va_facilities.rb
@@ -2,14 +2,14 @@ FactoryBot.define do
   factory :va_facility do
     association :visn
     
-    sta3n { Faker::Number.unique.number(digits: 3) }
-    station_number { Faker::Number.unique.number(digits: 3) }
+    sta3n { Faker::Number.unique.number(digits: 5) }
+    station_number { Faker::Number.unique.number(digits: 5) }
     official_station_name { Faker::Company.unique.name }
     common_name { Faker::Company.unique.industry }
     fy17_parent_station_complexity_level { Faker::Lorem.unique.word }
     latitude { Faker::Address.unique.latitude }
     longitude { Faker::Address.unique.longitude }
-    rurality { Faker::Lorem.unique.word }
+    rurality { Faker::Alphanumeric.alpha(number: 1) }
     street_address { Faker::Address.unique.street_address }
     street_address_city { Faker::Address.unique.city }
     street_address_state { Faker::Address.unique.state_abbr }
