0f50e7439a6f1c91e9b18284259206c50a7af404
PhilipDeFraties <philip.defraties@gmail.com>
2023-09-26 15:13:40 -0600

    add spec for `:with_categories_and_adoptions_ct` scope`

--- spec/models/practice_spec.rb
+++ spec/models/practice_spec.rb
@@ -93,4 +93,77 @@ RSpec.describe Practice, type: :model do
       }.to change { @practice.reload.diffusion_histories_count }.by(-1)
     end
   end
+
+  describe 'scopes' do
+    describe '.get_by_created_crh' do
+      let!(:clinical_resource_hub_1) { create(:clinical_resource_hub) }
+      let!(:clinical_resource_hub_2) { create(:clinical_resource_hub) }
+      
+      let!(:practice1) { create(:practice, published: true, enabled: true, approved: true, hidden: false) }
+      let!(:practice2) { create(:practice, published: true, enabled: true, approved: true, hidden: false) }
+      let!(:practice3) { create(:practice, published: false, enabled: false, approved: false, hidden: true) }
+
+      let!(:facility_with_hub_1) { create(:practice_origin_facility, practice: practice1, clinical_resource_hub: clinical_resource_hub_1) }
+      let!(:facility_with_hub_2) { create(:practice_origin_facility, practice: practice2, clinical_resource_hub: clinical_resource_hub_2) }
+
+      it 'returns practices that are published, enabled, approved, associated with provided clinical_resource_hub_id, and have loaded associations' do
+        result = Practice.published_enabled_approved.load_associations.get_by_created_crh(clinical_resource_hub_1.id)
+
+        expect(result).to include(practice1)
+        expect(result).not_to include(practice2, practice3)
+      end
+    end
+
+    describe '.get_by_adopted_crh' do
+      let!(:clinical_resource_hub_1) { create(:clinical_resource_hub) }
+      let!(:clinical_resource_hub_2) { create(:clinical_resource_hub) }
+      
+      let!(:practice1) { create(:practice, published: true, enabled: true, approved: true, hidden: false) }
+      let!(:practice2) { create(:practice, published: true, enabled: true, approved: true, hidden: false) }
+      let!(:practice3) { create(:practice, published: false, enabled: false, approved: false, hidden: true) } # Will be filtered out by `published_enabled_approved` scope
+
+      let!(:diffusion_history_1) { create(:diffusion_history, practice: practice1, clinical_resource_hub: clinical_resource_hub_1) }
+      let!(:diffusion_history_2) { create(:diffusion_history, practice: practice2, clinical_resource_hub: clinical_resource_hub_2) }
+      
+
+      it 'returns practices that are published, enabled, approved, adopted by the given clinical_resource_hub_id, and have loaded associations' do
+        result = Practice.published_enabled_approved.load_associations.get_by_adopted_crh(clinical_resource_hub_1.id)
+
+        expect(result).to include(practice1)
+        expect(result).not_to include(practice2, practice3)
+      end
+    end
+
+    describe '.with_categories_and_adoptions_ct' do
+      let!(:category1) { create(:category, name: "Category 1") }
+      let!(:category2) { create(:category, name: "Category 2") }
+      let!(:category3) { create(:category, name: "Category 3") }
+
+      let!(:practice1) { create(:practice, published: true, enabled: true, approved: true, hidden: false) }
+      let!(:practice2) { create(:practice, published: true, enabled: true, approved: true, hidden: false) }
+      let!(:practice3) { create(:practice, published: false, enabled: false, approved: false, hidden: true) }
+
+      let!(:diffusion_history1) { create(:diffusion_history, :with_va_facility, practice: practice1) }
+      let!(:diffusion_history2) { create(:diffusion_history, :with_va_facility, practice: practice1) }
+      let!(:diffusion_history3) { create(:diffusion_history, :with_va_facility, practice: practice2) }
+
+      it 'returns practices with associated category names and adoptions count' do
+        create(:category_practice, practice: practice1, category: category1)
+        create(:category_practice, practice: practice1, category: category2)
+        create(:category_practice, practice: practice2, category: category3)
+
+        result = Practice.with_categories_and_adoptions_ct.to_a
+
+        expect(result.count).to eq(2)
+
+        practice_with_counts_1 = result.find { |p| p.id == practice1.id }
+        expect(practice_with_counts_1.adoption_count).to eq(2)
+        expect(practice_with_counts_1.category_names).to include('Category 1', 'Category 2')
+
+        practice_with_counts_2 = result.find { |p| p.id == practice2.id }
+        expect(practice_with_counts_2.adoption_count).to eq(1)
+        expect(practice_with_counts_2.category_names).to include('Category 3')
+      end
+    end
+  end
 end
