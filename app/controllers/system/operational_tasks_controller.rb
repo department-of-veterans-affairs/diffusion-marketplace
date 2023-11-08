b70763746aa9c80152ddcb29134ecb500e96386b
PhilipDeFraties <philip.defraties@gmail.com>
2023-11-08 11:49:48 -0700

    edit `System::OperationalTasksController`
    skips `before_actions` inherited from the `ApplicationController`, utilizes `custom_verify_authenticity_token` as before action to keep endpoint secure

--- app/controllers/system/operational_tasks_controller.rb
+++ app/controllers/system/operational_tasks_controller.rb
@@ -1,6 +1,16 @@
 module System
   class OperationalTasksController < ApplicationController
-    before_action :check_token_authentication, only: [:clear_signer_cache]
+    skip_before_action  :verify_authenticity_token,
+                        :setup_breadcrumb_navigation, 
+                        :reload_turbolinks, 
+                        :store_user_location!, 
+                        :set_paper_trail_whodunnit, 
+                        :log_in_va_user, 
+                        :user_accepted_terms?, 
+                        :set_visit_props, 
+                        :set_visitor_props, 
+                        only: [:clear_signer_cache]
+    before_action :custom_verify_authenticity_token, only: [:clear_signer_cache]
 
     def clear_signer_cache
       begin
@@ -13,8 +23,9 @@ module System
 
     private
 
-    def check_token_authentication
-      return unless Rails.env.production?
+    def custom_verify_authenticity_token
+ 
+      return unless Rails.env.production? || Rails.env.development?
 
       secure_token = ENV['CLEAR_SIGNER_CACHE_TOKEN']
       authorization_header = request.headers['Authorization']
