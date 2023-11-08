22e2acfeda7fef96fc70ae372ac1497cae837863
PhilipDeFraties <philip.defraties@gmail.com>
2023-11-08 11:06:53 -0700

    update `System::OperationalTasksController` spec

--- spec/requests/operational_tasks_controller_spec.rb
+++ spec/requests/operational_tasks_controller_spec.rb
@@ -2,34 +2,41 @@ require 'rails_helper'
 
 RSpec.describe System::OperationalTasksController, type: :controller do
   describe 'POST #clear_signer_cache' do
-    let(:whitelisted_ip) { '123.45.67.89' }
-    let(:non_whitelisted_ip) { '98.76.54.32' }
+    let(:secure_token) { 'correct_secure_token_value' }
     let(:cache_key) { 's3_signer' }
 
     before do
       allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
-      ENV['WHITELISTED_IPS'] = '123.45.67.89,124.77.55.67'
-      request.env['REMOTE_ADDR'] = ip_address
+      ENV['CLEAR_SIGNER_CACHE_TOKEN'] = secure_token
       Rails.cache.write(cache_key, 'cached data')
+      request.headers['Authorization'] = "Bearer #{token}"
     end
 
-    context 'when called from a whitelisted IP' do
-      let(:ip_address) { whitelisted_ip }
-      
-      context 'when cache clearance is successful' do
-        it 'returns status ok' do
-          post :clear_signer_cache
-          expect(response).to have_http_status(:ok)
-          expect(response.body).to match(/Cache cleared successfully/)
-          expect(Rails.cache.read(cache_key)).to be_nil
-        end
+    context 'with valid authorization token' do
+      let(:token) { secure_token }
+
+      it 'returns status ok and clears the cache' do
+        post :clear_signer_cache
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to match(/Cache cleared successfully/)
+        expect(Rails.cache.read(cache_key)).to be_nil
       end
     end
 
-    context 'when called from a non-whitelisted IP' do
-      let(:ip_address) { non_whitelisted_ip }
+    context 'with invalid authorization token' do
+      let(:token) { 'incorrect_token' }
 
-      it 'returns forbidden' do
+      it 'returns forbidden status' do
+        post :clear_signer_cache
+        expect(response).to have_http_status(:forbidden)
+        expect(response.body).to match(/Access Denied/)
+      end
+    end
+
+    context 'without any authorization token' do
+      let(:token) { nil }
+
+      it 'returns forbidden status' do
         post :clear_signer_cache
         expect(response).to have_http_status(:forbidden)
         expect(response.body).to match(/Access Denied/)
