26585efc0fa9c8cf58e675ab6af4f04d47a5a769
PhilipDeFraties <philip.defraties@gmail.com>
2023-11-14 13:35:38 -0700

    update `PracticeResourcesController` to expect `practice` id in addition to `practice_resource` id in params

--- app/controllers/practice_resources_controller.rb
+++ app/controllers/practice_resources_controller.rb
@@ -1,15 +1,24 @@
 class PracticeResourcesController < ApplicationController
   before_action :authenticate_user!, except: [:download]
-  before_action :set_practice_resource, only: [:download]
+  before_action :set_practice_and_resource, only: [:download]
 
   def download
-    redirect_to @practice_resource.attachment_s3_presigned_url
+    if @practice_resource
+      redirect_to @practice_resource.attachment_s3_presigned_url
+    else
+      render plain: "Resource not found", status: :not_found
+    end
   end
 
   private
 
-  def set_practice_resource
-    @practice_resource = PracticeResource.find_by(id: params[:id])
-    render plain: "Resource not found", status: :not_found unless @practice_resource
+  def set_practice_and_resource
+    @practice = Practice.find_by(id: params[:practice_id])
+    if @practice
+      @practice_resource = @practice.practice_resources.find_by(id: params[:id])
+    end
+    unless @practice_resource
+      render plain: "Practice or Resource not found", status: :not_found and return
+    end
   end
 end
