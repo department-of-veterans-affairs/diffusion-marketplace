e1ecc990d5ddb30bb5c86d77d772d1123458d0b0
PhilipDeFraties <philip.defraties@gmail.com>
2023-11-15 12:18:23 -0700

    edit `PracticeResourcesController`
    edits `set` method, changes name, adds case statement to determine resource type
    
    adds strong params

--- app/controllers/practice_resources_controller.rb
+++ app/controllers/practice_resources_controller.rb
@@ -1,6 +1,6 @@
 class PracticeResourcesController < ApplicationController
   before_action :authenticate_user!, except: [:download]
-  before_action :set_practice_and_resource, only: [:download]
+  before_action :set_practice_resource, only: [:download]
 
   def download
     if @practice_resource
@@ -12,13 +12,30 @@ class PracticeResourcesController < ApplicationController
 
   private
 
-  def set_practice_and_resource
-    @practice = Practice.find_by(id: params[:practice_id])
-    if @practice
-      @practice_resource = @practice.practice_resources.find_by(id: params[:id])
+  def set_practice_resource
+    practice = Practice.find_by(id: resource_params[:practice_id])
+
+    if practice
+      @practice_resource = case resource_params[:resource_type]
+                           when 'PracticeResource'
+                             practice.practice_resources.find_by(id: resource_params[:id])
+                           when 'PracticeProblemResource'
+                             practice.practice_problem_resources.find_by(id: resource_params[:id])
+                           when 'PracticeSolutionResource'
+                             practice.practice_solution_resources.find_by(id: resource_params[:id])
+                           when 'PracticeResultsResource'
+                             practice.practice_results_resources.find_by(id: resource_params[:id])
+                           else
+                             nil
+                           end
     end
+
     unless @practice_resource
       render plain: "Practice or Resource not found", status: :not_found and return
     end
+  end
+
+  def resource_params
+    params.permit(:practice_id, :id, :resource_type)
   end
 end
