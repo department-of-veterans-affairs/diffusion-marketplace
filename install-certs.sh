737423f9b8f8ffd6600c4aaeedae7f3cdc0db0df
nuel247 <emekaraymond123@gmail.com>
2023-11-21 16:32:49 -0500

    Add install-certs.sh script for VA certs

---
+++ install-certs.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+wget \
+        --level=1 \
+        --quiet \
+        --recursive \
+        --no-parent \
+        --no-host-directories \
+        --no-directories \
+        --accept="VA*.cer" \
+        http://aia.pki.va.gov/PKI/AIA/VA/
+CA_CERTS=(
+  VA-Internal-S2-RCA1-v1.cer
+  VA-Internal-S2-ICA4.cer
+  VA-Internal-S2-ICA5.cer
+  VA-Internal-S2-ICA6.cer
+  VA-Internal-S2-ICA7.cer
+  VA-Internal-S2-ICA8.cer
+  VA-Internal-S2-ICA9.cer
+  VA-Internal-S2-ICA10.cer
+  VA-Internal-S2-ICA1-v1.cer
+  VA-Internal-S2-ICA2-v1.cer
+  VA-Internal-S2-ICA3-v1.cer
+ )
+ANCHORS=/usr/local/share/ca-certificates
+for CERT in ${CA_CERTS[@]}
+do
+  OUT=$ANCHORS/${CERT##*/}
+ echo "Copying $CERT to $OUT"
+  cp $CERT $OUT
+done
+for cert in $(find $ANCHORS -type f -name "*.cer")
+  do
+    echo "Combining $cert"
+    out_name="${cert%.*}"
+    openssl x509 -in $cert -out $out_name.crt
+    chmod 0444 $out_name.crt
+  done
+echo "Installed CA certs."
+update-ca-certificates -f
