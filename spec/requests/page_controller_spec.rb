0345aad4fd32d200e73c925deb0b1df371c66a28
PhilipDeFraties <philip.defraties@gmail.com>
2023-11-28 14:20:37 -0700

    add specs for `PageController`
    tests for authed vs un-authed access, refactors file utilizing factory bot

--- spec/requests/page_controller_spec.rb
+++ spec/requests/page_controller_spec.rb
@@ -1,37 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe PageController, type: :request do
-  before do
-    page_group = PageGroup.create!(
-      name: 'va-immersive',
-      description: 'Pages about programming go in this group.'
-    )
-    Page.create!(
-      title: 'VA Immersive Home',
-      description: 'This is a homepage',
-      slug: 'home',
-      page_group: page_group,
-      published: Time.now
-    )
-    Page.create!(
-      title: 'VA Immersive Events nad News',
-      description: 'This is an events page',
-      slug: 'events-and-news',
-      page_group: page_group,
-      published: Time.now
-    )
-  end
+  describe 'show' do
+    before do
+      @page_group = create(:page_group, name: 'va-immersive', description: 'Pages about programming go in this group.') 
+      create(:page, slug: 'home', page_group: @page_group, is_public: true) 
+      create(:page, slug: 'events-and-news', page_group: @page_group, is_public: true) 
+    end
 
-  context 'show' do
-    it "should redirect the user with a prepended '/communities' to the URL if the Page is a community page "\
-      "and the URL doesn't include '/communities'" do
-      get '/va-immersive'
-      # For some reason on CI, an ID gets appended to the sample domain (e.g. 'http://www.example.com482c087b40dc/communities/va-immersive').
-      # To circumvent this issue, we can just check to make sure that the response header 'Location' includes '/communities'
-      expect(response.header['Location']).to include('/communities/va-immersive')
+    context 'public pages' do
+      it "should redirect the user with a prepended '/communities' to the URL if the Page is a community page and the URL doesn't include '/communities'" do
+        get '/va-immersive'
+        expect(response.header['Location']).to include('/communities/va-immersive')
 
-      get '/va-immersive/events-and-news'
-      expect(response.header['Location']).to include('/communities/va-immersive/events-and-news')
+        get '/va-immersive/events-and-news'
+        expect(response.header['Location']).to include('/communities/va-immersive/events-and-news')
+      end
+    end
+
+    context 'non-public pages' do
+      before do
+        create(:page, slug: 'va-users-only', page_group: @page_group, is_public: false)
+      end
+
+      it 'should prevent a non-logged in user from accessing' do
+        get '/va-immersive/va-users-only'
+        expect(flash[:warning]).to eq('You are not authorized to view this content.')
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'should allow access for a logged-in user' do
+        user = create(:user)
+        login_as(user, :scope => :user, :run_callbacks => false)
+
+        get '/va-immersive/va-users-only'
+        expect(response.header['Location']).to include('/communities/va-immersive/va-users-only')
+      end
     end
   end
 end
