c661f3e4019aa7f8c5d6bbcd0c88c2300986b122
PhilipDeFraties <philip.defraties@gmail.com>
2023-11-30 17:09:09 -0700

    add tests for `PageController`
    refactor test data

--- spec/requests/page_controller_spec.rb
+++ spec/requests/page_controller_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe PageController, type: :request do
   describe 'show' do
-    before do
-      @page_group = create(:page_group, name: 'va-immersive', description: 'Pages about programming go in this group.') 
-      create(:page, slug: 'home', page_group: @page_group, is_public: true) 
-      create(:page, slug: 'events-and-news', page_group: @page_group, is_public: true) 
-    end
-
+    let(:page_group) { create(:page_group, name: 'va-immersive', description: 'Pages about programming go in this group.') }
+    
     context 'public pages' do
       it "should redirect the user with a prepended '/communities' to the URL if the Page is a community page and the URL doesn't include '/communities'" do
+        create(:page, slug: 'home', page_group: page_group, is_public: true) 
+        create(:page, slug: 'events-and-news', page_group: page_group, is_public: true) 
         get '/va-immersive'
         expect(response.header['Location']).to include('/communities/va-immersive')
 
@@ -20,7 +18,7 @@ RSpec.describe PageController, type: :request do
 
     context 'non-public pages' do
       before do
-        create(:page, slug: 'va-users-only', page_group: @page_group, is_public: false)
+        create(:page, slug: 'va-users-only', page_group: page_group, is_public: false)
       end
 
       it 'should prevent a non-logged in user from accessing' do
@@ -35,6 +33,25 @@ RSpec.describe PageController, type: :request do
 
         get '/va-immersive/va-users-only'
         expect(response.header['Location']).to include('/communities/va-immersive/va-users-only')
+      end
+    end
+
+    context 'un-published pages' do
+      it 'should redirect to root path for non-logged in users' do
+        create(:page, slug: 'un-published', page_group: page_group, is_public: true, published: false)
+
+        get '/va-immersive/un-published'
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'should redirect to root path for non-logged in users' do
+        create(:page, slug: 'un-published', page_group: page_group, is_public: false, published: false)
+
+        user = create(:user)
+        login_as(user, :scope => :user, :run_callbacks => false)
+
+        get '/va-immersive/un-published'
+        expect(response).to redirect_to(root_path)
       end
     end
   end
