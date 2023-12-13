8f8a94916f204fc6c20f50adf1de3a5a097ea189
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2023-12-12 16:15:45 -0800

    Clean up and reorganize spec file

--- spec/features/nominate_practices/nominate_a_practice_page_spec.rb
+++ spec/features/nominate_practices/nominate_a_practice_page_spec.rb
@@ -1,22 +1,28 @@
 require 'rails_helper'
 
 describe 'Nominate a practice page', type: :feature do
-  context 'VA user' do
-    before do
-      user = User.create!(email: 'admin-dmva@va.gov', password: 'Password123', password_confirmation: 'Password123', skip_va_validation: true, confirmed_at: Time.now, accepted_terms: true)
-      login_as(user, :scope => :user, :run_callbacks => false)
+  context 'Render' do
+    it 'renders form' do
       visit '/nominate-an-innovation'
-    end
-
-    it 'should be there' do
       expect(page).to be_accessible.according_to :wcag2a, :section508
       expect(page).to have_content('Nominate an innovation')
       expect(page).to have_content('VA staff and collaborators are welcome to nominate active innovations for consideration on the Diffusion Marketplace using the form below.')
     end
 
-    it 'should allow the user to send an email to the DM team about nominating an innovation' do
+    it 'redirect the user to /nominate-an-innovation if they try to visit the old /nominate-a-practice URL' do
+      visit '/nominate-a-practice'
+      expect(page).to have_current_path(nominate_an_innovation_path)
+    end
+  end
+
+  context 'Email' do
+    before do
+      visit '/nominate-an-innovation'
       fill_in('Your email address', with: 'test@test.com')
       fill_in('Subject line', with: 'Test subject')
+    end
+
+    it 'validates message body is filled out' do
       # all fields should be required
       click_button('Send message')
       message = find('#message').native.attribute('validationMessage')
@@ -30,29 +36,20 @@ describe 'Nominate a practice page', type: :feature do
       expect(page).to have_content('Message sent. The Diffusion Marketplace team will review your nomination.')
     end
 
+    it 'adds debug URL if sent from a non-PROD environment' do
+      fill_in('Your message', with: 'This is a different test message')
+      click_button('Send message')
+      expect(ActionMailer::Base.deliveries.last.body.raw_source).to have_content('Sent from')
+    end
+
     #spam detector................................................................
     it 'should log and redirect user to homepage if phone field is populated' do
-      fill_in('Your email', with: 'test@test.com')
-      fill_in('Subject line', with: 'Test subject')
       fill_in('Your message', with: 'This is a test message')
       fill_in('phone', visible: false, with: 'this is spam')
       # make sure the FormSpam records increase by 1
       expect { click_button('Send message') }.to change(FormSpam, :count).by(1)
       # make sure user is redirected to home page.
       expect(page).to have_current_path(root_path)
-    end
-
-    it 'should redirect the user to /nominate-an-innovation if they try to visit the old /nominate-a-practice URL' do
-      visit '/nominate-a-practice'
-      expect(page).to have_current_path(nominate_an_innovation_path)
-    end
-
-    it 'adds debug URL if sent from a non-PROD environment' do
-      fill_in('Your email', with: 'test@different.com')
-      fill_in('Subject line', with: 'Test stuff')
-      fill_in('Your message', with: 'This is a different test message')
-      click_button('Send message')
-      expect(ActionMailer::Base.deliveries.last.body.raw_source).to have_content('Sent from')
     end
   end
 end
