ab0ae2b9fba829a515ea03f6a052d09866ac36aa
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2023-12-14 16:18:50 -0700

    DM-4348 add recaptcha to `NominatePracticesController#email` (#750)
    * add recaptcha to `NominatePracticesController#email`
    
    * update `NominatePracticesController#email` to use `root_path`
    
    * edit Capybara driver in spec_helper
    
    changes `headless` flag for `Selenium::WebDriver::Chrome` to be `headless=new`

--- app/controllers/nominate_practices_controller.rb
+++ app/controllers/nominate_practices_controller.rb
@@ -4,15 +4,23 @@ class NominatePracticesController < ApplicationController
   end
 
   def email
-    if params["phone"].present?
-      log_spam_attempt "Nominate"
-      redirect_to root_path
+    recaptcha_result = verify_recaptcha(action: 'email', minimum_score: 0.5)
+
+    if recaptcha_result
+      if params["phone"].present?
+        log_spam_attempt "Nominate"
+        redirect_to root_path
+      else
+        NominateAPracticeMailer.send_contact_email(options = { email: params[:email], subject: params[:subject], message: params[:message], message_url: request.url }, 'Nominate', 'nominate_a_practice_mailer/nominate_a_practice_email').deliver_now
+        success = 'Message sent. The Diffusion Marketplace team will review your nomination.'
+        flash[:success] = success
+        respond_to do |format|
+          format.html { redirect_back fallback_location: root_path, notice: flash[:notice] }
+        end
+      end
     else
-      NominateAPracticeMailer.send_contact_email(options = { email: params[:email], subject: params[:subject], message: params[:message], message_url: request.url }, 'Nominate', 'nominate_a_practice_mailer/nominate_a_practice_email').deliver_now
-      success = 'Message sent. The Diffusion Marketplace team will review your nomination.'
-      flash[:success] = success
       respond_to do |format|
-        format.html { redirect_back fallback_location: root_path, notice: flash[:notice] }
+        format.html { redirect_to root_path }
       end
     end
   end
