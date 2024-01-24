5ba05198e547cb182a2643a2edd229e9e1065e16
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2024-01-23 17:13:11 -0800

    [WIP] Give editors access to /admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
   def authenticate_active_admin_user!
     authenticate_user!
     if current_user.present?
-      is_admin = current_user.has_role?(:admin)
+      is_admin = current_user.has_role?(:admin) || current_user.has_role?(:editor, :any) # TODO: move to relevant admin actions
       terms_accepted = current_user.accepted_terms
       flash[:alert] = "Unauthorized Access!" if !is_admin
       flash[:alert] = "Accept terms and conditions" if !terms_accepted
