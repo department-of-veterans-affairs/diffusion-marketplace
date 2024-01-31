231b81c272a531901ba047189a1f775297a68237
PhilipDeFraties <philip.defraties@gmail.com>
2024-01-31 16:46:22 -0700

    update `CategoriesHelper#get_most_popular_categories` method
    edits method name to be more accurate of return value
    
    adds comment explaining query
    
    remove unnecessary variable for return
    
    format for line length

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,16 +1,22 @@
 module CategoriesHelper
-  def get_most_popular_categories
+  def get_category_names_by_popularity
     time_limit = Time.now - 90.days
     Rails.cache.fetch('categories_with_popularity', expires_in: 24.hours) do
-      # returns only categories that can be found in the filters on the /search page
-      categories_by_popularity = Category.where(is_other: false)
-                                      .joins('LEFT JOIN ahoy_events ON categories.id = CAST(ahoy_events.properties ->> \'category_id\' AS INTEGER) AND ahoy_events.name = \'Category selected\' AND ahoy_events.time > \'' + time_limit.to_s(:db) + '\'')
-                                      .joins(:parent_category)
-                                      .where.not(parent_category: nil)
-                                      .group('categories.id')
-                                      .order('COUNT(ahoy_events.id) DESC, categories.name')
-                                      .pluck('categories.name')
-      categories_by_popularity
+      # returns only category names that can be found in the filters on the /search page,
+      # raw SQL allows for joining category selection events to be used for ordering,
+      # left joins allows categories with no selection events to be returned
+      Category.where(is_other: false)
+              .joins(
+                'LEFT JOIN ahoy_events ' \
+                'ON categories.id = CAST(ahoy_events.properties ->> \'category_id\' AS INTEGER) ' \
+                'AND ahoy_events.name = \'Category selected\' ' \
+                'AND ahoy_events.time > \'' + time_limit.to_s(:db) + '\''
+              )
+              .joins(:parent_category)
+              .where.not(parent_category: nil)
+              .group('categories.id')
+              .order('COUNT(ahoy_events.id) DESC, categories.name')
+              .pluck('categories.name')
     end
   end
 
