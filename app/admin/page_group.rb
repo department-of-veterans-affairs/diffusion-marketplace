d82372382518b80a2a5c000324c557f587556287
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2024-02-05 11:28:08 -0800

    [WIP] editors form

--- app/admin/page_group.rb
+++ app/admin/page_group.rb
@@ -18,13 +18,39 @@ ActiveAdmin.register PageGroup do
     f.inputs do
       f.input :name
       f.input :description
+      f.input :editors, # or roles? 
+              label: 'Commmunity editors',
+              as: :text,
+              input_html: { class: 'height-7' },
+              hint: "Enter VA emails as a comma-separated list, e.g. marketplace@va.gov,test@va.gov"
+      # if image.present?
+      #     f.input :delete_image_and_alt_text,
+      #             as: :boolean,
+      #             label: 'Delete image and alternative text',
+      #             input_html: { class: 'margin-left-2px' }
+      # end
     end
+
     f.actions
   end
 
   controller do
     def find_resource
       scoped_collection.friendly.find(params[:id])
+    end
+
+    def create
+      # add a new role
+      # should we require an editor for this field? 
+
+      super
+    end
+
+    def update
+
+      # delete users' roles if editors is empty
+
+      super
     end
   end
 end
