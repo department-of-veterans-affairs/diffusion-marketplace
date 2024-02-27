d8feb1969814f6f58ce6c9aac3d4f3d966e1059c
PhilipDeFraties <philip.defraties@gmail.com>
2024-02-27 14:40:18 -0700

    update innovation batch email specs to verify confirmation email sent

--- spec/features/admin/email_selected_practices_editors_spec.rb
+++ spec/features/admin/email_selected_practices_editors_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 describe 'Admin email all editors button', type: :feature do
   let(:admin) { create(:user, :admin) }
-  let!(:users) { create_list(:user, 3) }
+  let(:user) { create(:user) }
+  let!(:editors) { create_list(:user, 3) }
   let!(:recently_updated_practice) do
     create(
       :practice,
-      user: admin,
+      user: user,
       published: true,
       updated_at: 1.day.ago,
       last_email_date: 2.days.ago
@@ -16,7 +17,7 @@ describe 'Admin email all editors button', type: :feature do
   let!(:older_practice) do
     create(
       :practice,
-      user: admin,
+      user: user,
       published: true,
       updated_at: 10.days.ago,
       last_email_date: 11.days.ago
@@ -26,7 +27,7 @@ describe 'Admin email all editors button', type: :feature do
   let!(:unemailed_practice) do
     create(
       :practice,
-      user: admin,
+      user: user,
       published: true,
       updated_at: 5.days.ago,
       last_email_date: nil
@@ -36,7 +37,7 @@ describe 'Admin email all editors button', type: :feature do
   let!(:unpublished_practice) do
     create(
       :practice,
-      user: admin,
+      user: user,
       published: false,
       updated_at: 3.days.ago,
       last_email_date: 4.days.ago
@@ -49,24 +50,22 @@ describe 'Admin email all editors button', type: :feature do
     visit admin_practices_path
 
     [recently_updated_practice, older_practice, unemailed_practice].each do |practice|
-      create(:practice_editor, user: users.first, practice: practice)
+      create(:practice_editor, user: editors.first, practice: practice)
     end
 
     page.driver.browser.manage.window.resize_to(1920, 1080)
   end
 
   it 'sends an email to all editors of unfiltered and published practices' do
-    create(:practice_editor, user: users.second, practice: recently_updated_practice)
-    2.times do
-      create(:practice_editor, user: admin, practice: recently_updated_practice)
-    end
+    create(:practice_editor, user: editors.second, practice: recently_updated_practice)
+    create(:practice_editor, user: user, practice: recently_updated_practice)
 
     filter_practices_and_send_email
 
     emails = ActionMailer::Base.deliveries
-    expect(emails.count).to eq 3
+    expect(emails.count).to eq 4
 
-    email = emails.find { |e| e.to.include?(users.first.email) }
+    email = emails.find { |e| e.to.include?(editors.first.email) }
     expect(email.subject).to eq 'Important Update'
 
     expect(email.body.encoded).to include older_practice.name
@@ -74,7 +73,7 @@ describe 'Admin email all editors button', type: :feature do
     expect(email.body.encoded).to include recently_updated_practice.name
     expect(email.body.encoded).not_to include unpublished_practice.name
 
-    email2 = emails.find { |e| e.to.include?(users.second.email) }
+    email2 = emails.find { |e| e.to.include?(editors.second.email) }
     expect(email2.subject).to eq 'Important Update'
 
     expect(email2.body.encoded).to include recently_updated_practice.name
@@ -82,23 +81,31 @@ describe 'Admin email all editors button', type: :feature do
     expect(email2.body.encoded).not_to include unemailed_practice.name
     expect(email2.body.encoded).not_to include unpublished_practice.name
 
+    user_email = emails.find { |e| e.to.include?(user.email) }
+    expect(user_email.subject).to eq 'Important Update'
+
+    expect(user_email.body.encoded).to include older_practice.name
+    expect(user_email.body.encoded).to include unemailed_practice.name
+    expect(user_email.body.encoded).to include recently_updated_practice.name
+    expect(user_email.body.encoded).not_to include unpublished_practice.name
+
+    expect(user_email.body.encoded.scan(recently_updated_practice.name).length).to eq(1)
+
     admin_email = emails.find { |e| e.to.include?(admin.email) }
-    expect(admin_email.subject).to eq 'Important Update'
-
-    expect(admin_email.body.encoded).to include older_practice.name
-    expect(admin_email.body.encoded).to include unemailed_practice.name
-    expect(admin_email.body.encoded).to include recently_updated_practice.name
+    expect(admin_email.subject).to eq('Confirmation: Diffusion Marketplace Innovation Batch Emails Sent')
+    expect(admin_email.body.encoded).not_to include older_practice.name
+    expect(admin_email.body.encoded).not_to include unemailed_practice.name
+    expect(admin_email.body.encoded).not_to include recently_updated_practice.name
     expect(admin_email.body.encoded).not_to include unpublished_practice.name
-
-    expect(admin_email.body.encoded.scan(recently_updated_practice.name).length).to eq(1)
+    expect(admin_email.body.encoded).to include("The above message has been sent to the editors and owners of all published Innovations")
   end
 
   it 'sends an email to all editors of practices filtered by "Not Updated Since"' do
     filter_practices_and_send_email('practice_batch_email[not_updated_since]', 7.days.ago)
     emails = ActionMailer::Base.deliveries
-    expect(emails.count).to eq 2
+    expect(emails.count).to eq 3
 
-    email = emails.find { |e| e.to.include?(users.first.email) }
+    email = emails.find { |e| e.to.include?(editors.first.email) }
     expect(email.subject).to eq 'Important Update'
 
     expect(email.body.encoded).to include older_practice.name
@@ -106,20 +113,29 @@ describe 'Admin email all editors button', type: :feature do
     expect(email.body.encoded).not_to include recently_updated_practice.name
     expect(email.body.encoded).not_to include unpublished_practice.name
 
+    user_email = emails.find { |e| e.to.include?(user.email) }
+    expect(user_email.body.encoded).to include older_practice.name
+    expect(user_email.body.encoded).not_to include unemailed_practice.name
+    expect(user_email.body.encoded).not_to include recently_updated_practice.name
+    expect(user_email.body.encoded).not_to include unpublished_practice.name
+
     admin_email = emails.find { |e| e.to.include?(admin.email) }
+    expect(admin_email.subject).to eq('Confirmation: Diffusion Marketplace Innovation Batch Emails Sent')
+    expect(admin_email.body.encoded).to include("The above message has been sent to the editors and owners of the following Innovations:")
     expect(admin_email.body.encoded).to include older_practice.name
     expect(admin_email.body.encoded).not_to include unemailed_practice.name
     expect(admin_email.body.encoded).not_to include recently_updated_practice.name
     expect(admin_email.body.encoded).not_to include unpublished_practice.name
+    expect(admin_email.body.encoded).to include("not_updated_since: #{7.days.ago.to_date}")
   end
 
   it 'sends an email to all editors of practices filtered by "Not Emailed Since"' do
     filter_practices_and_send_email('practice_batch_email[not_emailed_since]', 12.days.ago)
 
     emails = ActionMailer::Base.deliveries
-    expect(emails.count).to eq 2
+    expect(emails.count).to eq 3
 
-    email = emails.find { |e| e.to.include?(users.first.email) }
+    email = emails.find { |e| e.to.include?(editors.first.email) }
     expect(email.subject).to eq 'Important Update'
 
     expect(email.body.encoded).to include unemailed_practice.name
@@ -127,11 +143,20 @@ describe 'Admin email all editors button', type: :feature do
     expect(email.body.encoded).not_to include recently_updated_practice.name
     expect(email.body.encoded).not_to include unpublished_practice.name
 
+    user_email = emails.find { |e| e.to.include?(user.email) }
+    expect(user_email.body.encoded).to include unemailed_practice.name
+    expect(user_email.body.encoded).not_to include older_practice.name
+    expect(user_email.body.encoded).not_to include recently_updated_practice.name
+    expect(user_email.body.encoded).not_to include unpublished_practice.name
+
     admin_email = emails.find { |e| e.to.include?(admin.email) }
+    expect(admin_email.subject).to eq('Confirmation: Diffusion Marketplace Innovation Batch Emails Sent')
+    expect(admin_email.body.encoded).to include("The above message has been sent to the editors and owners of the following Innovations:")
     expect(admin_email.body.encoded).to include unemailed_practice.name
     expect(admin_email.body.encoded).not_to include older_practice.name
     expect(admin_email.body.encoded).not_to include recently_updated_practice.name
     expect(admin_email.body.encoded).not_to include unpublished_practice.name
+    expect(admin_email.body.encoded).to include("not_emailed_since: #{12.days.ago.to_date}")
   end
 
   def filter_practices_and_send_email(filter=nil, value=nil)
--- spec/services/practice_mailer_service_spec.rb
+++ spec/services/practice_mailer_service_spec.rb
@@ -4,19 +4,20 @@ RSpec.describe PracticeMailerService do
   describe '.call' do
     let!(:admin) { create(:user, :admin) }
     let!(:user) { create(:user) }
-    let!(:other_user) { create(:user) }
+    let!(:user2) { create(:user) }
+    let!(:user3) { create(:user) }
     let(:subject_text) { 'Important Update' }
     let(:message_text) { 'Please review the latest changes.' }
 
-    let!(:practice) { create(:practice, user: admin, published: true) }
+    let!(:practice) { create(:practice, user: user3, published: true) }
     let!(:practice_a) { create(:practice, name: "Innovation A", user: user, published: true, updated_at: 2.days.ago, last_email_date: 3.days.ago) }
-    let!(:practice_b) { create(:practice, name: "Innovation B", user: other_user, published: true, updated_at: 4.days.ago, last_email_date: nil) }
+    let!(:practice_b) { create(:practice, name: "Innovation B", user: user2, published: true, updated_at: 4.days.ago, last_email_date: nil) }
     let!(:unpublished_practice) { create(:practice, name: "Innovation C", user: user, published: false, updated_at: 1.day.ago) }
 
     before do
-      create(:practice_editor, user: other_user, practice: practice_a)
+      create(:practice_editor, user: user2, practice: practice_a)
       create(:practice_editor, user: user, practice: practice_b)
-      create(:practice_editor, user: admin, practice: unpublished_practice)
+      create(:practice_editor, user: user3, practice: unpublished_practice)
 
       ActionMailer::Base.deliveries.clear
     end
@@ -31,7 +32,7 @@ RSpec.describe PracticeMailerService do
         filters: filters
       )
       emails = ActionMailer::Base.deliveries
-      expect(emails.count).to eq(3)
+      expect(emails.count).to eq(4)
 
       user_email = emails.find { |e| e.to.include?(user.email) }
       expect(user_email.subject).to eq(subject_text)
@@ -42,22 +43,29 @@ RSpec.describe PracticeMailerService do
       expect(user_email.body.encoded).to include(Rails.application.routes.url_helpers.practice_url(practice_a, Rails.application.config.action_mailer.default_url_options))
       expect(user_email.body.encoded).to include(Rails.application.routes.url_helpers.practice_url(practice_b, Rails.application.config.action_mailer.default_url_options))
 
-      other_user_email = emails.find { |e| e.to.include?(other_user.email) }
-      expect(other_user_email.subject).to eq(subject_text)
-      expect(other_user_email.to).to include(other_user.email)
-      expect(other_user_email.body.encoded).to include(practice_a.name)
-      expect(other_user_email.body.encoded).to include(practice_b.name)
-      expect(other_user_email.body.encoded).not_to include(practice.name)
-      expect(other_user_email.body.encoded).to include(Rails.application.routes.url_helpers.practice_url(practice_a, Rails.application.config.action_mailer.default_url_options))
-      expect(other_user_email.body.encoded).to include(Rails.application.routes.url_helpers.practice_url(practice_b, Rails.application.config.action_mailer.default_url_options))
+      user2_email = emails.find { |e| e.to.include?(user2.email) }
+      expect(user2_email.subject).to eq(subject_text)
+      expect(user2_email.to).to include(user2.email)
+      expect(user2_email.body.encoded).to include(practice_a.name)
+      expect(user2_email.body.encoded).to include(practice_b.name)
+      expect(user2_email.body.encoded).not_to include(practice.name)
+      expect(user2_email.body.encoded).to include(Rails.application.routes.url_helpers.practice_url(practice_a, Rails.application.config.action_mailer.default_url_options))
+      expect(user2_email.body.encoded).to include(Rails.application.routes.url_helpers.practice_url(practice_b, Rails.application.config.action_mailer.default_url_options))
+
+      user3_email = emails.find { |e| e.to.include?(user3.email) }
+      expect(user3_email.subject).to eq(subject_text)
+      expect(user3_email.to).to include(user3.email)
+      expect(user3_email.body.encoded).to include(practice.name)
+      expect(user3_email.body.encoded).not_to include(practice_a.name)
+      expect(user3_email.body.encoded).not_to include(practice_b.name)
+      expect(user3_email.body.encoded).to include(Rails.application.routes.url_helpers.practice_url(practice, Rails.application.config.action_mailer.default_url_options))
 
       admin_email = emails.find { |e| e.to.include?(admin.email) }
-      expect(admin_email.subject).to eq(subject_text)
-      expect(admin_email.to).to include(admin.email)
-      expect(admin_email.body.encoded).to include(practice.name)
+      expect(admin_email.subject).to eq('Confirmation: Diffusion Marketplace Innovation Batch Emails Sent')
+      expect(admin_email.body.encoded).not_to include(practice.name)
       expect(admin_email.body.encoded).not_to include(practice_a.name)
       expect(admin_email.body.encoded).not_to include(practice_b.name)
-      expect(admin_email.body.encoded).to include(Rails.application.routes.url_helpers.practice_url(practice, Rails.application.config.action_mailer.default_url_options))
+      expect(admin_email.body.encoded).to include("The above message has been sent to the editors and owners of all published Innovations")
     end
 
     context 'when applying specific scope filters' do
@@ -72,7 +80,7 @@ RSpec.describe PracticeMailerService do
         )
 
         emails = ActionMailer::Base.deliveries
-        expect(emails.count).to eq(2)
+        expect(emails.count).to eq(3)
 
         user_email = emails.find { |e| e.to.include?(user.email) }
         expect(user_email.subject).to eq(subject_text)
@@ -83,14 +91,22 @@ RSpec.describe PracticeMailerService do
         expect(user_email.body.encoded).not_to include(Rails.application.routes.url_helpers.practice_url(practice_a, Rails.application.config.action_mailer.default_url_options))
         expect(user_email.body.encoded).to include(Rails.application.routes.url_helpers.practice_url(practice_b, Rails.application.config.action_mailer.default_url_options))
 
-        other_user_email = emails.find { |e| e.to.include?(other_user.email) }
-        expect(other_user_email.subject).to eq(subject_text)
-        expect(other_user_email.to).to include(other_user.email)
-        expect(other_user_email.body.encoded).not_to include(practice_a.name)
-        expect(other_user_email.body.encoded).to include(practice_b.name)
-        expect(other_user_email.body.encoded).not_to include(practice.name)
-        expect(other_user_email.body.encoded).not_to include(Rails.application.routes.url_helpers.practice_url(practice_a, Rails.application.config.action_mailer.default_url_options))
-        expect(other_user_email.body.encoded).to include(Rails.application.routes.url_helpers.practice_url(practice_b, Rails.application.config.action_mailer.default_url_options))
+        user2_email = emails.find { |e| e.to.include?(user2.email) }
+        expect(user2_email.subject).to eq(subject_text)
+        expect(user2_email.to).to include(user2.email)
+        expect(user2_email.body.encoded).not_to include(practice_a.name)
+        expect(user2_email.body.encoded).to include(practice_b.name)
+        expect(user2_email.body.encoded).not_to include(practice.name)
+        expect(user2_email.body.encoded).not_to include(Rails.application.routes.url_helpers.practice_url(practice_a, Rails.application.config.action_mailer.default_url_options))
+        expect(user2_email.body.encoded).to include(Rails.application.routes.url_helpers.practice_url(practice_b, Rails.application.config.action_mailer.default_url_options))
+
+        admin_email = emails.find { |e| e.to.include?(admin.email) }
+        expect(admin_email.subject).to eq('Confirmation: Diffusion Marketplace Innovation Batch Emails Sent')
+        expect(admin_email.body.encoded).to include("The above message has been sent to the editors and owners of the following Innovations:")
+        expect(admin_email.body.encoded).to include(practice_b.name)
+        expect(admin_email.body.encoded).not_to include(practice.name)
+        expect(admin_email.body.encoded).not_to include(practice_a.name)
+        expect(admin_email.body.encoded).to include("not_updated_since: #{3.days.ago.to_date}")
       end
     end
 
@@ -103,7 +119,15 @@ RSpec.describe PracticeMailerService do
           current_user: admin,
           filters: filters
         )
-        expect(ActionMailer::Base.deliveries.count).to eq(0)
+        emails = ActionMailer::Base.deliveries
+        expect(emails.count).to eq(1)
+        admin_email = emails.find { |e| e.to.include?(admin.email) }
+        expect(admin_email.subject).to eq('Failure: Diffusion Marketplace Innovation Batch Emails Not Sent')
+        expect(admin_email.body.encoded).to include("You attempted to send the above message to filtered Innovation owners and editors but the applied filters returned no Innovation results.")
+        expect(admin_email.body.encoded).not_to include(practice_b.name)
+        expect(admin_email.body.encoded).not_to include(practice.name)
+        expect(admin_email.body.encoded).not_to include(practice_a.name)
+        expect(admin_email.body.encoded).to include("not_updated_since: #{5.days.ago.to_date}")
       end
     end
   end
