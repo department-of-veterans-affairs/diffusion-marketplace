bc5b835b36cd28252283bc5af65184f03ae97ffe
PhilipDeFraties <philip.defraties@gmail.com>
2024-03-12 14:48:22 -0600

    add error handling and retry config to PracticeMailerWorker

--- app/workers/practice_mailer_worker.rb
+++ app/workers/practice_mailer_worker.rb
@@ -1,5 +1,10 @@
 class PracticeMailerWorker
   include Sidekiq::Worker
+   sidekiq_options retry: 5
+
+  sidekiq_retries_exhausted do |msg, _|
+    Rails.logger.error "Failed to deliver email, job arguments: #{msg['args'].inspect}"
+  end
 
   def perform(email_type, *args)
     case email_type
@@ -10,5 +15,8 @@ class PracticeMailerWorker
     else
       raise ArgumentError, "Unknown email_type: #{email_type}"
     end
+  rescue => e
+    Rails.logger.error "Error sending email: #{e.message}"
+    raise e
   end
 end
