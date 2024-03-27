3b50fa967ec3b8b5d0276a62cc3f10fcda4dbb61
PhilipDeFraties <philip.defraties@gmail.com>
2024-03-27 17:01:05 -0600

    update migration for change in `PageEventComponent` col name from `hide_once_passed` to `hide_after_date`

--- db/migrate/20240326180328_add_hide_after_date_to_page_event_components.rb
+++ db/migrate/20240326180328_add_hide_after_date_to_page_event_components.rb
@@ -1,4 +1,4 @@
-class AddHideOncePassedToPageEventComponents < ActiveRecord::Migration[6.1]
+class AddHideAfterDateToPageEventComponents < ActiveRecord::Migration[6.1]
   def change
     add_column :page_event_components, :hide_after_date, :boolean, default: false
   end
