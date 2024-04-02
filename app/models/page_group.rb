9ce1da6f097a9fc15901de1dff22e4680f9a06de
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2024-04-01 17:35:10 -0700

    Move community slugs into helper

--- app/models/page_group.rb
+++ app/models/page_group.rb
@@ -6,15 +6,8 @@ class PageGroup < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
 
-  COMMUNITY_SLUGS = [
-    'xr-network',
-    'va-immersive',
-    'suicide-prevention',
-    'age-friendly'
-  ]
-
   def is_community?
-    COMMUNITY_SLUGS.include?(self.slug)
+    CommunityHelper.is_community_slug?(self.slug)
   end
 
   def self.ransackable_attributes(auth_object = nil)
