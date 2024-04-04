5b3abfb30f7840c40f31268e3ed1c4e2a4e5f037
PhilipDeFraties <philip.defraties@gmail.com>
2024-04-04 10:35:48 -0600

    update PageGroup model file
    adds `COMMUNITIES` const with hard-coded `page_groups` names that will be source of truth for which records are considered `communities`
    
    adds inst method `community_with_home_hash` that returns `PageGroup` names and slugs
    
    updates `is_community?` inst method to utilize new COMMUNITIES const

--- app/models/page_group.rb
+++ app/models/page_group.rb
@@ -1,4 +1,9 @@
 class PageGroup < ApplicationRecord
+  COMMUNITIES = [
+    "VA Immersive",
+		"Suicide Prevention",
+		"Age-Friendly"
+  ]
   extend FriendlyId
   friendly_id :name, use: :slugged
   has_many :pages, dependent: :destroy
@@ -6,8 +11,29 @@ class PageGroup < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
 
+  scope :community, -> { where(name: COMMUNITIES) }
+
+  def self.community_with_home_hash(public = true, admin = false)
+    query = PageGroup.community.joins(:pages).where(pages: { slug: 'home' })
+    query = query.where(pages: { is_public: true }) if public
+
+    query = query.where.not(pages: { published: nil }) unless admin
+
+    hash = {}
+    if query
+      query.find_each do |page_group|
+        key = page_group.name
+        key += " - Admin Preview" if admin && !page_group.pages.where(slug: 'home').first.published?
+
+        hash[key] = page_group.slug
+      end
+    end
+
+    hash
+  end
+
   def is_community?
-    CommunityHelper.is_community_slug?(self.slug)
+    COMMUNITIES.include?(self.name)
   end
 
   def self.ransackable_attributes(auth_object = nil)
--- spec/models/page_group_spec.rb
+++ spec/models/page_group_spec.rb
@@ -19,17 +19,58 @@ RSpec.describe PageGroup, type: :model do
   end
 
   describe '#is_community?' do
-    context 'when the slug is in COMMUNITY_SLUGS' do
+    context 'when the name is in COMMUNITIES' do
       it 'returns true' do
-        page_group = build(:page_group, slug: PageGroup::COMMUNITY_SLUGS.first)
+        page_group = create(:page_group, name: PageGroup::COMMUNITIES.first)
         expect(page_group.is_community?).to be true
       end
     end
 
-    context 'when the slug is not in COMMUNITY_SLUGS' do
+    context 'when the slug is not in COMMUNITIES' do
       it 'returns false' do
-        page_group = build(:page_group, slug: 'non-community-slug')
+        page_group = create(:page_group, name: 'non-public-slug')
         expect(page_group.is_community?).to be false
+      end
+    end
+  end
+
+  describe '.public_with_home_hash' do
+    let!(:public_page_group) { create(:page_group, name: "VA Immersive") }
+    let!(:non_public_page_group) { create(:page_group, name: "Suicide Prevention") }
+
+    let!(:public_page) { create(:page, page_group: public_page_group, slug: "home", is_public: true, published: Date.today) }
+    let!(:private_page) { create(:page, page_group: non_public_page_group, slug: "home", is_public: false, published: Date.today) }
+
+    context 'when public is true' do
+      it 'returns hash of public communities with "home" pages' do
+        result = PageGroup.community_with_home_hash(true, false)
+        expect(result.keys).to include(public_page_group.name)
+        expect(result.values).to include(public_page_group.slug)
+        expect(result.keys).not_to include(non_public_page_group.name)
+      end
+    end
+
+    context 'when public is false' do
+      it 'returns hash with public and non-public community names and page slugs' do
+        result = described_class.community_with_home_hash(false, false)
+        expect(result.keys).to include(public_page_group.name)
+        expect(result.values).to include(public_page_group.slug)
+        expect(result.keys).to include(non_public_page_group.name)
+        expect(result.values).to include(non_public_page_group.slug)
+      end
+    end
+
+    context 'when admin is true' do
+      it 'includes admin preview for unpublished home pages' do
+        admin_only_page_group = create(:page_group, name: "Age-Friendly")
+        create(:page, page_group: admin_only_page_group, slug: "home", is_public: false, published: nil)
+        result = described_class.community_with_home_hash(false, true)
+        expect(result.keys).to include(public_page_group.name)
+        expect(result.values).to include(public_page_group.slug)
+        expect(result.keys).to include(non_public_page_group.name)
+        expect(result.values).to include(non_public_page_group.slug)
+        expect(result.keys).to include("#{admin_only_page_group.name} - Admin Preview")
+        expect(result.values).to include(admin_only_page_group.slug)
       end
     end
   end
