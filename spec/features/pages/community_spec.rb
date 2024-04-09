591c402443f6fbf71c1efccf93015ad4b2a0c3c4
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2024-04-08 21:05:51 -0700

    Update specs

--- spec/features/pages/community_spec.rb
+++ spec/features/pages/community_spec.rb
@@ -6,6 +6,7 @@ describe 'Communities', type: :feature, js:true do
 		@non_community_pagegroup = PageGroup.create(name: "Competitions", slug: 'competitions', description: "not a community")
 		@community_landing_page = Page.create(page_group: @approved_community, title: 'Community homepage', description: 'cool stuff', slug: 'home', created_at: Time.now, is_public: true, published: Time.now)		
 		@non_community_landing_page = Page.create(page_group: @non_community_pagegroup, title: 'Competitions open call', description: 'not a community', slug: 'home', created_at: Time.now, is_public: true, published: Time.now)
+		@community_subpage = Page.create(page_group: @approved_community, title: 'Community about page', description: 'about us', slug: 'about', created_at: Time.now, is_public: true, published: Time.now)
 	end
 
 	context 'subnav' do
@@ -14,17 +15,19 @@ describe 'Communities', type: :feature, js:true do
 			expect(page).to have_css('#community-subnav')
 			within('#community-subnav') do
 				expect(page).to have_content('Community')
-				expect(page).not_to have_content('About')
+				expect(page).to have_content('About')
 			end
 		end
 
 		it 'only shows pages that exist' do
-			community_subpage = Page.create(page_group: @approved_community, title: 'Community about page', description: 'about us', slug: 'about', created_at: Time.now, is_public: true, published: Time.now)
-			visit('/communities/va-immersive')
+			another_subpage = Page.create(page_group: @approved_community, title: 'Community Events and News page', description: 'events and news', slug: 'events-and-news', created_at: Time.now, is_public: true, published: Time.now)
+			visit('/communities/va-immersive/about')
 			within('#community-subnav') do
 				expect(page).to have_content('About')
-				expect(page).to have_link('About', css: '.current-page')
-				expect(page).not_to have_content('Events and News')
+				expect(page).to have_link('About', class: 'current-page')
+				expect(page).to have_content('Events and News')
+				expect(page).to have_link('Events and News')
+				expect(page).not_to have_content('Publications')
 			end
 		end
 
