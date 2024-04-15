f7086c5d85782b1b622f44c1fb44349fe121169b
PhilipDeFraties <philip.defraties@gmail.com>
2024-04-15 17:45:38 -0600

    remove edit to ActiveAdminAdapter

--- app/models/active_admin_adapter.rb
+++ app/models/active_admin_adapter.rb
@@ -1,5 +1,5 @@
 class ActiveAdminAdapter < ActiveAdmin::AuthorizationAdapter
   def authorized?(action, subject = nil)
-    user.has_role?(:admin) || user.has_role?(:editor, :any) # TODO: move to relevant admin actions
+    user.has_role?(:admin)
   end
 end
