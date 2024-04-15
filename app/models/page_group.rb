4927b62470278f60f9d2eddb7f794c065e25ca32
PhilipDeFraties <philip.defraties@gmail.com>
2024-04-15 17:01:22 -0600

    update PageGroup model
    add `remove_editor_roles`, `add_editor_roles_by_emails` methods
    
    add attr_accessors for use in active_admin form
    
    add before_destroy callback to remove editor roles upon record destruction

--- app/models/page_group.rb
+++ app/models/page_group.rb
@@ -11,7 +11,7 @@ class PageGroup < ApplicationRecord
   has_many :editor_roles, -> { where(name: 'page_group_editor', resource_type: 'PageGroup') }, class_name: 'Role', foreign_key: :resource_id
   has_many :editors, through: :editor_roles, source: :users
 
-  before_destroy :remove_editor_roles
+  before_destroy :remove_all_editor_roles
 
   validates_uniqueness_of :name
   validates :name, presence: true
@@ -19,6 +19,8 @@ class PageGroup < ApplicationRecord
   resourcify
 
   scope :community, -> { where(name: COMMUNITIES) }
+
+  attr_accessor :new_editors, :remove_editors
 
   def self.community_with_home_hash(public = true, admin = false)
     query = PageGroup.community.joins(:pages).where(pages: { slug: 'home' })
@@ -51,13 +53,29 @@ class PageGroup < ApplicationRecord
     %w[pages editors]
   end
 
-  def editors_emails_string
-    editors.order(:email).pluck(:email).join(', ')
+  def remove_editor_roles(editor_ids)
+    editors.where(id: editor_ids).find_each do |editor|
+      editor.remove_role(:page_group_editor, self)
+    end
+  end
+
+  def add_editor_roles_by_emails(editor_emails)
+    emails = editor_emails.to_s.split(',').map(&:strip).uniq
+    users = User.where(email: emails)
+    existing_emails = users.pluck(:email)
+    non_existent_emails = emails - existing_emails
+
+    if non_existent_emails.empty?
+      users.each { |user| user.add_role(:page_group_editor, self) }
+      [nil, true]
+    else
+      [non_existent_emails, false]
+    end
   end
 
   private
 
-  def remove_editor_roles
+  def remove_all_editor_roles
     editor_roles.destroy_all
   end
 end
