ba76c215535505cea1ae45697287db42945b8497
PhilipDeFraties <philip.defraties@gmail.com>
2024-04-23 16:39:23 -0600

    add DashboardHelper with communities_with_home_hash method
    gathers and formats communities for navbar dropdown

---
+++ app/helpers/dashboard_helper.rb
@@ -0,0 +1,43 @@
+module DashboardHelper
+  extend ActiveSupport::Concern
+
+  def communities_with_home_hash(user=nil)
+    query = PageGroup.community.joins(:pages).where(pages: { slug: 'home' })
+
+    if user.present?
+      query = handle_user_specific_communities_query(query, user)
+    else
+      query = query.where(pages: { is_public: true }).where.not(pages: { published: nil })
+    end
+
+    assemble_page_group_hash(query)
+  end
+
+  private
+
+  def handle_user_specific_communities_query(query, user)
+    is_editor = user.has_role?(:page_group_editor, :any)
+    is_admin = user.has_role?(:admin)
+
+    if is_editor && !is_admin
+      editor_page_group_ids = user.editable_page_group_ids
+      query.where.not(pages: { published: nil }).or(query.where(id: editor_page_group_ids))
+    elsif !is_admin
+      query.where.not(pages: { published: nil })
+    else
+      query
+    end
+  end
+
+  def assemble_page_group_hash(query)
+    query.each_with_object({}) do |page_group, acc|
+      key = page_group.name
+      home_page = page_group.pages.first
+      home_page_published = home_page.published?
+      if !home_page_published
+        key += " - Preview"
+      end
+      acc[key] = page_group.slug
+    end
+  end
+end
---
+++ spec/helpers/dashboard_helper_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+RSpec.describe DashboardHelper, type: :helper do
+  let(:user) { nil }
+
+  let(:published_public_page_group) { create(:page_group) }
+  let(:published_public_page) { create(:page, page_group: published_public_page_group, slug: 'home', is_public: true, published: Time.zone.now) }
+
+  let(:non_published_non_public_page_group) { create(:page_group) }
+  let(:non_published_non_public_page) { create(:page, page_group: non_published_non_public_page_group, slug: 'home', is_public: false, published: nil) }
+
+  let(:published_non_public_page_group) { create(:page_group) }
+  let(:published_non_public_page) { create(:page, page_group: published_non_public_page_group, slug: 'home') }
+
+  let(:public_non_published_page_group) { create(:page_group) }
+  let(:public_non_published_page) { create(:page, page_group: public_non_published_page_group, slug: 'home', is_public: true, published: nil) }
+
+  let(:page_group_ids) {
+    [
+      published_public_page.id,
+      non_published_non_public_page.id,
+      published_non_public_page.id,
+      public_non_published_page.id
+    ]
+   }
+
+  before do
+    allow(PageGroup).to receive(:community).and_return(PageGroup.where(id: page_group_ids))
+  end
+
+  describe "#communities_with_home_hash" do
+    subject(:community_hash) { helper.communities_with_home_hash(user) }
+
+    context "when user is nil" do
+      it "returns hash of public and published page groups" do
+        expect(community_hash[published_public_page_group.name]).to eq(published_public_page_group.slug)
+        expect(community_hash.count).to eq(1)
+      end
+
+      it "does not include page groups with unpublished home pages" do
+        published_public_page.update(published: nil)
+        expect(community_hash).to be_empty
+      end
+
+      it "does not include page groups with published but non-public home pages" do
+        published_public_page.update(is_public: false, published: Time.zone.now)
+        expect(community_hash).to be_empty
+      end
+    end
+
+    context "when user is an admin" do
+      let(:user) { create(:user) }
+
+      before { user.add_role(:admin) }
+
+      it "returns all page groups regardless of publication status" do
+        expect(community_hash[published_public_page_group.name]).to eq(published_public_page_group.slug)
+        expect(community_hash["#{non_published_non_public_page_group.name} - Preview"]).to eq(non_published_non_public_page_group.slug)
+        expect(community_hash[published_non_public_page_group.name]).to eq(published_non_public_page_group.slug)
+        expect(community_hash["#{public_non_published_page_group.name} - Preview"]).to eq(public_non_published_page_group.slug)
+      end
+    end
+
+    context "when user is an editor but not admin" do
+      let(:user) { create(:user) }
+      let(:non_published_non_public_page_group_b) { create(:page_group) }
+      let(:non_published_non_public_page_b) { create(:page, page_group: non_published_non_public_page_group_b, slug: 'home', is_public: false, published: nil) }
+
+      before do
+        user.add_role(:page_group_editor, non_published_non_public_page_group)
+        user.add_role(:page_group_editor, public_non_published_page_group)
+      end
+
+      it "returns own editable unpublished page groups and published page_groups" do
+        expect(community_hash[published_public_page_group.name]).to eq(published_public_page_group.slug)
+        expect(community_hash["#{non_published_non_public_page_group.name} - Preview"]).to eq(non_published_non_public_page_group.slug)
+        expect(community_hash[published_non_public_page_group.name]).to eq(published_non_public_page_group.slug)
+        expect(community_hash["#{public_non_published_page_group.name} - Preview"]).to eq(public_non_published_page_group.slug)
+      end
+
+      it "doesn't return unpublished page_groups user is not an editor for" do
+        expect(community_hash).not_to include(non_published_non_public_page_group_b.name)
+        expect(community_hash).not_to include("#{non_published_non_public_page_group_b.name} - Preview")
+      end
+    end
+  end
+end
