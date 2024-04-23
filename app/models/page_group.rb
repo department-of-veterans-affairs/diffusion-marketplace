7cfcae62acafbd94bb851e375b399786f7229b7c
PhilipDeFraties <philip.defraties@gmail.com>
2024-04-23 16:41:42 -0600

    remove PageGroup model method community_with_home_hash
    logic moved to new DashboardHelper class

--- app/models/page_group.rb
+++ app/models/page_group.rb
@@ -30,31 +30,12 @@ class PageGroup < ApplicationRecord
 
   attr_accessor :new_editors, :remove_editors
 
-  def self.community_with_home_hash(public = true, admin = false)
-    query = PageGroup.community.joins(:pages).where(pages: { slug: 'home' })
-    query = query.where(pages: { is_public: true }) if public
-
-    query = query.where.not(pages: { published: nil }) unless admin
-
-    hash = {}
-    if query
-      query.find_each do |page_group|
-        key = page_group.name
-        key += " - Admin Preview" if admin && !page_group.pages.where(slug: 'home').first.published?
-
-        hash[key] = page_group.slug
-      end
-    end
-
-    hash
-  end
-
   def is_community?
     COMMUNITIES.include?(self.name)
   end
 
   def landing_page
-    Page.find_by(page_group_id: self.id, slug: 'home')
+    pages.find_by(slug: 'home')
   end
 
   def subnav_hash
