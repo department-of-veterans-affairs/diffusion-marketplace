9d46ce2b79080aa0764353fd1870c89ea39f61a5
PhilipDeFraties <philip.defraties@gmail.com>
2024-05-30 15:43:25 -0600

    update css and js concerning page_group ul drag and drop

--- app/assets/javascripts/admin/page_group.js
+++ app/assets/javascripts/admin/page_group.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('#current_pages').sortable({
     items: 'li',
     handle: '.handle',
+    axis: 'x',
     update: function(event, ui) {
       $('#current_pages li').each(function(index) {
         $(this).find('input[name*="[position]"]').val(index + 1);
