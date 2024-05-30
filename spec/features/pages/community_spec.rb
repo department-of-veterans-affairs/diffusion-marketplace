90bab9fd5f8a0b1defcc08dac2c2f9d0df6a695c
PhilipDeFraties <philip.defraties@gmail.com>
2024-05-30 11:20:18 -0600

    update community_spec

--- spec/features/pages/community_spec.rb
+++ spec/features/pages/community_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 describe 'Communities', type: :feature, js:true do
-	before do
-		@approved_community = create(:community)
-		@non_community_pagegroup = PageGroup.create(name: "Competitions", slug: 'competitions', description: "not a community")
-		@community_landing_page = Page.create(page_group: @approved_community, title: 'Community homepage', description: 'cool stuff', slug: 'home', created_at: Time.now, is_public: true, published: Time.now)
-		@non_community_landing_page = Page.create(page_group: @non_community_pagegroup, title: 'Competitions open call', description: 'not a community', slug: 'home', created_at: Time.now, is_public: true, published: Time.now)
-		@community_subpage = Page.create(page_group: @approved_community, title: 'Community about page', description: 'about us', slug: 'about', created_at: Time.now, is_public: true, published: Time.now)
-	end
+	let!(:approved_community) { create(:community) }
+	let!(:non_community_pagegroup) { create(:page_group, name: "Competitions", slug: 'competitions', description: "not a community") }
+	let!(:community_landing_page) { create(:page, page_group: approved_community, title: 'Community homepage', description: 'cool stuff', slug: 'home', created_at: Time.now, is_public: true, published: Time.now) }
+	let!(:non_community_landing_page) { create(:page, page_group: non_community_pagegroup, title: 'Competitions open call', description: 'not a community', slug: 'home', created_at: Time.now, is_public: true, published: Time.now) }
+	let!(:community_subpage) { create(:page, page_group: approved_community, title: 'Community about page', description: 'about us', slug: 'about', created_at: Time.now, is_public: true, published: Time.now, short_name: 'About') }
 
 	describe 'subnav' do
-		it 'does not render for generic pagegroups' do
-			visit page_show_path(@non_community_pagegroup.slug, @non_community_landing_page.slug)
+		it 'does not render for a page that is not a community page' do
+			visit page_show_path(non_community_pagegroup.slug, non_community_landing_page.slug)
 			expect(page).not_to have_css('#community-subnav')
 		end
 
 		context 'published homepage' do
-			it 'is rendered for approved communities' do
+			it 'is rendered for pages that are community pages' do
 				visit('/communities/va-immersive')
 				expect(page).to have_css('#community-subnav')
 				within('#community-subnav') do
@@ -26,7 +24,7 @@ describe 'Communities', type: :feature, js:true do
 			end
 
 			it 'only shows pages that exist and have been published' do
-				another_subpage = Page.create(page_group: @approved_community, title: 'Community Events and News page', description: 'events and news', slug: 'events-and-news', created_at: Time.now, is_public: true, published: Time.now)
+				another_subpage = Page.create(page_group: approved_community, title: 'Community Events and News page', description: 'events and news', slug: 'events-and-news', created_at: Time.now, is_public: true, published: Time.now)
 				visit('/communities/va-immersive/about')
 				within('#community-subnav') do
 					expect(page).to have_content('About')
@@ -40,8 +38,8 @@ describe 'Communities', type: :feature, js:true do
 
 		context 'unpublished homepage' do
 			it 'renders all approved subpages regardless of publication status' do
-				@community_landing_page.update(published: nil)
-				unpublished_subpage = Page.create(page_group: @approved_community, title: 'Publications', description: 'approved subpage', slug: 'publications', created_at: Time.now, is_public: true, published: nil)
+				community_landing_page.update(published: nil)
+				unpublished_subpage = Page.create(page_group: approved_community, title: 'Publications', description: 'approved subpage', slug: 'publications', created_at: Time.now, is_public: true, published: nil)
 				visit('/communities/va-immersive/about')
 				within('#community-subnav') do
 					expect(page).to have_link('About', class: 'current-page')
