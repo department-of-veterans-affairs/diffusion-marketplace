be66e53b9582aff420ca722691952cb369ffe0dd
PhilipDeFraties <philip.defraties@gmail.com>
2024-06-12 12:40:23 -0600

    refactor ThreeColumnHelper#split_data_into_three_columns
    fixes so that it returns the same formatting as original logic
    
    adds spec file

--- app/helpers/three_column_data_helper.rb
+++ app/helpers/three_column_data_helper.rb
@@ -1,11 +1,15 @@
 module ThreeColumnDataHelper
   def split_data_into_three_columns(data_set)
     flat_data_set = data_set.to_a.flatten
-    count_per_column = (flat_data_set.size + 2) / 3
-    sliced_data_set = Array.new(3) { |i| flat_data_set[i * count_per_column, count_per_column] || [] }
-    if flat_data_set.size % 3
-      sliced_data_set[1] << sliced_data_set[2].shift if sliced_data_set[2].any?
-    end
+    n = flat_data_set.size
+    base_count = n / 3
+    remainder = n % 3
+
+    sliced_data_set = Array.new(3) { |i|
+      start_index = (i * base_count) + [i, remainder].min
+      end_index = start_index + base_count - 1 + (i < remainder ? 1 : 0)
+      flat_data_set[start_index..end_index] || []
+    }
     sliced_data_set
   end
 end
