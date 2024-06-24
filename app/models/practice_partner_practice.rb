87cca9f84b0d9d31f8fd55c7515736c3d75ffe3a
PhilipDeFraties <philip.defraties@gmail.com>
2024-06-24 16:11:35 -0600

    update PracticePartnerPractice model
    consolidates callbacks for clearing cache to safer after_commit callback and clears related practice's cache

--- app/models/practice_partner_practice.rb
+++ app/models/practice_partner_practice.rb
@@ -3,33 +3,15 @@ class PracticePartnerPractice < ApplicationRecord
   belongs_to :practice_partner
   belongs_to :practice
 
-  before_save :clear_searchable_practices_cache_on_save
-  before_destroy :clear_searchable_practices_cache_on_destroy
-  after_save :reset_searchable_practices_cache
-  after_destroy :reset_searchable_practices_cache
-
-  attr_accessor :reset_cached_searchable_practices
+  after_commit :clear_caches
 
   scope :order_by_id, -> { order(id: :asc) }
 
-  def clear_searchable_practices_cache
-    cache_keys = ["searchable_practices_json", "searchable_public_practices_json"]
-    cache_keys.each do |cache_key|
-      Cache.new.delete_cache_key(cache_key)
-    end
-  end
+  private
 
-  def clear_searchable_practices_cache_on_save
-    if self.changed?
-      self.reset_cached_searchable_practices = true
-    end
-  end
-
-  def clear_searchable_practices_cache_on_destroy
-    self.reset_cached_searchable_practices = true
-  end
-
-  def reset_searchable_practices_cache
-    clear_searchable_practices_cache if self.reset_cached_searchable_practices
+  def clear_caches
+    Rails.cache.delete("searchable_practices_json")
+    Rails.cache.delete("searchable_public_practices_json")
+    practices.clear_searchable_cache
   end
 end
