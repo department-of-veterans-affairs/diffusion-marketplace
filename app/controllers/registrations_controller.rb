96c6a480048e6d5a213781a6f4fa82aa840818eb
PhilipDeFraties <philip.defraties@gmail.com>
2024-07-16 15:00:13 -0600

    update RegistrationsController
    adds new action in order to overwrite and redirect to root path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,11 @@
 # Override the devise registrations controller
 class RegistrationsController < Devise::RegistrationsController
-  
+
   def edit
     super
+  end
+
+  def new
+    redirect_to root_path
   end
 end
