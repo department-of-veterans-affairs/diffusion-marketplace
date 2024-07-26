d35d320951f8e1aa7d3cccdfc6db6acb06e36ab1
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2024-07-25 17:23:31 -0700

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_25_224021) do
+ActiveRecord::Schema.define(version: 2024_07_26_002037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -1100,8 +1100,6 @@ ActiveRecord::Schema.define(version: 2024_07_25_224021) do
     t.boolean "published", default: false
     t.boolean "approved", default: false
     t.string "slug"
-    t.boolean "highlight", default: false, null: false
-    t.boolean "featured", default: false, null: false
     t.integer "ahoy_visit_id"
     t.string "training_provider_role"
     t.boolean "enabled", default: true, null: false
@@ -1113,15 +1111,10 @@ ActiveRecord::Schema.define(version: 2024_07_25_224021) do
     t.integer "maturity_level"
     t.datetime "date_published"
     t.datetime "practice_pages_updated"
-    t.string "highlight_body"
     t.boolean "retired", default: false, null: false
     t.string "retired_reason"
     t.boolean "is_public", default: false
     t.boolean "hidden", default: false, null: false
-    t.string "highlight_attachment_file_name"
-    t.string "highlight_attachment_content_type"
-    t.bigint "highlight_attachment_file_size"
-    t.datetime "highlight_attachment_updated_at"
     t.text "main_display_image_alt_text"
     t.integer "diffusion_histories_count", default: 0
     t.datetime "last_email_date"
