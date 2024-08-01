b94d0596e616e481778523e95fb1dcd8f25d8a06
Camille Villa <5402927+camillevilla@users.noreply.github.com>
2024-08-01 11:53:17 -0700

    gold_status_tagline, target_measures, target_success

---
+++ db/migrate/20240801184827_remove_retired_fields_from_practices.rb
@@ -0,0 +1,7 @@
+class RemoveRetiredFieldsFromPractices < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :practices, :gold_status_tagline, :string
+    remove_column :practices, :target_measures, :string
+    remove_column :practices, :target_success, :integer
+  end
+end
