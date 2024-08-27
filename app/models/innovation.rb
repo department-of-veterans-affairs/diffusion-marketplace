0d95f0ef1c6c7cfdca1ee487b7d5812612c8cb2e
PhilipDeFraties <philip.defraties@gmail.com>
2024-08-27 17:29:36 -0600

    add polymorphic relation on Innovation model to category_practices

--- app/models/innovation.rb
+++ app/models/innovation.rb
@@ -3,4 +3,6 @@ class Innovation < ApplicationRecord
   has_paper_trail
 
   belongs_to :user, optional: true
+  has_many :category_practices, as: :innovable, dependent: :destroy, autosave: true
+  has_many :categories, through: :category_practices
 end
