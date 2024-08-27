4b7f198d5813bf901237cc3e048f22f31ec6a357
PhilipDeFraties <philip.defraties@gmail.com>
2024-08-27 17:37:37 -0600

    update admin/practices file to expect new polymorphic association to categories

--- app/admin/practices.rb
+++ app/admin/practices.rb
@@ -471,9 +471,9 @@ ActiveAdmin.register Practice do # rubocop:disable Metrics/BlockLength
       selected_categories = params[:practice][:category_ids].drop(1)
       selected_categories.map! { |cat| cat.to_i }
       practice = Practice.find_by(name: params[:practice][:name])
-      current_categories = CategoryPractice.where(practice_id: practice[:id]) unless practice.nil?
+      current_categories = CategoryPractice.where(innovable_id: practice[:id]) unless practice.nil?
       if selected_categories.length > 0 && practice.present?
-        selected_categories.map { |cat| CategoryPractice.find_or_create_by!(category_id: cat, practice_id: practice[:id]) }
+        selected_categories.map { |cat| CategoryPractice.find_or_create_by!(category_id: cat, innovable_id: practice[:id]) }
       end
 
       if params[:action] == 'update' && current_categories.present?
