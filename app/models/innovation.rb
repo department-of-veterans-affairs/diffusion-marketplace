38b7ad8639ed89a2b3d79eab3e7b34449d4f08e4
PhilipDeFraties <philip.defraties@gmail.com>
2024-08-28 17:42:32 -0600

    move practice_multimedia relationship from Product and Practice models into parent class Innovation

--- app/models/innovation.rb
+++ app/models/innovation.rb
@@ -5,4 +5,5 @@ class Innovation < ApplicationRecord
   belongs_to :user, optional: true
   has_many :category_practices, as: :innovable, dependent: :destroy, autosave: true
   has_many :categories, through: :category_practices
+  has_many :practice_multimedia, -> { order(id: :asc) }, as: :innovable, dependent: :destroy
 end
