29ccaf613f7951beaaf884014f0b432f1855f8c5
PhilipDeFraties <philip.defraties@gmail.com>
2024-08-28 14:53:58 -0600

    update Practice, Product, VaEmployee, and VaEmployee practice models to utilize polymorphic relationship reference innovable

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < Innovation
+  has_many :va_employee_practices, as: :innovable, dependent: :destroy
+  has_many :va_employees, -> { order(position: :asc) }, through: :va_employee_practices
+
   has_attached_file :main_display_image, styles: {thumb: '768x432>'}, :processors => [:cropper]
   validates :main_display_image_alt_text, presence: true, if: :main_display_image_present?
+  validates_attachment_content_type :main_display_image, content_type: /\Aimage\/.*\z/
 
   validates_uniqueness_of :name, {message: 'Product name already exists'}
-  validates_attachment_content_type :main_display_image, content_type: /\Aimage\/.*\z/
 
   private
 
