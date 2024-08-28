93e7bad62bfd8bd803fb8f01e49c2b785b6348a2
PhilipDeFraties <philip.defraties@gmail.com>
2024-08-28 17:45:02 -0600

    move va_employees relationship from Product and Practice models into parent class Innovation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,4 @@
 class Product < Innovation
-  has_many :va_employee_practices, as: :innovable, dependent: :destroy
-  has_many :va_employees, -> { order(position: :asc) }, through: :va_employee_practices
-
   has_attached_file :main_display_image, styles: {thumb: '768x432>'}, :processors => [:cropper]
 
   validates :main_display_image_alt_text, presence: true, if: :main_display_image_present?
