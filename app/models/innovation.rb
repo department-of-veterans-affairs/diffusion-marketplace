7da60b9cac52c43723ac52dfb713cea6f62f41ed
PhilipDeFraties <philip.defraties@gmail.com>
2024-08-28 17:45:02 -0600

    move va_employees relationship from Product and Practice models into parent class Innovation

--- app/models/innovation.rb
+++ app/models/innovation.rb
@@ -5,5 +5,7 @@ class Innovation < ApplicationRecord
   belongs_to :user, optional: true
   has_many :category_practices, as: :innovable, dependent: :destroy, autosave: true
   has_many :categories, through: :category_practices
+  has_many :va_employee_practices, as: :innovable, dependent: :destroy
+  has_many :va_employees, -> { order(position: :asc) }, through: :va_employee_practices
   has_many :practice_multimedia, -> { order(id: :asc) }, as: :innovable, dependent: :destroy
 end
