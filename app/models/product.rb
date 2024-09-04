04b4b5137b70fcb4546279ce3fd89990e05aa794
PhilipDeFraties <philip.defraties@gmail.com>
2024-09-04 12:15:07 -0600

    update Product model
    add name presence validation
    
    add ransackable_attributes for name and user_email

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,12 +3,20 @@ class Product < Innovation
 
   validates :main_display_image_alt_text, presence: true, if: :main_display_image_present?
   validates_attachment_content_type :main_display_image, content_type: /\Aimage\/.*\z/
-
+  validates :name, presence: true
   validates_uniqueness_of :name, {message: 'Product name already exists'}
 
   private
 
   def main_display_image_present?
     main_display_image.present?
+  end
+
+  ransacker :user_email do
+    Arel.sql("users.email")
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["name", "user_email"]
   end
 end
