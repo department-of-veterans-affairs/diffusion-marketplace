4d27c56274184033ce805795ca27b00efc12c638
PhilipDeFraties <philip.defraties@gmail.com>
2024-09-05 10:43:48 -0600

    update message for name validation on Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < Innovation
   has_attached_file :main_display_image, styles: {thumb: '768x432>'}, :processors => [:cropper]
 
-  validates_uniqueness_of :name, {message: 'Innovation name already exists'}
+  validates_uniqueness_of :name, {message: 'Product name already exists'}
   validates_attachment_content_type :main_display_image, content_type: /\Aimage\/.*\z/
 end
