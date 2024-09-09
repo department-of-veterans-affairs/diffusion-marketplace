de57e336c02342e84927e89ead905224a143f5eb
PhilipDeFraties <philip.defraties@gmail.com>
2024-09-09 17:12:03 -0600

    update admin/products feature specs
    adds sad path testing, restructures test organization

--- spec/features/admin/admin_products_spec.rb
+++ spec/features/admin/admin_products_spec.rb
@@ -7,73 +7,143 @@ RSpec.feature "Admin::Products", type: :feature do
     login_as(admin_user, scope: :user)
   end
 
-  scenario "Admin creates a product successfully" do
-    visit new_admin_product_path
+  describe "Creating a product" do
+    context "with valid attributes" do
+      scenario "creates a product successfully" do
+        visit new_admin_product_path
 
-    fill_in "Product name *Required*", with: "Test Product"
-    fill_in "User email", with: "testuser@va.gov"
+        fill_in "Product name *Required*", with: "Test Product"
+        fill_in "User email", with: "testuser@va.gov"
 
-    click_button "Create Product"
+        click_button "Create Product"
 
-    expect(page).to have_content("Product was successfully created")
-    expect(page).to have_content("Test Product")
-    expect(page).to have_content("testuser@va.gov")
+        expect(page).to have_content("Product was successfully created")
+        expect(page).to have_content("Test Product")
+        expect(page).to have_content("testuser@va.gov")
+      end
+
+      scenario "creates a product and adds another" do
+        visit new_admin_product_path
+
+        fill_in "Product name *Required*", with: "First Product"
+        fill_in "User email", with: "user@va.gov"
+        check "create_another"
+
+        click_button "Create Product"
+
+        expect(page).to have_content("Product was successfully created. You can create another one.")
+        expect(current_path).to eq(new_admin_product_path)
+        expect(page).not_to have_field("Product name *Required*", with: "First Product")
+      end
+
+      scenario "automatically creates a new user if email does not exist" do
+        visit new_admin_product_path
+
+        fill_in "Product name *Required*", with: "Product with New User"
+        fill_in "User email", with: "newuser@va.gov"
+
+        click_button "Create Product"
+
+        expect(page).to have_content("Product was successfully created")
+        expect(page).to have_content("Product with New User")
+        expect(page).to have_content("newuser@va.gov")
+
+        expect(User.find_by(email: "newuser@va.gov")).not_to be_nil
+      end
+    end
+
+    context "with invalid attributes" do
+      scenario "tries to create a product with an invalid email" do
+        visit new_admin_product_path
+
+        fill_in "Product name *Required*", with: "Invalid Email Product"
+        fill_in "User email", with: "invalidemail.com"
+
+        click_button "Create Product"
+
+        expect(page).to have_content("Email must be a valid @va.gov address")
+        expect(current_path).to eq(new_admin_product_path)
+
+        expect(Product.find_by(name: "Invalid Email Product")).to be_nil
+      end
+
+      scenario "tries to create a product with a taken name" do
+        create(:product, name: "Taken Product")
+
+        visit new_admin_product_path
+        fill_in "Product name *Required*", with: "Taken Product"
+        fill_in "User email", with: "user@va.gov"
+
+        click_button "Create Product"
+
+        expect(page).to have_content("Product name already exists")
+        expect(current_path).to eq(new_admin_product_path)
+
+        expect(Product.where(name: "Taken Product").count).to eq(1)
+      end
+    end
   end
 
-  scenario "Admin creates a product and adds another" do
-    visit new_admin_product_path
+  describe "Updating a product" do
+    let!(:product) { create(:product, name: "Old Product", user: create(:user, email: "olduser@va.gov")) }
 
-    fill_in "Product name *Required*", with: "First Product"
-    fill_in "User email", with: "user@va.gov"
+    context "with valid attributes" do
+      scenario "updates a product successfully" do
+        visit edit_admin_product_path(product)
 
-    check "create_another"
+        fill_in "Product name *Required*", with: "Updated Product"
+        fill_in "User email", with: "newuser@va.gov"
 
-    click_button "Create Product"
+        click_button "Update Product"
 
-    expect(page).to have_content("Product was successfully created. You can create another one.")
-    expect(current_path).to eq(new_admin_product_path)
-    expect(page).not_to have_field("Product name *Required*", with: "First Product")
-  end
+        expect(page).to have_content("Product was successfully updated")
+        expect(page).to have_content("Updated Product")
+        expect(page).to have_content("newuser@va.gov")
+      end
 
-  scenario "Admin updates a product successfully" do
-    product = create(:product, name: "Old Product", user: create(:user, email: "olduser@va.gov"))
+      scenario "allows the user_email to be blank and sets the product's user to nil" do
+        visit edit_admin_product_path(product)
 
-    visit edit_admin_product_path(product)
+        fill_in "Product name *Required*", with: "Updated Product"
+        fill_in "User email", with: ""
 
-    fill_in "Product name *Required*", with: "Updated Product"
-    fill_in "User email", with: "newuser@va.gov"
+        click_button "Update Product"
 
-    click_button "Update Product"
+        expect(page).to have_content("Product was successfully updated")
+        expect(page).to have_content("Updated Product")
+        expect(product.reload.user).to be_nil
+      end
+    end
 
-    expect(page).to have_content("Product was successfully updated")
-    expect(page).to have_content("Updated Product")
-    expect(page).to have_content("newuser@va.gov")
-  end
+    context "with invalid attributes" do
+      scenario "tries to update a product with an invalid email" do
+        visit edit_admin_product_path(product)
 
-  scenario "Admin tries to create a product with an invalid email" do
-    visit new_admin_product_path
+        fill_in "Product name *Required*", with: "Updated Product"
+        fill_in "User email", with: "invalidemail.com"
 
-    fill_in "Product name *Required*", with: "Invalid Email Product"
-    fill_in "User email", with: "invalidemail.com"
+        click_button "Update Product"
 
-    click_button "Create Product"
+        expect(page).to have_content("Email must be a valid @va.gov address")
+        expect(current_path).to eq(edit_admin_product_path(product))
 
-    expect(page).to have_content("There was an error. Email must be a valid @va.gov address.")
-    expect(current_path).to eq(new_admin_product_path)
-  end
+        expect(product.reload.user.email).to eq("olduser@va.gov")
+      end
 
-  scenario "Admin creates a product and automatically creates a new user if email does not exist" do
-    visit new_admin_product_path
+      scenario "tries to update a product with a taken name" do
+        create(:product, name: "Taken Product")
 
-    fill_in "Product name *Required*", with: "Product with New User"
-    fill_in "User email", with: "newuser@va.gov"
+        visit edit_admin_product_path(product)
+        fill_in "Product name *Required*", with: "Taken Product"
+        fill_in "User email", with: "newuser@va.gov"
 
-    click_button "Create Product"
+        click_button "Update Product"
 
-    expect(page).to have_content("Product was successfully created")
-    expect(page).to have_content("Product with New User")
-    expect(page).to have_content("newuser@va.gov")
+        expect(page).to have_content("Product name already exists")
+        expect(current_path).to eq(edit_admin_product_path(product))
 
-    expect(User.find_by(email: "newuser@va.gov")).not_to be_nil
+        expect(product.reload.name).to eq("Old Product")
+      end
+    end
   end
 end
