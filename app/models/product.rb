d9dfeb56fff06cbad33a7abcb6af76f19607861f
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2024-09-11 14:03:50 -0600

    dm-5067 product active admin (#1010)
    * update schema with new products table
    
    * update schema with new polymorphic association for category_practices
    
    * update Practice, Product, VaEmployee, and VaEmployee practice models to utilize polymorphic relationship reference innovable
    
    * update Practice, Product, and PracticeMultimedium models and specs with polymorphic relationship
    
    * move practice_multimedia relationship from Product and Practice models into parent class Innovation
    
    * move va_employees relationship from Product and Practice models into parent class Innovation
    
    * add index to products name column in order to add unique constraint at the db level
    
    this enables the use of `create_or_find_by` with the name attribute
    
    * add admin/products file with overwrites for create and update actions
    
    * update Product model
    
    add name presence validation
    
    add ransackable_attributes for name and user_email
    
    * add admin/products feature spec file
    
    * edit admin/products to remove file export until setup decided upon
    
    * remove except: [:destroy] from admin/products actions :all
    
    * add user_email method to Product model
    
    * add "Owner email", timestamps, actions to admin/products index
    
    * add scoped_collection to admin/products controller block
    
    allows for filtering through association
    
    * remove download_links: false from admin/products index view
    
    * spacing
    
    * edit Product#user_email to not be private
    
    * update admin/products
    
    removes permit_params method in favor of traditional strong params methods for product attrs and user_email
    
    edits scoped_collection to use left_joins for records with no :user
    
    edits set_product_user method to set nil value for product.user
    
    * update admin/products to utilize strong params for user_email
    
    * update admin/products
    
    edits structure of create and update actions to utilize transaction blocks
    
    updates error handling method to deliver concise error messages
    
    * update admin/products feature specs
    
    adds sad path testing, restructures test organization

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,26 @@
 class Product < Innovation
   has_attached_file :main_display_image, styles: {thumb: '768x432>'}, :processors => [:cropper]
-  validates :main_display_image_alt_text, presence: true, if: :main_display_image_present?
 
-  validates_uniqueness_of :name, {message: 'Product name already exists'}
+  validates :main_display_image_alt_text, presence: true, if: :main_display_image_present?
   validates_attachment_content_type :main_display_image, content_type: /\Aimage\/.*\z/
+  validates :name, presence: true
+  validates_uniqueness_of :name, {message: 'Product name already exists'}
+
+  def user_email
+    user&.email
+  end
 
   private
 
   def main_display_image_present?
     main_display_image.present?
+  end
+
+  ransacker :user_email do
+    Arel.sql("users.email")
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["name", "user_email"]
   end
 end
