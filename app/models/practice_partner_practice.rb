638ca5a10a310b7fd03750a0835cdeaf216e21be
PhilipDeFraties <philip.defraties@gmail.com>
2024-09-12 15:09:23 -0600

    update PracticePartnerPractice relationship to practices to be polymorphic as innovable

--- app/models/practice_partner_practice.rb
+++ app/models/practice_partner_practice.rb
@@ -1,9 +1,9 @@
 class PracticePartnerPractice < ApplicationRecord
   include ActiveModel::Dirty
   belongs_to :practice_partner
-  belongs_to :practice
+  belongs_to :innovable, polymorphic: true
 
-  after_commit -> { practice.clear_searchable_cache }
+  after_commit -> { innovable.clear_searchable_cache }
 
   scope :order_by_id, -> { order(id: :asc) }
 end
