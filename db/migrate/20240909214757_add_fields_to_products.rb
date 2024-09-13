ca3e7124221477fe8cc0f86938602931becb4270
PhilipDeFraties <philip.defraties@gmail.com>
2024-09-13 12:45:35 -0600

    remove addition of :origin column from AddFieldsToProducts migration

--- db/migrate/20240909214757_add_fields_to_products.rb
+++ db/migrate/20240909214757_add_fields_to_products.rb
@@ -1,6 +1,5 @@
 class AddFieldsToProducts < ActiveRecord::Migration[6.1]
   def change
-    add_column :products, :origin, :string
     add_column :products, :usage, :string
     add_column :products, :price, :string
   end
