a58836507035273994a767788761348b6c92bb27
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2024-09-26 16:39:05 -0600

    dm-5202 product editor categories (#1043)
    * update ProductsController
    
    adds update logic to handle image cropping
    
    * abstract shared controller method permitted_dynamic_keys to new concern file
    
    update method to be non-destructive to incoming params
    
    include the concern class in both controllers
    
    * update product editor description spec
    
    adds test for category management
    
    * update product editor description page partial
    
    adds inclusion of allCategories js
    
    adds categories section to ui
    
    * update category_checkbox partial
    
    implements dynamically generated innovation_type for input name in order to properly nest submitted params
    
    extracts query logic to categories_helper module
    
    * update products controller
    
    adds inst var for categories to description action
    
    adds category key to strong params
    
    adds update_category_practices method for handling the updating of product's categories
    
    * move ProductsController#update_category_practices method to Product model
    
    * update ProductsController#handle_multimedia_updates to be model method Product#update_multimedia
    
    * update SavePracticeService#update_category_practices to utilize polymorphic association name innovable
    
    * add "Products Tagged" to admin/categories/show table

--- spec/features/product_editor/description_spec.rb
+++ spec/features/product_editor/description_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Product editor - introduction', type: :feature do
+describe 'Product editor - description', type: :feature do
   let!(:product) { create(:product)}
   let!(:user) { create(:user) }
   let!(:admin) { create(:user, :admin)}
@@ -58,6 +58,40 @@ describe 'Product editor - introduction', type: :feature do
       click_button 'Submit'
       expect(page).to have_current_path(product_description_path(new_product))
       expect(page).to have_content('Product name already exists')
+    end
+
+    it 'allows product categories to be updated' do
+      parent_cat1 = create(:category, name: "clinical")
+      cat1 = create(:category, name: "Test Category 1", parent_category: parent_cat1)
+      cat2 = create(:category, name: "Test Category 2", parent_category: parent_cat1)
+      create(:category_practice, innovable: product, category: cat1)
+
+      visit product_description_path(product)
+      within(:css, '.dm-clinical-category-columns-container') do
+        expect(find("#cat-2-input", visible: false)).to be_checked
+        expect(find("#cat-3-input", visible: false)).to_not be_checked
+        expect(find("#cat-all-clinical-input", visible: false)).to_not be_checked
+        find('.usa-checkbox__label[title="Test Category 2"]').click
+        expect(find("#cat-all-clinical-input", visible: false)).to be_checked
+      end
+      click_button 'Submit'
+
+      expect(product.categories).to include(cat1, cat2)
+
+      visit product_description_path(product)
+      within(:css, '.dm-clinical-category-columns-container') do
+        expect(find("#cat-2-input", visible: false)).to be_checked
+        expect(find("#cat-3-input", visible: false)).to be_checked
+        expect(find("#cat-all-clinical-input", visible: false)).to be_checked
+        find('.usa-checkbox__label[title="Test Category 1"]').click
+        expect(find("#cat-2-input", visible: false)).to_not be_checked
+        expect(find("#cat-3-input", visible: false)).to be_checked
+        expect(find("#cat-all-clinical-input", visible: false)).to_not be_checked
+      end
+      click_button 'Submit'
+
+      expect(product.categories).to include(cat2)
+      expect(product.categories).to_not include(cat1)
     end
   end
 
