8fe20f4f8321a3e0fff96c6ede8a27f4921b8afd
Camille Villa <Camille.Villa@va.gov>
2024-10-02 15:26:49 -0700

    DM-5220: Add siderail with Tags to Product page (#1051)
    * Add siderail with Tags to product page
    
    * Move Partners field up on show page
    
    * Add spec
    
    * Update scope and share between practices and products
    
    * Make related not_none scope a little more concise
    
    * Make table name explicit
    
    * Update factories
    
    ---------
    
    Co-authored-by: Camille Villa <5402927+camillevilla@users.noreply.github.com>

--- spec/factories/category_practices.rb
+++ spec/factories/category_practices.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
   factory :category_practice do
     association :category
-    association :innovable, factory: :practice
+
+    trait :for_product do
+      association :innovable, factory: :product
+    end
+
+    trait :for_practice do
+      association :innovable, factory: :practice
+    end
   end
 end
