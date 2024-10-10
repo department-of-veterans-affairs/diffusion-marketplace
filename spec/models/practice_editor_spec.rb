a699a602abd9aa2697a0d6ab5cded3bb319af47b
PhilipDeFraties <philip.defraties@gmail.com>
2024-10-10 12:30:04 -0600

    update practice_editor_spec to utilize polymorphic association in PracticeEditor query

--- spec/models/practice_editor_spec.rb
+++ spec/models/practice_editor_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe PracticeEditor, type: :model do
   describe 'associations' do
-    it { should belong_to(:practice) }
+    it { should belong_to(:innovable) }
     it { should belong_to(:user) }
   end
 end
