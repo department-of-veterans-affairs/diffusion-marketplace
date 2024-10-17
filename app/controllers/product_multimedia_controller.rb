4efad6f7cbc46213194ef1ec0d0f95c0e17a8e24
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2024-10-17 15:27:04 -0600

    DM-5273 Dynamically render product resource links (#1079)
    * update practice resource partials to dynamically render links to resource download endpoint
    
    * add ProductMultimediaController
    
    * rename PracticeEditorUtils to ProductEditorUtils and rename is_user_an_editor_for_practice to is_user_an_editor_for_innovation and update query to utilize polymorphic association
    
    * update all places using old PracticeEditorUtils class and method names
    
    * update resource_file_attachment partial to conditionally render product multimedia link if resource belongs to a Product
    
    * add :product_multimedia to routes nested under :products with path to download endpoint
    
    * update resource_file_attachment partial to check class type of resource
    
    * update practices/overview partial to remove redundand "Files" header
    
    * add empty line at end of file
    
    * update resource_file_attachement partial
    
    swap file check conditional so more specific condition is on top
    
    ---------
    
    Co-authored-by: Camille Villa <5402927+camillevilla@users.noreply.github.com>

---
+++ app/controllers/product_multimedia_controller.rb
@@ -0,0 +1,52 @@
+class ProductMultimediaController < ApplicationController
+  before_action :authenticate_user!, except: [:download]
+  before_action :set_product_resource, only: [:download]
+
+  def download
+    if @product.published?
+      handle_enabled_practice
+    elsif current_user
+      handle_user_with_unpublished_product
+    else
+      unauthorized_response
+    end
+  end
+
+  private
+
+  def set_product_resource
+    @product = Product.find_by(id: resource_params[:product_id])
+
+    if @product
+      @product_resource = @product.practice_multimedia.find_by(id: resource_params[:id])
+    end
+
+    unless @product_resource
+      render plain: "Product or Resource not found", status: :not_found and return
+    end
+  end
+
+  def resource_params
+    params.permit(:product_id, :id, :resource_type)
+  end
+
+  def handle_enabled_practice
+    if @product.is_public || current_user
+      redirect_to @product_resource.attachment_s3_presigned_url
+    else
+      unauthorized_response
+    end
+  end
+
+  def handle_user_with_unpublished_product
+    if check_user_product_permissions
+      redirect_to @product_resource.attachment_s3_presigned_url
+    else
+      unauthorized_response
+    end
+  end
+
+  def check_user_product_permissions
+    current_user.has_role?(:admin) || @product.user_id == current_user.id || is_user_an_editor_for_innovation(@product, current_user)
+  end
+end
