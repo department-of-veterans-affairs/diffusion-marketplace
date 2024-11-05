321d3582ef7dbd08456e0af3d4019208eea6fce4
PhilipDeFraties <philip.defraties@gmail.com>
2024-11-05 11:14:57 -0700

    update users profile form to remove :fellowship field

--- spec/features/users/user_profile_spec.rb
+++ spec/features/users/user_profile_spec.rb
@@ -83,7 +83,6 @@ describe 'The user index', type: :feature do
     fill_in('user[alt_first_name]', with: 'Alt first name')
     fill_in('user[alt_last_name]', with: 'Alt last name')
     fill_in('Title (Public Bio)', with: 'public bio title text')
-    fill_in('Fellowship (Public Bio)', with: 'public bio fellowship text')
     fill_in('Work (Public Bio)', with: 'public bio work text')
     fill_in('Credentials (Public Bio)', with: 'public bio credentials text')
     fill_in('Project (Public Bio)', with: 'project text')
@@ -93,7 +92,6 @@ describe 'The user index', type: :feature do
     expect(sb.alt_first_name).to eq('Alt first name')
     expect(sb.alt_last_name).to eq('Alt last name')
     expect(sb.alt_job_title).to eq('public bio title text')
-    expect(sb.fellowship).to eq('public bio fellowship text')
     expect(sb.work).to eq('public bio work text')
     expect(sb.credentials).to eq('public bio credentials text')
     expect(sb.project).to eq('project text')
@@ -108,7 +106,6 @@ describe 'The user index', type: :feature do
     expect(page).not_to have_selector("input[value='#{@user.alt_job_title}']")
     expect(page).not_to have_selector("input[value='#{@user.credentials}']")
     expect(page).not_to have_selector("input[value='#{@user.work}']")
-    expect(page).not_to have_selector("input[value='#{@user.fellowship}']")
     expect(page).not_to have_selector("input[value='#{@user.project}']")
   end
 
