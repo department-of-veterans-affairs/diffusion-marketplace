834c68178ed6a64acb5bd4bec0574f3991c4b3fe
PhilipDeFraties <philip.defraties@gmail.com>
2024-11-05 11:14:21 -0700

    update users col additions migrations to remove :fellowship col

--- db/migrate/20241030205550_add_columns_to_users.rb
+++ db/migrate/20241030205550_add_columns_to_users.rb
@@ -6,6 +6,5 @@ class AddColumnsToUsers < ActiveRecord::Migration[6.1]
     add_column :users, :work, :text
     add_column :users, :project, :text
     add_column :users, :alt_job_title, :string
-    add_column :users, :fellowship, :string
   end
 end
