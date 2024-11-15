32be301cf05a59d3f787dbf9f9e9651c8eb424b2
PhilipDeFraties <philip.defraties@gmail.com>
2024-11-14 17:10:29 -0700

    update users migration to make :work col json

--- db/migrate/20241030205550_add_columns_to_users.rb
+++ db/migrate/20241030205550_add_columns_to_users.rb
@@ -3,7 +3,7 @@ class AddColumnsToUsers < ActiveRecord::Migration[6.1]
     add_column :users, :alt_first_name, :string
     add_column :users, :alt_last_name, :string
     add_column :users, :credentials, :text
-    add_column :users, :work, :text
+    add_column :users, :work, :json
     add_column :users, :project, :text
     add_column :users, :alt_job_title, :string
     add_column :users, :accolades, :string
