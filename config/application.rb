742ee641cf5cb6bc83c1c03f35c4b6aac8d017ba
PhilipDeFraties <philip.defraties@gmail.com>
2025-01-06 12:03:57 -0700

    set config.active_support.disable_to_s_conversion as per rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module DiffusionMarketplace
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.active_support.cache_format_version = 7.0
+    config.active_support.disable_to_s_conversion = true
 
     # Configuration for the application, engines, and railties goes here.
     #
