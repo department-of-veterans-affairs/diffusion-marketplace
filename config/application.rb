0660466db80236b0b84283803717017f67cc5e4e
PhilipDeFraties <philip.defraties@gmail.com>
2025-01-06 11:17:18 -0700

    set config.active_support.cache_format_version per rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module DiffusionMarketplace
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.active_support.cache_format_version = 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
